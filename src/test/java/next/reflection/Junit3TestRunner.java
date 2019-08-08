7b1f9f687555cd679fb24c3b6cfeb4d42bf7e4d8
이기승 <keeseung@sixshop.com>
2019-08-08 22:09:46 +0900

    feat: 요구사항 2 - test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,21 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
+
+    private static final String TEST = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        for(Method method : declaredMethods) {
+            if (method.getName().startsWith(TEST)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
