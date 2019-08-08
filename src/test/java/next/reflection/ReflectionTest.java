63579b601111647c0a7f07027be7ff5449bf311d
dave915 <Htaehyun@gmail.com>
2019-08-08 22:15:59 +0900

    feat 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,59 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
+import java.lang.reflect.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
+    @DisplayName("Question class의 필드, 생성자, 메소드 정보 출력")
     public void showClass() {
         Class<Question> clazz = Question.class;
+        String fieldsInfo = membersInfo(clazz.getDeclaredFields());
+        String constructorInfo = membersInfo(clazz.getConstructors());
+        String methodInfo = membersInfo(clazz.getDeclaredMethods());
+
         logger.debug(clazz.getName());
+        logger.debug("fields info : \n{}", fieldsInfo);
+        logger.debug("constructor info : \n{}", constructorInfo);
+        logger.debug("method info : \n{}", methodInfo);
+
+    }
+
+    private String membersInfo(Member[] members) {
+        return Stream.of(members)
+                .map(this::makeMemberInfoString)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String makeMemberInfoString(Member member) {
+        String fieldFormat = "%s %s %s";
+
+        String modifier = Modifier.toString(member.getModifiers());
+        String type = getMemberType(member);
+        String name = member.getName();
+        return String.format(fieldFormat, modifier, type, name);
+    }
+
+    private String getMemberType(Member member) {
+        if (member instanceof Field) {
+            Field field = (Field) member;
+            return field.getType().getSimpleName();
+        }
+
+        if (member instanceof Method) {
+            Method method = (Method) member;
+            return method.getReturnType().getSimpleName();
+        }
+
+        return "";
     }
 
     @Test
