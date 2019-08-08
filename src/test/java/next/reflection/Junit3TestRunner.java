0618c8715a9ed23169f2b666fa579868aa34e385
KangHyunGu <hkkang@woowahan.com>
2019-08-08 22:28:40 +0900

    feat : 요구사항 1 - 클래스 정보 출력
    - 클래스 리플렉션 테스트에서 갯수 체크 외에 실제 반환값 테스트하도록 수정

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,13 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Stream.of(clazz.getDeclaredMethods());
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,25 +33,73 @@ public class ReflectionTest {
                 .as("Field[] getFields() : 접근 가능한 public 필드 목록을 반환한다.")
                 .isEmpty();
 
-        assertThat(clazz.getDeclaredFields().length)
+        List<String> declaredFields = Arrays.stream(clazz.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.toList());
+        assertThat(declaredFields)
                 .as("Field[] getDeclaredFields() : 모든 필드 목록을 반환한다.")
-                .isEqualTo(6);
+                .hasSize(6)
+                .containsOnly("questionId", "writer", "title", "contents", "createdDate", "countOfComment");
 
-        assertThat(clazz.getConstructors().length)
+        List<String> constructors = Arrays.stream(clazz.getConstructors())
+                .map(Constructor::getName)
+                .collect(Collectors.toList());
+        assertThat(constructors)
                 .as("Constructor[] getConstructors() : 접근 가능한 public 생성자 목록을 반환한다.")
-                .isEqualTo(2);
+                .hasSize(2)
+                .containsOnly("next.reflection.Question", "next.reflection.Question");
 
-        assertThat(clazz.getDeclaredConstructors().length)
+        List<String> declaredConstructors = Arrays.stream(clazz.getDeclaredConstructors())
+                .map(Constructor::getName)
+                .collect(Collectors.toList());
+        assertThat(declaredConstructors)
                 .as("Constructor[] getDeclaredConstructors() : 모든 생성자 목록을 반환한다.")
-                .isEqualTo(2);
+                .hasSize(2)
+                .containsOnly("next.reflection.Question", "next.reflection.Question");
 
-        assertThat(clazz.getMethods().length)
+        List<String> methods = Arrays.stream(clazz.getMethods())
+                .map(Method::getName)
+                .collect(Collectors.toList());
+        assertThat(methods)
                 .as("Method[] getMethods() : 부모 클래스, 자신 클래스의 접근 가능한 public 메서드 목록을 반환한다.")
-                .isEqualTo(17);
+                .hasSize(17)
+                .containsOnly(
+                        "equals",
+                        "toString",
+                        "hashCode",
+                        "update",
+                        "getQuestionId",
+                        "getWriter",
+                        "getTitle",
+                        "getCreatedDate",
+                        "getTimeFromCreateDate",
+                        "getCountOfComment",
+                        "getContents",
+                        "wait",
+                        "wait",
+                        "wait",
+                        "getClass",
+                        "notify",
+                        "notifyAll");
 
-        assertThat(clazz.getDeclaredMethods().length)
-                .as("Method[] getDeclaredMethods() : 모든 메서드 목록을 반환한다.")
-                .isEqualTo(11);
+        List<String> declaredMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .map(Method::getName)
+                .collect(Collectors.toList());
+        assertThat(declaredMethods)
+                .as("Method[] getDeclaredMethods() : 상속 제외한 자신의 모든 메서드 목록을 반환한다.")
+                .hasSize(11)
+                .containsOnly(
+                        "equals",
+                        "toString",
+                        "hashCode",
+                        "update",
+                        "getTitle",
+                        "getQuestionId",
+                        "getWriter",
+                        "getCreatedDate",
+                        "getTimeFromCreateDate",
+                        "getCountOfComment",
+                        "getContents");
     }
 
     @Test
