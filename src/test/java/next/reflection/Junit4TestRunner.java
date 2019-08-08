433cc859123c20fe65381cf22feb6890ea5892cb
Hwatu(박현식) <phs1116@gmail.com>
2019-08-08 22:26:27 +0900

    (feat) 1단계 테스트 구현

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,23 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> methodList = Arrays.asList(clazz.getDeclaredMethods());
+        for (Method method : methodList) {
+            Annotation testAnnotation = method.getAnnotation(MyTest.class);
+            if (Objects.nonNull(testAnnotation)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
+
     }
 }
