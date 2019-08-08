01150e9682d83606c864647c5628000617c7e105
Dongkwon Lee <circlee7@gmail.com>
2019-08-08 21:53:40 +0900

    feat : 요구사항2

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit3TestRunner {
+
+    private static final String TEST_PREFIX = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith(TEST_PREFIX))
+                .collect(Collectors.toList());
+
+        Object testInatance = clazz.newInstance();
+        for(Method method : methods) {
+            method.invoke(testInatance);
+        }
+
+
     }
 }
