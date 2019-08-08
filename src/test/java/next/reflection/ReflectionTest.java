95994112c983e0f0aa9cf5934c93232ebb3cd806
ssosso <ssossohow@gmail.com>
2019-08-08 22:31:48 +0900

    [step1] feat generate instance with constructor reflaction

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,21 +40,35 @@ public class ReflectionTest {
     }
 
     @Test
-    public void privateFieldAccess() throws Exception {
-        Map<String, Object> fieldValue = new HashMap<>();
+    public void accessPrivateField() throws Exception {
+        Map<String, Object> fieldValues = new HashMap<>();
 
-        fieldValue.put("name", "장소현");
-        fieldValue.put("age", 28);
+        fieldValues.put("name", "장소현");
+        fieldValues.put("age", 28);
 
         Class<Student> clazz = Student.class;
         Student student = new Student();
         logger.debug(clazz.getName());
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
-            field.set(student, fieldValue.get(field.getName()));
+            field.set(student, fieldValues.get(field.getName()));
         }
 
         assertThat(student.getName()).isEqualTo("장소현");
         assertThat(student.getAge()).isEqualTo(28);
+    }
+
+    @Test
+    public void accessConstructorWithArgs() throws Exception {
+        Map<Type, Object> parameterValues = new HashMap<>();
+        parameterValues.put(Long.TYPE, 100);
+        parameterValues.put(Integer.TYPE, 1515);
+        parameterValues.put(String.class, "스트링입니다");
+
+        Class<Question> clazz = Question.class;
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            logger.debug(constructor.newInstance(Arrays.stream(constructor.getParameterTypes())
+                    .map(parameterValues::get).toArray()).toString());
+        }
     }
 }
