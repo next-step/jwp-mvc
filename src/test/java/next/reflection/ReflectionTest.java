d2f7d9fc9cdde749473322ba1e0c10f5ebd9776d
Dongkwon Lee <circlee7@gmail.com>
2019-08-08 23:32:05 +0900

    feat : 요구사항5

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,14 +43,25 @@ public class ReflectionTest {
     @Test
     @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
+
+        Map<Class<?>, Object> sampleTypeValues = new HashMap<>();
+        sampleTypeValues.put(int.class, 1);
+        sampleTypeValues.put(long.class, 2L);
+        sampleTypeValues.put(String.class, "test");
+        sampleTypeValues.put(Date.class, new Date());
+
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
+
+            List<Object> params = Arrays.stream(parameterTypes)
+                    .map(type -> sampleTypeValues.get(type))
+                    .collect(Collectors.toList());
+
+            Object newInstance = constructor.newInstance(params.toArray(new Object[params.size()]));
+            logger.debug("newInstance : {}", newInstance);
+            assertThat(newInstance).isNotNull();
         }
     }
 
