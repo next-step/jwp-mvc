c9324ea44a4711f6fa08eca116b7e824499c80ee
Juyoung <juy5790@outlook.com>
2019-08-08 22:06:49 +0900

    feat #step1 요구사항3 구현

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,18 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if(method.isAnnotationPresent(MyTest.class)){
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
