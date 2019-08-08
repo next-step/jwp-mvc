59bb90747c051f087695ada1548798aa0bbc2762
KangHyunGu <hkkang@woowahan.com>
2019-08-08 22:13:07 +0900

    feat : 요구사항 1 - 클래스 정보 출력
    - src/test/java > next.reflection > ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -13,6 +15,38 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        assertThat(clazz.getName())
+                .as("String getName() : 패키지 + 클래스 이름을 반환한다.")
+                .isEqualTo("next.reflection.Question");
+
+        assertThat(clazz.getModifiers())
+                .as("int getModifiers() : 클래스의 접근 제어자를 숫자로 반환한다.")
+                .isEqualTo(1);
+
+        assertThat(clazz.getFields())
+                .as("Field[] getFields() : 접근 가능한 public 필드 목록을 반환한다.")
+                .isEmpty();
+
+        assertThat(clazz.getDeclaredFields().length)
+                .as("Field[] getDeclaredFields() : 모든 필드 목록을 반환한다.")
+                .isEqualTo(6);
+
+        assertThat(clazz.getConstructors().length)
+                .as("Constructor[] getConstructors() : 접근 가능한 public 생성자 목록을 반환한다.")
+                .isEqualTo(2);
+
+        assertThat(clazz.getDeclaredConstructors().length)
+                .as("Constructor[] getDeclaredConstructors() : 모든 생성자 목록을 반환한다.")
+                .isEqualTo(2);
+
+        assertThat(clazz.getMethods().length)
+                .as("Method[] getMethods() : 부모 클래스, 자신 클래스의 접근 가능한 public 메서드 목록을 반환한다.")
+                .isEqualTo(17);
+
+        assertThat(clazz.getDeclaredMethods().length)
+                .as("Method[] getDeclaredMethods() : 모든 메서드 목록을 반환한다.")
+                .isEqualTo(11);
     }
 
     @Test
