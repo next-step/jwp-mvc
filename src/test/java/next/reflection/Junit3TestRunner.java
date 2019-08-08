d2370c16e0bc9aac5e0ffb7381fd60ea9f7e0d5e
박시홍 <hongsii.dev@gmail.com>
2019-08-09 08:28:24 +0900

    [Step1] 자바 reflection (#3)
    * [요구사항-1] 클래스 정보 출력
    
    * [요구사항-2] test로 시작하는 메소드 실행
    
    * [요구사항-3] @Test 애노테이션 메소드 실행
    
    * [요구사항-4] private field에 값 할당
    
    * [요구사항-5] 인자를 가진 생성자의 인스턴스 생성
    
    * [요구사항-6] component scan

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,20 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,21 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,19 +24,59 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        // 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(it -> logger.debug("Field : {}", it.getName()));
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(it -> logger.debug("constructor : {}", it.getName()));
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(it -> logger.debug("method : {}", it.getName()));
+    }
+
+    @Test
+    public void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Student student = new Student();
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "이름");
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 5);
+
+        logger.debug("student : {}", student);
     }
 
     @Test
     @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+            if (parameterTypes.length == 3) {
+                constructor.newInstance("작성자", "제목", "내용");
+            } else if (parameterTypes.length == 6) {
+                constructor.newInstance(1, "작성자", "제목", "내용", new Date(), 5);
+            } else {
+                constructor.newInstance();
             }
         }
+    }
+
+    @Test
+    void di() throws Exception {
+        Reflections reflections = new Reflections("core.di.factory.example", new SubTypesScanner(), new TypeAnnotationsScanner());
+        findAnnotatedClass(reflections, Controller.class);
+        findAnnotatedClass(reflections, Service.class);
+        findAnnotatedClass(reflections, Repository.class);
+    }
+
+    private void findAnnotatedClass(Reflections reflections, Class<? extends Annotation> annotation) {
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(annotation);
+        controllers.stream()
+                .forEach(it -> logger.debug(it.getName()));
     }
 }
