2f4f1cc3f1c971726871f3aa2f627f25ed44cdc5
Jaeyeonling <jaeyeonling@gmail.com>
2019-08-08 22:30:23 +0900

    [feat] 요구사항5 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Date;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -15,7 +17,7 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
-        for (final Constructor constructor : clazz.getConstructors()) {
+        for (final Constructor constructor : clazz.getDeclaredConstructors()) {
             logger.debug("\tconstructor: {}", constructor);
 
             for (final Class parameterType : constructor.getParameterTypes()) {
@@ -56,14 +58,7 @@ public class ReflectionTest {
         final Object object = clazz.getConstructor().newInstance();
         for (final Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
-
-            final String fieldType = field.getType().getSimpleName();
-            if ("String".equals(fieldType)) {
-                field.set(object, "StringValue");
-            }
-            if ("int".equals(fieldType)) {
-                field.set(object, 100);
-            }
+            field.set(object, getMockValue(field.getType()));
         }
 
         for (final Method method : clazz.getMethods()) {
@@ -75,4 +70,38 @@ public class ReflectionTest {
             logger.debug("{}: {}", method.getName(), fieldValue);
         }
     }
+
+    @Test
+    void newQuestion() throws Exception {
+        Class<Question> clazz = Question.class;
+
+        for (final Constructor constructor : clazz.getDeclaredConstructors()) {
+            final Object[] parameters = Arrays.stream(constructor.getParameterTypes())
+                    .map(this::getMockValue)
+                    .toArray();
+
+            final Object object = constructor.newInstance(parameters);
+
+            logger.debug("{}", object);
+        }
+    }
+
+    private Object getMockValue(final Class type) {
+        final String fieldType = type.getSimpleName().toLowerCase();
+        if ("string".equals(fieldType)) {
+            return "StringValue";
+        }
+        if ("int".equals(fieldType)) {
+            return 100;
+        }
+        if ("long".equals(fieldType)) {
+            return 100_000_000;
+        }
+        if ("date".equals(fieldType)) {
+            return new Date();
+        }
+
+        return null;
+    }
 }
+
