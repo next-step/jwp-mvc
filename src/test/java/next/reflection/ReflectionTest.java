0147cba83ca7bde59b515d1dd907803d0d7c3ede
hongsii <hongsii.dev@gmail.com>
2019-08-08 21:48:11 +0900

    [요구사항-1] 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +14,13 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        // 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(it -> logger.debug("Field : {}", it.getName()));
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(it -> logger.debug("constructor : {}", it.getName()));
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(it -> logger.debug("method : {}", it.getName()));
     }
 
     @Test
