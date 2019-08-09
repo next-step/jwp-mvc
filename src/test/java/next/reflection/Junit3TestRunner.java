892fd22e0476e60443b6b9dd8790180d8aa7dde6
Juyoung <juy5790@outlook.com>
2019-08-09 09:55:48 +0900

    refactor #step1 요구사항2 요구사항 수정

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+
+    private static final String PREFIX_METHOD = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         Method[] declaredMethods = clazz.getDeclaredMethods();
         for (Method method : declaredMethods) {
-            method.invoke(clazz.newInstance());
+            if(method.getName().startsWith(PREFIX_METHOD)){
+                method.invoke(clazz.newInstance());
+            }
         }
     }
 }
