db4ea65280bb7510d50412827aa1839edce34225
leechang0423 <leechang0423@naver.com>
2019-08-09 13:57:14 +0900

    feat step1 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -2,8 +2,13 @@ package next.reflection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +47,33 @@ public class ReflectionTest {
 
     assertThat(student.getName()).isEqualTo("changjun");
     assertThat(student.getAge()).isEqualTo(31);
+  }
+
+  @Test
+  void argumentConstructorCreate()
+      throws IllegalAccessException, InstantiationException, InvocationTargetException {
+    Class<Question> clazz = Question.class;
+    for (Constructor constructor : clazz.getDeclaredConstructors()) {
+      Question question = (Question) constructor.newInstance(
+          Arrays.stream(constructor.getParameterTypes())
+              .map(type -> getTypeValue(type)).toArray());
+
+      printClassInfo(question);
+    }
+  }
+
+  private Object getTypeValue(Class clazz) {
+    Map<Class, Object> typeDefaultValue = new HashMap<>();
+    typeDefaultValue.put(String.class, "string");
+    typeDefaultValue.put(Date.class, new Date());
+    typeDefaultValue.put(long.class, 1L);
+    typeDefaultValue.put(int.class, 1);
+
+    return typeDefaultValue.get(clazz);
+  }
+
+  private void printClassInfo(Question question) {
+    logger.debug("info : {}", question.toString());
   }
 
 }
