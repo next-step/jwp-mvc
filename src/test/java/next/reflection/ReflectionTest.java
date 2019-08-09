8127496f65a328747e0f1d871e1f9a234cdce262
Juyoung <juy5790@outlook.com>
2019-08-09 09:32:42 +0900

    feat #step1 요구사항4 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.Arrays;
+import java.util.Date;
 
 public class ReflectionTest {
+
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
@@ -23,8 +23,25 @@ public class ReflectionTest {
     }
 
     @Test
+    public void privateFieldAccess() throws IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            if (field.getName().equals("name")) {
+                field.set(new Student(), "이름");
+            } else {
+                field.setInt(new Student(), 16);
+                logger.debug("field : {}", field);
+            }
+        }
+    }
+
+
+    @Test
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
+    public void constructor() {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
