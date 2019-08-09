b280439d2ae1effb79526dbf6ad24a10dc966e39
JeongHoon Cho <jeonghoon1107@gmail.com>
2019-08-09 13:34:01 +0900

    feat : step1 구현 (#11)

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,20 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException ignored) {}
+                });
     }
 }
