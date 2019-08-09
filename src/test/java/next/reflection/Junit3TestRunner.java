9e363deacf637acd798007c22b24fcd1a73ba5bc
Summer <jihanbaek805@gmail.com>
2019-08-09 13:21:29 +0900

    feat : step1 구현 (#5)

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,22 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            invokeIfMatches(method, clazz);
+        }
+    }
+
+    private void invokeIfMatches(Method method, Class<Junit3Test> clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        if(method.getName().startsWith("test")) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,22 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +25,21 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        // constructors
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        Arrays.stream(constructors)
+                .forEach(constructor -> logger.debug("constructor : {}", constructor));
+
+        // fields
+        Field[] fields = clazz.getDeclaredFields();
+        Arrays.stream(fields)
+                .forEach(field -> logger.debug("field : {}", field));
+
+        // methods
+        Method[] methods = clazz.getDeclaredMethods();
+        Arrays.stream(methods)
+                .forEach(method -> logger.debug("method : {}", method));
     }
 
     @Test
@@ -27,5 +54,49 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    public void constructorWithArguments() throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        Class<Question> clazz = Question.class;
+        Question question = clazz.getConstructor(String.class, String.class, String.class)
+                .newInstance("Summer", "Winter", "Spring");
+
+        assertEquals(question.getWriter(), "Summer");
+        assertEquals(question.getTitle(), "Winter");
+        assertEquals(question.getContents(), "Spring");
+    }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        Student student = new Student();
+
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "재성");
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 40);
+
+        assertEquals("재성", student.getName());
+        assertEquals(40, student.getAge());
+
+    }
+
+    @Test
+    public void componentScan() {
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        scanBy(reflections, Controller.class);
+        scanBy(reflections, Repository.class);
+        scanBy(reflections, Service.class);
+    }
+
+    private void scanBy(Reflections reflections, Class<? extends Annotation> annotationType) {
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(annotationType);
+        typesAnnotatedWith
+                .forEach(type -> logger.debug("type : {}", type.getName()));
     }
 }
