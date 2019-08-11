bd3eba4652b4cfb21038856807b9bd29869fd86f
korkorna <korkorna@gmail.com>
2019-08-11 09:33:09 +0900

    feat: 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -22,10 +24,39 @@ public class ReflectionTest {
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
+            printParameterTypes(parameterTypes);
+        }
+    }
+
+    private void printParameterTypes(Class[] parameterTypes) {
+        logger.debug("paramer length : {}", parameterTypes.length);
+        for (Class paramType : parameterTypes) {
+            logger.debug("param type : {}", paramType);
+        }
+    }
+    
+    @Test
+    @SuppressWarnings("rawtypes")
+    public void method() throws Exception {
+        Class<Question> clazz = Question.class;
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("name: " + method.getName());
+            logger.debug("modifier: " + method.getModifiers());
+            logger.debug("returntype: " + method.getReturnType());
+            printParameterTypes(method.getParameterTypes());
+
+        }
+    }
+
+    @Test
+    @SuppressWarnings("rawtypes")
+    public void fields() throws Exception {
+        Class<Question> clazz = Question.class;
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field: fields) {
+            logger.debug("name: " + field.getName());
+            logger.debug("type: " + field.getType());
         }
     }
 }
