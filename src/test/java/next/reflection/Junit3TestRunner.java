91a606ccc8d87b591dfa26313faff736baf29b60
Jinho Yim <jinhoyim@outlook.com>
2019-08-11 12:26:20 +0900

    test: 이름이 test로 시작하는 메서드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,27 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final List<Method> targetMethods = filterMethodsStartsWith(clazz, "test");
+        final Junit3Test junit3Test = clazz.newInstance();
+
+        for(Method method : targetMethods) {
+            method.invoke(junit3Test);
+        }
+    }
+
+    private List<Method> filterMethodsStartsWith(Class<?> clazz, String startsWith) {
+        final Method[] methods = clazz.getDeclaredMethods();
+        return Arrays.stream(methods)
+                .filter(m -> m.getName().startsWith(startsWith))
+                .collect(Collectors.toList());
     }
 }
