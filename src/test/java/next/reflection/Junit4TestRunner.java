8e2901959d286eb26497080d6c404db9ff0e3cae
korkorna <korkorna@gmail.com>
2019-08-11 13:56:00 +0900

    Step1 (#24)
    * feat: 클래스 정보 출력
    
    * feat: test로 시작하는 매소드 실행
    
    * feat: @Test 애너테이션 메소드 실행
    
    * private field에 값 할당
    
    * 인자를 가진 생성자의 인스터스 생성
    
    * feat: component scan

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Junit4TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit4TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method: methods) {
+            logger.debug("method name: " + method.getName());
+            logger.debug("get annotation(MyTest.class): " + method.getAnnotation(MyTest.class));
+            invoke(clazz, method);
+        }
+
+    }
+
+    private void invoke(Class<Junit4Test> clazz, Method method) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (method.isAnnotationPresent(MyTest.class)) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,13 +1,35 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    public static Question newQuestion() {
+        return new Question("홍길동", "홍길동전", "내용..");
+    }
 
     @Test
     public void showClass() {
@@ -22,10 +44,115 @@ public class ReflectionTest {
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
+            printParameterTypes(parameterTypes);
         }
     }
+
+    private void printParameterTypes(Class[] parameterTypes) {
+        logger.debug("paramer length : {}", parameterTypes.length);
+        for (Class paramType : parameterTypes) {
+            logger.debug("param type : {}", paramType);
+        }
+    }
+
+    @Test
+    @SuppressWarnings("rawtypes")
+    public void method() throws Exception {
+        Class<Question> clazz = Question.class;
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("name: " + method.getName());
+            logger.debug("modifier: " + method.getModifiers());
+            logger.debug("returntype: " + method.getReturnType());
+            printParameterTypes(method.getParameterTypes());
+
+        }
+    }
+
+    @Test
+    @SuppressWarnings("rawtypes")
+    public void fields() throws Exception {
+        Class<Question> clazz = Question.class;
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field: fields) {
+            logger.debug("name: " + field.getName());
+            logger.debug("type: " + field.getType());
+        }
+    }
+
+
+    //자바 Reflection API를 활용해 다음 Student 클래스의 name과 age 필드에 값을 할당한 후 getter 메소드를 통해 값을 확인한다.
+    @Test
+    public void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Student student = new Student();
+
+        Field nameField = getAccessibleField(clazz, "name");
+        nameField.set(student, "길동");
+
+        Field ageField = getAccessibleField(clazz, "age");
+        ageField.set(student, 20);
+
+        assertThat(student.getName()).isEqualTo("길동");
+        assertThat(student.getAge()).isEqualTo(20);
+
+    }
+
+    private Field getAccessibleField(Class<Student> clazz, String filedName) throws NoSuchFieldException {
+        Field nameField = clazz.getDeclaredField(filedName);
+        nameField.setAccessible(true);
+        return nameField;
+    }
+
+    // Qutions 클래스의 인스턴스를 자바 Reflection API를 활용해 Question 인스턴스를 생성한다.
+    @Test
+    public void create_class_no_constructor() throws IllegalAccessException, InstantiationException {
+        Class<Question> clazz = Question.class;
+
+        assertThrows(InstantiationException.class, () -> {
+            clazz.newInstance();
+        });
+    }
+
+    @Test
+    public void create_class() throws Exception {
+        Class<Question> clazz = Question.class;
+
+        Question question = newQuestion();
+        Question otherQuestion = null;
+
+        Constructor constructor = clazz.getDeclaredConstructor(String.class, String.class, String.class);
+        otherQuestion = (Question) constructor.newInstance(question.getWriter(), question.getTitle(), question.getContents());
+
+        assertThat(otherQuestion).isEqualTo(question);
+    }
+
+
+    // code.di.factory.example 패키지에서 @Controller, @Service, @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력하라.
+    @Test
+    public void componentScan() {
+        Reflections reflections = new Reflections(
+                "core.di.factory.example",
+                new TypeAnnotationsScanner(),
+                new SubTypesScanner());
+
+        Set<Class> findClasses = getTypesAnnotatedWith(reflections, Controller.class, Service.class, Repository.class);
+
+        for (Class clazz : findClasses) {
+            logger.debug(clazz.getName());
+        }
+    }
+
+    private Set<Class> getTypesAnnotatedWith(Reflections reflections, Class... annotations) {
+        Set<Class> findAnnotatedTypes = new HashSet<>();
+
+        for (Class annotation : annotations) {
+            findAnnotatedTypes.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+
+        return findAnnotatedTypes;
+    }
+
 }
