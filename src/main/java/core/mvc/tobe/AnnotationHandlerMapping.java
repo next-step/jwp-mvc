f361491f3505248effc2099eb671a13abaafaf6f
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-08-11 11:47:54 +0900

    # modify : 요구사항1 기능구현
    - AnnotationHandlerMapping : @Controller, @RequestMapping 어노테이션을 스캔하여 실행할 method정보를 url과 매핑하여 등록
    - HandlerExecution : 기능 수행할 객체와 method정보 등록, 실행 기능 추가

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,23 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -16,7 +27,43 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage
+                , new TypeAnnotationsScanner()
+                , new MethodAnnotationsScanner()
+                , new SubTypesScanner());
 
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        initializeHandlerExecutions(annotatedClasses);
+    }
+
+    private void initializeHandlerExecutions(Set<Class<?>> annotatedClasses) {
+        for (Class annotatedClass : annotatedClasses) {
+            scanMethods(annotatedClass);
+        }
+    }
+
+    private void scanMethods(Class annotatedClass) {
+        Method[] methods = annotatedClass.getDeclaredMethods();
+        for (Method method : methods) {
+            addHandlerExecution(annotatedClass, method, RequestMapping.class);
+        }
+    }
+
+    private void addHandlerExecution(Class<?> annotatedClass, Method method, Class<RequestMapping> annotationType) {
+        if (!method.isAnnotationPresent(annotationType)) {
+            return;
+        }
+
+        try {
+            RequestMapping requestMapping = method.getAnnotation(annotationType);
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
+            handlerExecutions.put(handlerKey, new HandlerExecution(annotatedClass.newInstance(), method));
+
+            logger.debug("Path : [{}], Controller : [{}]", requestMapping.value(), method.getDeclaringClass());
+
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        };
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
