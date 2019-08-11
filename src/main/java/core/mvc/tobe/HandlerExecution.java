c08b0b4360656208571f29112242f23946dda4c5
internet200q <internet200@qdillion.com>
2019-08-11 21:19:14 +0900

    2단계 MVC 구현

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,46 @@
 package core.mvc.tobe;
 
+import core.mvc.HandleView;
 import core.mvc.ModelAndView;
+import core.mvc.View;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
-    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    Class<?> clazz;
+    Method executionMethod;
+
+    public HandlerExecution(Class<?> clazz, Method executionMethod) {
+        this.clazz = clazz;
+        this.executionMethod = executionMethod;
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) {
+        Class<?> returnObjectType = executionMethod.getReturnType();
+
+        try {
+            Object returnObject = executionMethod.invoke(clazz.newInstance(), request, response);
+            if (returnObjectType.equals(String.class)) {
+                String viewName = (String) returnObject;
+                View views = new HandleView(viewName);
+                return new ModelAndView(views);
+            }
+
+            if (returnObjectType.equals(ModelAndView.class)) {
+                return (ModelAndView) returnObject;
+            }
+        }catch (IllegalAccessException e) {
+
+        } catch (InvocationTargetException e) {
+
+        } catch (InstantiationException e) {
+
+        }
+
         return null;
     }
 }
--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -1,14 +1,23 @@
 package next.controller;
 
+import core.annotation.web.RequestMapping;
 import core.db.DataBase;
 import core.mvc.asis.Controller;
+import core.mvc.asis.DispatcherServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeController implements Controller {
-    @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+@core.annotation.web.Controller
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    @RequestMapping("/")
+    public String indexMainPage(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        logger.debug("!!!!!!!!!!~~~~~~~~~!!!!!!!!!!");
         req.setAttribute("users", DataBase.findAll());
         return "home.jsp";
     }
