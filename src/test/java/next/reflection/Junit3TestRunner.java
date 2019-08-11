4db329f3775638e011a1c32e96a89c056987f909
changjun89 <leechang0423@naver.com>
2019-08-11 13:13:34 +0900

    refactor #step1 중첩 if문 메소드로 분리

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,20 +1,30 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
 
-  private static final String METHOD_PREFIX ="test";
+  private static final String METHOD_PREFIX = "test";
 
   @Test
   public void run() throws Exception {
     Class<Junit3Test> clazz = Junit3Test.class;
 
     for (Method method : clazz.getDeclaredMethods()) {
-      if (method.getName().startsWith(METHOD_PREFIX)) {
-        method.invoke(clazz.newInstance());
-      }
+      testMethodRun(clazz, method);
     }
+  }
+
+  private void testMethodRun(Class<Junit3Test> clazz, Method method)
+      throws IllegalAccessException, InvocationTargetException, InstantiationException {
+    if (isTestMethod(method)) {
+      method.invoke(clazz.newInstance());
+    }
+  }
+
+  private boolean isTestMethod(Method method) {
+    return method.getName().startsWith(METHOD_PREFIX);
   }
 }
