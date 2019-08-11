fa4a28267906962fbe090d54db95fb053cab1701
korkorna <korkorna@gmail.com>
2019-08-11 09:42:01 +0900

    feat: test로 시작하는 매소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+    public static final String INVOKE_START_METHOD_NAME = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method: methods) {
+            String methodName = method.getName();
+            logger.debug("method name: " + methodName);
+            invokeLikeMethodName(clazz, method, INVOKE_START_METHOD_NAME);
+        }
+    }
+
+    private void invokeLikeMethodName(Class<Junit3Test> clazz, Method method, String methodName) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (methodName.indexOf(methodName) > -1) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
