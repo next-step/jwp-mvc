f4e7606d5b7025e2c53e2fe0f5f5dd64c384e614
eldie <eldie@whitin.co.kr>
2019-08-12 17:13:19 +0900

    feat : AnnotationHandlerMappingTest 테스트 케이스 추가

--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,24 +1,50 @@
 package core.mvc.tobe;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import core.mvc.ModelAndView;
+
 public class AnnotationHandlerMappingTest {
+	private static final String BASE_PACKAGE = "core.mvc.tobe";
+	private static final String HTTP_METHOD = "GET";
+	private static final String REQUEST_URI = "/users/findUserId";
+	
     private AnnotationHandlerMapping handlerMapping;
 
     @BeforeEach
     public void setup() {
-        handlerMapping = new AnnotationHandlerMapping("core.mvc.tobe");
+        handlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
         handlerMapping.initialize();
     }
 
+    @DisplayName("handler 호출 테스트")
     @Test
     public void getHandler() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users/findUserId");
+        MockHttpServletRequest request = new MockHttpServletRequest(HTTP_METHOD, REQUEST_URI);
         MockHttpServletResponse response = new MockHttpServletResponse();
         HandlerExecution execution = handlerMapping.getHandler(request);
         execution.handle(request, response);
+    }
+    
+    @DisplayName("handler 호출 테스트 : return model 비교")
+    @Test
+    public void getHandlerModelAndView() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest(HTTP_METHOD, REQUEST_URI);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        HandlerExecution execution = handlerMapping.getHandler(request);
+        ModelAndView modelAndView = execution.handle(request, response);
+        assertThat(modelAndView).isNotNull();
+        
+        Map<String, Object> model = modelAndView.getModel();
+        assertThat(model).isNotNull();
+        assertThat(model.get(MyController.ATTRIBUTE_USER_ID_NAME)).isEqualTo(MyController.ATTRIBUTE_USER_ID_VALUE);
     }
 }
