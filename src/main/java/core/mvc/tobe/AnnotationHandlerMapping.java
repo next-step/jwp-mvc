ff3d1ccb632ada8170e728aef1531cf6c88bc4be
korkorna <korkorna@gmail.com>
2019-08-15 14:41:18 +0900

    feat: @RequestMapping() method 설정이 없을 경우 모든 HTTP method 지원해야한다.

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,17 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
-import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
-import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.sql.SQLOutput;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
@@ -36,16 +31,31 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         for (Class<?> controller : controllerScanner.getAnnotatedControllers()) {
             for (Method method : ReflectionUtils.getAllMethods(controller, RequestMapping.class)) {
                 RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                handlerExecutions.put(createHandlerKey(requestMapping), HandlerExecution.of(controller, method));
+
+                createHandlerKeys(requestMapping)
+                        .stream()
+                        .forEach(handlerKey -> handlerExecutions.put(handlerKey, HandlerExecution.of(controller, method)));
+
                 logger.debug("Add RequestMapping - {} {}.{}", requestMapping.method(), controller.getName(), method.getName());
             }
         }
     }
 
-    private HandlerKey createHandlerKey(RequestMapping requestMapping) {
+    private Set<HandlerKey> createHandlerKeys(RequestMapping requestMapping) {
         String path = requestMapping.value();
-        RequestMethod requestMethod = requestMapping.method();
-        return new HandlerKey(path, requestMethod);
+        RequestMethod[] requestMethods = getRequestMethods(requestMapping);
+
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(path, requestMethod))
+                .collect(Collectors.toSet());
+    }
+
+    private RequestMethod[] getRequestMethods(RequestMapping requestMapping) {
+        RequestMethod[] requestMethods = requestMapping.method();
+        if (requestMethods.length == 0) {
+            return RequestMethod.values();
+        }
+        return requestMapping.method();
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
