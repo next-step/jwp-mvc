6f0de21cda158087f9007b5f9e8a77515da4a8a5
hongsii <hongsii.dev@gmail.com>
2019-08-15 16:55:22 +0900

    [Step2:요구사항1] feat : 어노테이션 매핑 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,14 +1,25 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private Object[] basePackage;
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
@@ -16,7 +27,27 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Arrays.stream(basePackage)
+                .peek(it -> logger.info("Scan package [{}]", it))
+                .map(it -> new Reflections(it, new SubTypesScanner(), new TypeAnnotationsScanner()))
+                .flatMap(it -> it.getTypesAnnotatedWith(Controller.class).stream())
+                .flatMap(it -> Arrays.stream(it.getMethods()))
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> createHandlerExecution(method));
+    }
 
+    private void createHandlerExecution(Method method) {
+        Class<?> controllerClass = method.getDeclaringClass();
+        try {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String url = requestMapping.value();
+            RequestMethod requestMethod = requestMapping.method();
+            logger.info("Request mapping : {} [value = {}, method = {}]", controllerClass.getSimpleName(), url, requestMethod);
+            handlerExecutions.put(new HandlerKey(url, requestMethod), new HandlerExecution(method));
+        } catch (Exception e) {
+            logger.error("Failed to create HandlerExecution. class : {}, method : {}",
+                    controllerClass.getSimpleName(), method.getName(), e);
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
