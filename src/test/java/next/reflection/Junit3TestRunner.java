a920d3f200fe942dde26153525bb90b30495398f
KingCjy <tlssycks@gmail.com>
2020-06-07 11:20:57 +0900

    add: 요구사항 2 - test로 시작하는 메소드 실행 완성

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,34 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Set<Method> testMethods = new LinkedHashSet<>();
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (isTestMethod(method)) {
+                testMethods.add(method);
+            }
+        }
+
+        if(testMethods.size() >= 1) {
+            Object instance = clazz.getConstructor().newInstance();
+
+            for (Method method : testMethods) {
+                method.invoke(instance);
+            }
+        }
+    }
+
+    public boolean isTestMethod(Method method) {
+        return method.getName().contains("test");
     }
 }
