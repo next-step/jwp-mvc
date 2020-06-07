91a769175cf0a17021ac586e2a5b1086950e7aff
KingCjy <tlssycks@gmail.com>
2020-06-07 11:15:59 +0900

    add: 요구사항 1 - 클래스 정보 출력 완성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,8 +17,38 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+
+        for (Field field : clazz.getDeclaredFields()) {
+            String modifier = Modifier.toString(field.getModifiers());
+            String type = field.getType().toString();
+            String fieldName = field.getName();
+
+            logger.debug("Field: {} {} {}", modifier, type, fieldName);
+        }
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            String modifier = Modifier.toString(constructor.getModifiers());
+            String className = constructor.getDeclaringClass().getName();
+            String arguments = Arrays.stream(constructor.getParameterTypes())
+                    .map(targetParameter -> targetParameter.getName())
+                    .collect(Collectors.joining(", "));
+            logger.debug("Constructor: {} " + className + "({})", modifier, arguments);
+        }
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            String modifier = Modifier.toString(method.getModifiers());
+            String returnType = method.getReturnType().toString();
+            String methodName = method.getName();
+            String arguments = Arrays.stream(method.getParameterTypes())
+                    .map(targetParameter -> targetParameter.getName())
+                    .collect(Collectors.joining(", "));
+
+            logger.debug("Method: {} {} " + methodName + "({})", modifier, returnType, arguments);
+        }
+
         logger.debug(clazz.getName());
     }
+
 
     @Test
     @SuppressWarnings("rawtypes")
