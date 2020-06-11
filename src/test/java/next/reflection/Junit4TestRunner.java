054189d2303fffa2ec8cd40878f20758dc0e7bac
nokchax <nokchax@gmail.com>
2020-06-11 13:59:46 +0900

    study: run test method that has MyTest annotation using reflection's isAnnotationPresent()

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Junit4TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit4TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test 에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class)) // filter method that has MyTest annotation
+                .forEach(method -> runMethod(method, clazz));
+    }
+
+    private void runMethod(final Method method, final Class<Junit4Test> clazz) {
+        try {
+            method.invoke(clazz.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+            logger.error("Fail to run method {}", method.getName());
+        }
     }
 }
