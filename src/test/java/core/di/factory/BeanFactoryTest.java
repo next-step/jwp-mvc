a8b1b32c101960d85472c1686680a69cc9758b07
nokchax <nokchax@gmail.com>
2020-06-11 14:44:59 +0900

    study: scan classes that annotated with controller, service, repository

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -6,16 +6,23 @@ import core.annotation.Service;
 import core.annotation.web.Controller;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
+import next.reflection.ReflectionTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
+    private static final Logger logger = LoggerFactory.getLogger(BeanFactoryTest.class);
+
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -29,6 +36,7 @@ public class BeanFactoryTest {
     }
 
     @Test
+    @DisplayName("의존성 주입")
     public void di() throws Exception {
         QnaController qnaController = beanFactory.getBean(QnaController.class);
 
@@ -47,5 +55,15 @@ public class BeanFactoryTest {
             beans.addAll(reflections.getTypesAnnotatedWith(annotation));
         }
         return beans;
+    }
+
+    @Test
+    @DisplayName("컴포넌트 스캔")
+    @SuppressWarnings("unchecked")
+    void componentScan() {
+        Set<Class<?>> components = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+
+        components.forEach(component -> logger.debug("{}", component.toString()));
+        assertThat(components).hasSize(4);
     }
 }
