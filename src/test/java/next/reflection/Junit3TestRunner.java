781cc4421b68386a408a49cb5eed458445f258e6
nokchax <nokchax@gmail.com>
2020-06-11 13:49:02 +0900

    study: run test method that start with test using reflection

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Junit3TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        // Junit3Test 에서 test 로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> runMethod(method, clazz)); // filter that method name start with test
+    }
+
+    private void runMethod(Method method, Class<Junit3Test> clazz) {
+        try {
+            method.invoke(clazz.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+            logger.error("Fail to invoke method {}", method.getName());
+        }
     }
 }
