56fc9d0e1cec61e1354af060dea22de3feb4e746
ninjasul <ninjasul@gmail.com>
2020-06-11 18:26:52 +0900

    test: removed try-catch statement in lambda-function

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,12 +2,15 @@ package next.reflection;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Junit4TestRunner {
+    private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
 
     @Test
     @DisplayName("MyTest 어노테이션이 있는 메소드만 실행하는 테스트")
@@ -27,13 +30,17 @@ public class Junit4TestRunner {
 
         Arrays.stream(methods)
             .filter(method -> method.isAnnotationPresent(MyTest.class))
-            .forEach(method -> {
-                try {
-                    method.invoke(junit4Test);
-                }
-                catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            });
+            .forEach(method -> invokeMethod(junit4Test, method));
+    }
+
+    private Object invokeMethod(Junit4Test Junit4Test, Method method) {
+        try {
+            return method.invoke(Junit4Test);
+        }
+        catch (IllegalAccessException | InvocationTargetException e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
     }
 }
