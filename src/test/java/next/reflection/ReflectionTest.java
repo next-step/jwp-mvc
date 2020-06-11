97d60adf1b7804428a23b4e0b10150d7560961c7
nokchax <nokchax@gmail.com>
2020-06-11 14:17:12 +0900

    study: set private field value using reflection

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,5 +1,6 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +13,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
+    @DisplayName("Question 클래스의 필드, 생성자, 메소드 를 모두 출력")
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
@@ -92,5 +96,26 @@ public class ReflectionTest {
         return origin.toString()
                 .replace("[", "")
                 .replace("]", "");
+    }
+
+    @Test
+    @DisplayName("private 필드에 값 할당하기")
+    void injectFieldValue() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+
+        Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+
+        Student student = new Student();
+        nameField.set(student, "kwang");
+        ageField.set(student, -25);
+
+        assertThat(student.getAge()).isEqualTo(-25);
+        assertThat(student.getName()).isEqualTo("kwang");
+        logger.debug("{}", student);
     }
 }
