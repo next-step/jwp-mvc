2080a7151858860d6b9fe611ae564e7a0d9a68b1
nokchax <nokchax@gmail.com>
2020-06-12 16:27:44 +0900

    test: initialize reflections

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,16 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ConfigurationBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +22,16 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(
+                basePackage,
+                new SubTypesScanner(false),
+                new TypeAnnotationsScanner()
+        );
 
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        System.out.println("before");
+        controllers.forEach(System.out::println);
+        System.out.println("after");
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,22 +1,14 @@
 package core.mvc.tobe;
 
-import core.annotation.web.Controller;
 import core.db.DataBase;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.ReflectionUtils;
-import org.reflections.Reflections;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import java.lang.reflect.AnnotatedElement;
-import java.util.Set;
-import java.util.function.Predicate;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 public class AnnotationHandlerMappingTest {
     private static final String BASE_PACKAGE = "core.mvc.tobe";
@@ -57,8 +49,12 @@ public class AnnotationHandlerMappingTest {
     @Test
     @DisplayName("initialize 를 하면 해당 패키지 내분에 존재하는 @Controller 가 붙은 클래스를 찾아서 url 에 맞춰 초기화한다")
     void initialize() {
+/*
         Reflections reflections = new Reflections(BASE_PACKAGE);
+//        reflections.getSubTypesOf(Object.class);
 
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        controllers.forEach(System.out::println);
+*/
     }
 }
