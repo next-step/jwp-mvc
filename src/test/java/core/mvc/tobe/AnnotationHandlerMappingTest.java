34f4d45e6c9d90462c0046fcccf5dee28b0bd624
nokchax <nokchax@gmail.com>
2020-06-12 11:08:36 +0900

    feat: overall flow check, temporal commit

--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,20 +1,30 @@
 package core.mvc.tobe;
 
+import core.annotation.web.Controller;
 import core.db.DataBase;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.lang.reflect.AnnotatedElement;
+import java.util.Set;
+import java.util.function.Predicate;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 public class AnnotationHandlerMappingTest {
+    private static final String BASE_PACKAGE = "core.mvc.tobe";
     private AnnotationHandlerMapping handlerMapping;
 
     @BeforeEach
     public void setup() {
-        handlerMapping = new AnnotationHandlerMapping("core.mvc.tobe");
+        handlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
         handlerMapping.initialize();
     }
 
@@ -42,5 +52,13 @@ public class AnnotationHandlerMappingTest {
         MockHttpServletResponse response = new MockHttpServletResponse();
         HandlerExecution execution = handlerMapping.getHandler(request);
         execution.handle(request, response);
+    }
+
+    @Test
+    @DisplayName("initialize 를 하면 해당 패키지 내분에 존재하는 @Controller 가 붙은 클래스를 찾아서 url 에 맞춰 초기화한다")
+    void initialize() {
+        Reflections reflections = new Reflections(BASE_PACKAGE);
+
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
     }
 }
