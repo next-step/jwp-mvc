04497b8cec031b4802accc460509c9e68fd670e9
LeeSeungHee <palmseung@gmail.com>
2020-06-13 08:05:10 +0900

    Test (requirement-2) : modify method to run Junit3TestRunner.run()

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
+        //given
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
     }
 }
