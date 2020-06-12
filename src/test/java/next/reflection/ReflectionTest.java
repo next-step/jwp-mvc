227a7c17a0582a52d011eebca35cedda21e63837
LeeSeungHee <palmseung@gmail.com>
2020-06-13 07:28:14 +0900

    Test (requirement-1) : add test to get all fields name using Reflection

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,33 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Reflection - 모든 Fields 반환")
     @Test
-    public void showClass() {
+    public void showClass_Fields() {
+        //given
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        //when
+        Field[] declaredFields = clazz.getDeclaredFields();
+
+        //then
+        assertThat(declaredFields.length).isEqualTo(6);
+        IntStream.range(0, declaredFields.length)
+                .forEach(i -> System.out.println("Field : " + declaredFields[i]));
     }
 
     @Test
