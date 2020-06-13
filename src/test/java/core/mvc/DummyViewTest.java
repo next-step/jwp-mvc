b34fb921f0aa9cb2bf0fb1ac2ccdf9c275a8e52b
nokchax <nokchax@gmail.com>
2020-06-14 01:45:14 +0900

    test: DummyView equals test

---
+++ src/test/java/core/mvc/DummyViewTest.java
@@ -0,0 +1,17 @@
+package core.mvc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("더미 뷰")
+class DummyViewTest {
+
+    @Test
+    @DisplayName("동치성 테스트")
+    void equals() {
+        assertThat(DummyView.INSTANCE)
+                .isEqualTo(DummyView.INSTANCE);
+    }
+}
