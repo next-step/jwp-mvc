817fd6f6540aa28715f83f06545ff92cd63eea3b
kjs4395 <kjs4395@gmail.com>
2020-06-15 11:40:44 +0900

    feat : 요구사항 1 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +18,10 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        constructorInfo(clazz);
+        fieldInfo(clazz);
+        methodInfo(clazz);
     }
 
     @Test
@@ -26,6 +35,41 @@ public class ReflectionTest {
             for (Class paramType : parameterTypes) {
                 logger.debug("param type : {}", paramType);
             }
+        }
+    }
+
+    private void fieldInfo(Class clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        logger.debug(" Field info =====================");
+        for(Field field : fields) {
+            String modifier = Modifier.toString(field.getModifiers());
+            logger.debug("{} field : {} {} {}",clazz.getName(), modifier, field.getType().toGenericString() ,field.getName());
+        }
+    }
+
+    private void constructorInfo(Class clazz) {
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+
+        logger.debug(" Constructor info =====================");
+        for(Constructor constructor : constructors) {
+            String modifier = Modifier.toString(constructor.getModifiers());
+            String parameterType = Arrays.stream(constructor.getParameterTypes())
+                    .map(Class::toGenericString)
+                    .collect(Collectors.joining(","));
+
+            logger.debug("Modifier :{} , parameters: {} , Name : {}",modifier, parameterType, constructor.getName());
+        }
+    }
+
+    private void methodInfo(Class clazz) {
+        Method[] methods = clazz.getDeclaredMethods();
+        logger.debug(" Method info =====================");
+        for(Method method : methods) {
+            String parameterType = Arrays.stream(method.getParameterTypes())
+                    .map(Class::toGenericString)
+                    .collect(Collectors.joining(","));
+
+            logger.debug("Modifier : {} , parameters : {}, Name : {} ", Modifier.toString(method.getModifiers()), parameterType, method.getName());
         }
     }
 }
