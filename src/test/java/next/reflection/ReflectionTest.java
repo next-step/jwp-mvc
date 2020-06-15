3e267709df9d9d7a95cbe658a7b2c6a9557483f7
kjs4395 <kjs4395@gmail.com>
2020-06-15 15:49:34 +0900

    feat : 요구사항4 - 값 할당 test

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -11,6 +11,8 @@ import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -38,12 +40,38 @@ public class ReflectionTest {
         }
     }
 
+    @Test
+    public void privateFieldAccess() {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+        try {
+            Student student = clazz.newInstance();
+            Field nameField = clazz.getDeclaredField("name");
+            Field ageField = clazz.getDeclaredField("age");
+
+            nameField.setAccessible(true);
+            ageField.setAccessible(true);
+
+            nameField.set(student, "지선");
+            ageField.set(student, 29);
+
+            assertEquals(student.getAge(), 29);
+            assertEquals(student.getName(), "지선");
+        } catch (InstantiationException e) {
+            logger.error("No exist default constructor");
+        } catch (IllegalAccessException e) {
+            logger.error("not access private field");
+        } catch (NoSuchFieldException e) {
+            logger.error("Not Found Field");
+        }
+    }
+
     private void fieldInfo(Class clazz) {
         Field[] fields = clazz.getDeclaredFields();
         logger.debug(" Field info =====================");
-        for(Field field : fields) {
+        for (Field field : fields) {
             String modifier = Modifier.toString(field.getModifiers());
-            logger.debug("{} field : {} {} {}",clazz.getName(), modifier, field.getType().toGenericString() ,field.getName());
+            logger.debug("{} field : {} {} {}", clazz.getName(), modifier, field.getType().toGenericString(), field.getName());
         }
     }
 
@@ -51,20 +79,20 @@ public class ReflectionTest {
         Constructor[] constructors = clazz.getDeclaredConstructors();
 
         logger.debug(" Constructor info =====================");
-        for(Constructor constructor : constructors) {
+        for (Constructor constructor : constructors) {
             String modifier = Modifier.toString(constructor.getModifiers());
             String parameterType = Arrays.stream(constructor.getParameterTypes())
                     .map(Class::toGenericString)
                     .collect(Collectors.joining(","));
 
-            logger.debug("Modifier :{} , parameters: {} , Name : {}",modifier, parameterType, constructor.getName());
+            logger.debug("Modifier :{} , parameters: {} , Name : {}", modifier, parameterType, constructor.getName());
         }
     }
 
     private void methodInfo(Class clazz) {
         Method[] methods = clazz.getDeclaredMethods();
         logger.debug(" Method info =====================");
-        for(Method method : methods) {
+        for (Method method : methods) {
             String parameterType = Arrays.stream(method.getParameterTypes())
                     .map(Class::toGenericString)
                     .collect(Collectors.joining(","));
