788fefdb0d47565219a6b6f5c5a8a864e0790b25
혜윰 <31656707+hyeyoom@users.noreply.github.com>
2020-06-16 08:40:21 +0900

    1단계 - 자바 reflection (#88)
    * test: 요구사항 1 - 클래스 정보 출력
    
    * test: 요구사항 2 - test로 시작하는 메소드 실행
    
    * test: 요구사항 3 - @Test 애노테이션 메소드 실행
    
    * test: 요구사항 4 - private field에 값 할당
    
    * test: 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
    
    * test: 요구사항 6 - component scan

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,44 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class Junit4TestRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = getMethods(clazz);
+        final Constructor<Junit4Test> constructor = determineNonArgsConstructor(clazz);
+        final Junit4Test instance = constructor.newInstance();
+
+        for (Method method : methods) {
+            if (isAnnotatedWithMyTest(method)) {
+                method.invoke(instance);
+            }
+        }
+    }
+
+    private Method[] getMethods(Class<?> clazz) {
+        return clazz.getDeclaredMethods();
+    }
+
+    private boolean isAnnotatedWithMyTest(Method method) {
+        return method.isAnnotationPresent(MyTest.class);
+    }
+
+    private Constructor<Junit4Test> determineNonArgsConstructor(Class<Junit4Test> clazz) {
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterCount() == 0) {
+                return constructor;
+            }
+        }
+        return null;
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,69 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
+import java.lang.reflect.*;
+import java.util.Date;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("클래스의 정보를 투머치하게 보여줍시다")
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        for (Field field : clazz.getDeclaredFields()) {
+            printPrettified(field);
+        }
+
+        logger.info("=======================");
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            printPrettified(constructor);
+        }
+
+        logger.info("=======================");
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            printPrettified(method);
+        }
+
+    }
+
+    private void printPrettified(Field field) {
+        logger.debug("Field -------------------------");
+        logger.debug("field type: {}", field.getType());
+        logger.debug("field name: {}", field.getName());
+        logger.debug("modifier  : {}", Modifier.toString(field.getModifiers()));
+        logger.debug("-------------------------------");
+    }
+
+    private void printPrettified(Constructor<?> constructor) {
+        logger.debug("Constructor -------------------");
+        final Class[] parameterTypes = constructor.getParameterTypes();
+        logger.debug("constructor modifier: {}", Modifier.toString(constructor.getModifiers()));
+        logger.debug("param length : {}", parameterTypes.length);
+        for (Class paramType : parameterTypes) {
+            logger.debug("param type : {}", paramType);
+        }
+        logger.debug("-------------------------------");
+    }
+
+    private void printPrettified(Method method) {
+        logger.debug("Method ------------------------");
+        logger.debug("method name: {}", method.getName());
+        logger.debug("method modifier: {}", Modifier.toString(method.getModifiers()));
+        logger.debug("method param count: {}", method.getParameterCount());
+        for (Parameter parameter : method.getParameters()) {
+            logger.debug("param type: {}", parameter.getType());
+            logger.debug("param name: {}", parameter.getName());
+        }
+        logger.debug("-------------------------------");
     }
 
     @Test
@@ -27,5 +78,50 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @DisplayName("private 필드에 접근해봅시당")
+    @Test
+    public void privateFieldAccess() throws Exception {
+        final Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+        final Student student = new Student();
+        for (Field field : clazz.getDeclaredFields()) {
+            injectValue(student, field, determineValueByFieldName(field));
+        }
+        logger.debug("result: {}", student);
+    }
+
+    private void injectValue(Student target, Field field, Object value) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(target, value);
+    }
+
+    private Object determineValueByFieldName(Field field) {
+        final String name = field.getName();
+        if (name.startsWith("name")) {
+            return "이름 아닌 이름 같은거";
+        } else if (name.startsWith("age")) {
+            return 100;
+        }
+        return null;
+    }
+
+    @DisplayName("인자가 있는 생성자를 이용해 인스턴스를 생성해봐요!")
+    @Test
+    public void test_args_constructor() throws Exception {
+        final Class<Question> clazz = Question.class;
+        final Class<?>[] parameterTypes = {
+                long.class, String.class, String.class, String.class, Date.class, int.class
+        };
+        final Constructor<Question> constructor = clazz.getDeclaredConstructor(parameterTypes);
+        final Question question = constructor.newInstance(
+                1L,
+                "Chwon",
+                "질문이 있습니다!",
+                "오늘 출근 전에 아침을 사가고 싶은데 뭘 먹으면 좋을까요!",
+                new Date(System.currentTimeMillis()),
+                0);
+        logger.debug("question: {}", question);
     }
 }
