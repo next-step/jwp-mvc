fee98eb72e08cf44aa8e0ddcedd11f709491a99e
iltaek <iltaekkwon@gmail.com>
2020-06-15 20:22:04 +0900

    feat($test): 요구사항 1 - 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,60 @@
 package next.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-
 public class ReflectionTest {
+
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("Fields===");
+        logFields(clazz);
+
+        logger.debug("Constructors===");
+        logConstructors(clazz);
+
+        logger.debug("Methods===");
+        logMethods(clazz);
+    }
+
+    private void logMethods(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredMethods())
+            .map(method -> method.getName() + "(" + getParameters(method) + ")")
+            .forEach(logger::debug);
+    }
+
+    private String getParameters(Method method) {
+        return Arrays.stream(method.getParameters())
+            .map(param -> param.getType().getName() + " " + param.getName())
+            .collect(Collectors.joining(", "));
+    }
+
+    private void logConstructors(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredConstructors())
+            .map(con -> con.getName() + "(" + getParameters(con) + ")")
+            .forEach(logger::debug);
+    }
+
+    private void logFields(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredFields())
+            .map(field -> field.getType().getName() + " " + field.getName())
+            .forEach(logger::debug);
+    }
+
+    private String getParameters(Constructor constructor) {
+        return Arrays.stream(constructor.getParameters())
+            .map(param -> param.getType().getName() + " " + param.getName())
+            .collect(Collectors.joining(", "));
     }
 
     @Test
