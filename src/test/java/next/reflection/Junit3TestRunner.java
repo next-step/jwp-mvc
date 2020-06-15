3272d8ccdd4d45de938160deb87262ddd9484f9b
hyeyoom <neoul_chw@icloud.com>
2020-06-16 07:54:36 +0900

    test: 요구사항 2 - test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,38 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = getMethods(clazz);
+        for (Method method : methods) {
+            if (startsWithTest(method.getName())) {
+                final Constructor<Junit3Test> constructor = determineNonArgsConstructor(clazz);
+                final Junit3Test test = constructor.newInstance();
+                method.invoke(test);
+            }
+        }
+    }
+
+    private Method[] getMethods(Class<?> clazz) {
+        return clazz.getDeclaredMethods();
+    }
+
+    private boolean startsWithTest(String value) {
+        return value != null && value.startsWith("test");
+    }
+
+    private Constructor<Junit3Test> determineNonArgsConstructor(Class<Junit3Test> clazz) {
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterCount() == 0) {
+                return constructor;
+            }
+        }
+        return null;
     }
 }
