2d4a5c231358e60d2b04d272359f8053154331d5
kjs4395 <kjs4395@gmail.com>
2020-06-16 19:19:35 +0900

    feat :annotationHandlerMapping / handlerExecution test 통과하게 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -15,7 +20,25 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
+    /**
+     * 1. base 패키지에 있는 class 들 scan
+     * 2. class 정보 별로 handlerkey / handlerExecution을 만들어 handlerExecutions 에 넣음
+     *
+     *  HadlerKey = url(String) , requestMethod (GET,POST,PUT,DELETE....)
+     *  HandleExecutions handle구현 (method invoke?)
+     */
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage, new TypeAnnotationsScanner());
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class,true);
+
+        for(Class<?> controller : controllers) {
+            this.handlerExecutions = Arrays.stream(controller.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toMap(method -> {
+                        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                        return new HandlerKey(requestMapping.value(), requestMapping.method());
+                        }, method -> new HandlerExecution(controller,method)));
+        }
 
     }
 
--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,19 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+
+    private final Class<?> clazz;
+    private final Method method;
+
+    public HandlerExecution(Class<?> clazz,Method method) {
+        this.clazz = clazz;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(clazz.newInstance(), request,response);
     }
 }
