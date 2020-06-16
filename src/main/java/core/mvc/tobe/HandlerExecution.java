204b6091726d2736462d1f1c3da1c693b3f70c15
hyeyoom <neoul_chw@icloud.com>
2020-06-17 00:11:08 +0900

    feat: reflection한 메소드를 실행하기 위한 최소한의 필드를 추가!

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,8 +4,18 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
         return null;
     }
