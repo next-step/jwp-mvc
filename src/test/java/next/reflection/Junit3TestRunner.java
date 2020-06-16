b0029f20df72437395b3114e58b3cc1354d8b8b0
Iltaek Kwon <iltaekkwon@gmail.com>
2020-06-16 18:28:43 +0900

    1단계 - 자바 reflection (#90)
    * feat($test): 요구사항 1 - 클래스 정보 출력
    
    * feat($test): 요구사항 2 - test로 시작하는 메소드 실행
    
    * feat($test): 요구사항 3 - @Test 애노테이션 메소드 실행
    
    * feat($test): 요구사항 4 - private field에 값 할당
    
    * feat($test): 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
    
    * feat($test): 요구사항 6 - component scan

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,17 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,94 @@
 package next.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-
 public class ReflectionTest {
+
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    void createInstanceWithParameter() throws Exception {
+        Class<Question> clazz = Question.class;
+
+        // String writer, String title, String contents
+        Constructor<?> constructor1 = clazz.getConstructor(String.class, String.class, String.class);
+        Question qustion1 = (Question) constructor1.newInstance("javajigi", "JWP", "Hi, guys");
+        logger.debug("{} attended {} class and said {}", qustion1.getWriter(), qustion1.getTitle(), qustion1.getContents());
+
+        // long questionId, String writer, String title, String contents, Date createdDate, int countOfComment
+        Constructor<?> constructor2 = clazz.getConstructor(long.class, String.class, String.class, String.class, Date.class, int.class);
+        Question qustion2 = (Question) constructor2.newInstance(1L, "conan", "JWP-MVC", "Hello, World!", new Date(), 0);
+        logger.debug("{} attended {} class and said {}", qustion2.getWriter(), qustion2.getTitle(), qustion2.getContents());
+    }
+
+    @Test
+    void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Student student = new Student();
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "iltaek");
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 20);
+
+        logger.debug("student {}'s age is {}", student.getName(), student.getAge());
+    }
 
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("Fields===");
+        logFields(clazz);
+
+        logger.debug("Constructors===");
+        logConstructors(clazz);
+
+        logger.debug("Methods===");
+        logMethods(clazz);
+    }
+
+    private void logMethods(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredMethods())
+            .map(method -> method.getName() + "(" + getParameters(method) + ")")
+            .forEach(logger::debug);
+    }
+
+    private String getParameters(Method method) {
+        return Arrays.stream(method.getParameters())
+            .map(param -> param.getType().getName() + " " + param.getName())
+            .collect(Collectors.joining(", "));
+    }
+
+    private void logConstructors(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredConstructors())
+            .map(con -> con.getName() + "(" + getParameters(con) + ")")
+            .forEach(logger::debug);
+    }
+
+    private void logFields(Class<Question> clazz) {
+        Arrays.stream(clazz.getDeclaredFields())
+            .map(field -> field.getType().getName() + " " + field.getName())
+            .forEach(logger::debug);
+    }
+
+    private String getParameters(Constructor constructor) {
+        return Arrays.stream(constructor.getParameters())
+            .map(param -> param.getType().getName() + " " + param.getName())
+            .collect(Collectors.joining(", "));
     }
 
     @Test
