10b12fc7ea2e93a90d6dce6ab9cc1dfa373cdafa
LeeSeungHee <palmseung@gmail.com>
2020-06-17 03:21:04 +0900

    Test (handlerKey) : add test to create handler when requestMethod is null

--- src/main/java/core/mvc/tobe/handler/HandlerKey.java
+++ src/main/java/core/mvc/tobe/handler/HandlerKey.java
@@ -25,6 +25,10 @@ public class HandlerKey {
         return new HandlerKey(requestUri, method);
     }
 
+    public RequestMethod[] getRequestMethod() {
+        return requestMethod;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
