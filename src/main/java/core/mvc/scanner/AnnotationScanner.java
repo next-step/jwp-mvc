1286e426505b78625dcd8da318fc44e1e79e1201
hyeyoom <neoul_chw@icloud.com>
2020-06-17 00:06:18 +0900

    feat: 어노테이션을 스캔하는 모듈 추가

--- src/main/java/core/mvc/scanner/AnnotationScanner.java
+++ src/main/java/core/mvc/scanner/AnnotationScanner.java
@@ -1,16 +1,37 @@
 package core.mvc.scanner;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class AnnotationScanner {
+public final class AnnotationScanner {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationScanner.class);
 
     public static Set<Class<?>> loadClasses(Class<? extends Annotation> annotation, Object... basePackages) {
-        return null;
+        final Reflections reflections = new Reflections(
+                ConfigurationBuilder
+                        .build(basePackages)
+                        .setScanners(
+                                new SubTypesScanner(false),
+                                new TypeAnnotationsScanner()
+                        )
+        );
+        return reflections.getTypesAnnotatedWith(annotation);
     }
 
     public static Set<Method> loadMethods(Class<?> targetClass, Class<? extends Annotation> annotation) {
-        return null;
+        return Arrays.stream(targetClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(annotation))
+                .collect(Collectors.toSet());
     }
 }
