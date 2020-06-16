99b2dbb44c913ae0b5dea9e9a18cd9a1dbce257f
parkeeseul <eeseul.p@gmail.com>
2020-06-17 00:44:25 +0900

    fix: HandlerExecution 수정

---
+++ src/main/java/core/mvc/HandlerExecution.java
@@ -0,0 +1,19 @@
+package core.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+public class HandlerExecution {
+    private Object object;
+    private Method method;
+
+    public HandlerExecution(Object object, Method method) {
+        this.method = method;
+        this.object = object;
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return (ModelAndView) method.invoke(this.object, request, response);
+    }
+}
