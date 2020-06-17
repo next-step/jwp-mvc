e552d83f1117cebc8e4061439a60b04deddddc7f
kjs4395 <kjs4395@gmail.com>
2020-06-17 09:05:53 +0900

    Step2 (#92)
    * feat :annotationHandlerMapping / handlerExecution test 통과하게 구현
    
    * refactor : controller가져오는 부분 method로 변경
    
    * feat : as-is to-be방식 공존하도록 변경

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -15,7 +20,24 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
+    /**
+     * 1. base 패키지에 있는 class 들 scan
+     * 2. class 정보 별로 handlerkey / handlerExecution을 만들어 handlerExecutions 에 넣음
+     * <p>
+     * HadlerKey = url(String) , requestMethod (GET,POST,PUT,DELETE....)
+     * HandleExecutions handle구현 (method invoke?)
+     */
     public void initialize() {
+        Set<Class<?>> controllers = this.getControllers();
+
+        for (Class<?> controller : controllers) {
+            this.handlerExecutions.putAll(Arrays.stream(controller.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toMap(method -> {
+                        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                        return new HandlerKey(requestMapping.value(), requestMapping.method());
+                    }, method -> new HandlerExecution(controller, method))));
+        }
 
     }
 
@@ -23,5 +45,16 @@ public class AnnotationHandlerMapping {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    public boolean containsExecution(HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
+        return this.handlerExecutions.containsKey(new HandlerKey(requestUri, rm));
+    }
+
+    private Set<Class<?>> getControllers() {
+        Reflections reflections = new Reflections(basePackage, new TypeAnnotationsScanner());
+        return reflections.getTypesAnnotatedWith(Controller.class, true);
     }
 }
