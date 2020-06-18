79ffa1866a0f1e7a9d322e2b23c8893efa1b4219
LeeSeungHee <palmseung@gmail.com>
2020-06-18 15:18:26 +0900

    Fix : fix words used in custom exception

--- src/main/java/core/mvc/tobe/handlermapping/exception/ControllerScanException.java
+++ src/main/java/core/mvc/tobe/handlermapping/exception/ControllerScanException.java
@@ -2,7 +2,7 @@ package core.mvc.tobe.handlermapping.exception;
 
 public class ControllerScanException extends RuntimeException{
     private static final String MESSAGE_CONTROLLER_SCAN
-            = "Please check where the base package has any controller with Controller annotation";
+            = "Please check whether if the base package has any controller with Controller annotation";
 
     public ControllerScanException(Throwable cause) {
         super(MESSAGE_CONTROLLER_SCAN, cause);
