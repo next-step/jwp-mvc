e06f42d8d8487d4dda460887fb0e861a479eda17
hyeyoom <neoul_chw@icloud.com>
2020-06-19 02:36:42 +0900

    test: 학습용 테스트

---
+++ src/test/java/study/test/TestUserController.java
@@ -0,0 +1,39 @@
+package study.test;
+
+import core.annotation.web.PathVariable;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.mvc.tobe.TestUser;
+import core.mvc.view.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestUserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestUserController.class);
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ModelAndView create_string(String userId, String password) {
+        logger.debug("userId: {}, password: {}", userId, password);
+        return null;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ModelAndView create_int_long(long id, int age) {
+        logger.debug("id: {}, age: {}", id, age);
+        return null;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ModelAndView create_javabean(TestUser testUser) {
+        logger.debug("testUser: {}", testUser);
+        return null;
+    }
+
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public ModelAndView show_pathvariable(@PathVariable long id) {
+        logger.debug("userId: {}", id);
+        return null;
+    }
+}
