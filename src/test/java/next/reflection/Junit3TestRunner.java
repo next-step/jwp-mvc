76c5e4e6717a5feb7b3b541774cc3a16d052b413
wbluke <pwb0127@gmail.com>
2020-06-19 22:02:32 +0900

    test: Reflection 학습테스트 - Junit3에서는 이름이 test로 시작하는 메소드만 자동 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,34 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit3TestRunner {
+
+    private static final String METHOD_PREFIX_FOR_EXECUTING_TEST = "test";
+
+    @DisplayName("Junit3에서는 메소드 이름이 'test'로 시작해야 테스트를 자동 실행한다.")
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(this::hasTestingPrefix)
+                .collect(Collectors.toList());
+
+        Junit3Test testClassInstance = clazz.newInstance();
+        for (Method testMethod : testMethods) {
+            testMethod.invoke(testClassInstance);
+        }
+    }
+
+    private boolean hasTestingPrefix(Method method) {
+        String methodName = method.getName();
+        return methodName.startsWith(METHOD_PREFIX_FOR_EXECUTING_TEST);
     }
 }
