197e61aa6e36e7b72d65ad50c69781663c0095df
wbluke <pwb0127@gmail.com>
2020-06-19 21:47:55 +0900

    test: Reflection 학습테스트 - 클래스 정보 출력하기

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,30 +1,59 @@
 package next.reflection;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
+@Slf4j
 public class ReflectionTest {
-    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Class의 필드, 생성자, 메소드 정보 보여주기")
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        log.debug(clazz.getName());
+
+        for (Field field : clazz.getDeclaredFields()) {
+            log.debug("field : name={}, type={}", field.getName(), field.getType());
+        }
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            log.debug("constructor : name={}", constructor.getName());
+
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                log.debug("constructor parameterType : name={}, type={}", parameterType.getName(), parameterType.getTypeName());
+            }
+        }
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            log.debug("method : name={}", method.getName());
+
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                log.debug("method parameterType : name={}, type={}", parameterType.getName(), parameterType.getTypeName());
+            }
+
+            Class<?> returnType = method.getReturnType();
+            log.debug("method returnType : type={}", returnType.getName());
+        }
     }
 
     @Test
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
+    public void constructor() {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
+
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
+            log.debug("paramer length : {}", parameterTypes.length);
             for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+                log.debug("param type : {}", paramType);
             }
         }
     }
