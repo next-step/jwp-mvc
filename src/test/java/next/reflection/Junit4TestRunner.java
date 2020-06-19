2571e83573732d0d5f1b8adc66e382a788274a3a
wbluke <pwb0127@gmail.com>
2020-06-19 22:15:26 +0900

    test: Reflection 학습테스트 - Junit4에서는 특정 어노테이션이 붙은 메소드만 자동 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,33 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit4TestRunner {
+
+    private static final Class<MyTest> ANNOTATION_FOR_EXECUTING_TEST = MyTest.class;
+
+    @DisplayName("Junit4에서는 어노테이션을 기준으로 테스트를 자동 실행한다.")
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(this::hasTestingAnnotation)
+                .collect(Collectors.toList());
+
+        Junit4Test testClassInstance = clazz.newInstance();
+        for (Method testMethod : testMethods) {
+            testMethod.invoke(testClassInstance);
+        }
+    }
+
+    private boolean hasTestingAnnotation(Method method) {
+        return method.isAnnotationPresent(ANNOTATION_FOR_EXECUTING_TEST);
     }
 }
