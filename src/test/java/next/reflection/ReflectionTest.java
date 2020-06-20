a9678963a0101873a7e9e2a0f10a5c7524e3200b
liquidjoo <liquidjoo@gmail.com>
2020-06-20 17:53:12 +0900

    feat : 요구사항 1 - 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,29 @@
 package next.reflection;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    private Class<Question> clazz;
+
+    @BeforeEach
+    void setUp() {
+        clazz = Question.class;
+    }
+
     @Test
     public void showClass() {
-        Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        field();
+        allConstructor();
+        method();
     }
 
     @Test
@@ -28,4 +39,29 @@ public class ReflectionTest {
             }
         }
     }
+
+    @Test
+    void allConstructor() {
+        Constructor[] constructors = clazz.getConstructors();
+        for (final Constructor constructor : constructors) {
+            logger.debug("constructor : {}", constructor.toString());
+        }
+    }
+
+    @Test
+    void field() {
+        Field[] fields = clazz.getDeclaredFields();
+        for (final Field declaredField : fields) {
+            logger.debug("field : {}", declaredField.toString());
+        }
+    }
+
+    @Test
+    void method() {
+        Method[] methods = clazz.getMethods();
+        for (final Method method : methods) {
+            logger.debug("method : {}", method.toString());
+        }
+    }
+
 }
