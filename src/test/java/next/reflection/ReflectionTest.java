2731528810cc522d38485be00ff44c5136d0b04a
gch01410 <gch01410@naver.com>
2020-06-21 01:57:39 +0900

    chore: Question 클래스의 정보출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,27 +5,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public class ReflectionTest {
+
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    private Class<Question> clazz = Question.class;
 
     @Test
     public void showClass() {
-        Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
     }
 
     @Test
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
+    public void showConstructor() {
         Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
-        }
+        Arrays.stream(constructors)
+                .forEach(constructor -> logger.debug("parameter count : {}, paramter type : {}",
+                        constructor.getParameterCount(), constructor.getParameterTypes()));
+    }
+
+    @Test
+    void showFields() {
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(field -> logger.debug("filed name : {}, field type : {}", field.getName(), field.getType()));
+    }
+
+    @Test
+    void showMethod() {
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(method -> logger.debug("method name : {}, parameter count : {}, parameter type : {}, return type : {}",
+                        method.getName(), method.getParameterCount(), method.getParameterTypes(), method.getReturnType()));
     }
 }
