bcb57d5b0f61bd68db5d7210a29a28fd585fd9dd
LeeSeungHee <palmseung@gmail.com>
2020-06-20 15:05:01 +0900

    Refactor (ControllerScanner) : refactoring based on reviewer's review (2)

--- src/main/java/core/mvc/tobe/handlermapping/ControllerScanner.java
+++ src/main/java/core/mvc/tobe/handlermapping/ControllerScanner.java
@@ -1,6 +1,7 @@
 package core.mvc.tobe.handlermapping;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimaps;
 import core.annotation.web.Controller;
 import core.mvc.tobe.handler.HandlerExecution;
 import core.mvc.tobe.handler.HandlerExecutions;
@@ -10,19 +11,26 @@ import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class ControllerScanner {
     public static HandlerExecutions scan(Object basePackage) {
-        Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
-
-        getTypesAnnotatedWith(Controller.class, basePackage)
+        Map<HandlerKey, HandlerExecution> collect = getTypesAnnotatedWith(Controller.class, basePackage)
                 .stream()
                 .map(clazz -> HandlerExecutions.init(clazz))
-                .forEach(handler -> handlerExecutions.putAll(handler.getHandlerExecutions()));
+                .map(HandlerExecutions::getHandlerExecutions)
+                .collect(collectingAndThen(toList(), Collection::stream))
+                .flatMap(m -> m.entrySet().stream())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-        return new HandlerExecutions(handlerExecutions);
+        return new HandlerExecutions(collect);
     }
 
     private static Set<Class> getTypesAnnotatedWith(Class clazz, Object basePackage) {
