8e6239dcdd6867c437a77f1010368484eef1202e
hongjongwan <serverwizard@naver.com>
2020-06-21 18:17:16 +0900

    [feature(Junit4TestRunner)] Junit4Test에서 @MyTest 애노테이션이 설정되어 있는 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,31 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+@DisplayName("Junit4TestRunner 관련 테스트")
 public class Junit4TestRunner {
+
+    @DisplayName("Junit4Test에서 @MyTest애노테이션으로 설정되어 있는 메소드를 자동으로 실행한다.")
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Constructor<?> constructor = clazz.getConstructor();
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        Arrays.stream(declaredMethods)
+                .filter(declaredMethod -> declaredMethod.isAnnotationPresent(MyTest.class))
+                .forEach(methodMyTestAnnotated -> {
+                    try {
+                        methodMyTestAnnotated.invoke(constructor.newInstance());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
