fb614a80ebb8e428785679daab90499e46a30d1f
hongjongwan <serverwizard@naver.com>
2020-06-21 17:58:44 +0900

    [test(ReflectionTest)] Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,30 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
+@DisplayName("Reflection 테스트")
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.")
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(field -> logger.debug("Class 모든 필드 : {}", field));
+
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(constructor -> logger.debug("Class의 모든 생성자 : {}", constructor));
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(method -> logger.debug("Class의 모든 메소드 : {}", method));
     }
 
     @Test
