4ed072e66588a1dedb929f163fba3ea6c4807377
ka0oll <jjyoung0216@woowahan.com>
2020-06-21 09:26:27 +0900

    feat 요구사항 1 - 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,5 +1,7 @@
 package next.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +14,21 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field field : fields){
+            logger.debug("field : {}", field);
+        }
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for(Constructor constructor : constructors){
+            logger.debug("constructor : {}", constructor);
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for(Method method : methods){
+            logger.debug("method : {}", method);
+        }
     }
 
     @Test
