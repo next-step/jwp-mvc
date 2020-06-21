db89d4143665d5c2276cdf019843ea3b0758c1b9
ka0oll <jjyoung0216@woowahan.com>
2020-06-21 21:34:30 +0900

    feat 애노테이션 기반 MVC 프레임워크

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,22 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
+import core.mvc.ModelAndView;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 
 public class AnnotationHandlerMapping {
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -16,7 +26,41 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage,
+            new MethodAnnotationsScanner(),
+            new TypeAnnotationsScanner(),
+            new SubTypesScanner()
+        );
 
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Method> requestMappings = reflections.getMethodsAnnotatedWith(RequestMapping.class);
+
+        for(Method method : requestMappings){
+            if (!controllers.contains(method.getDeclaringClass())) {
+                continue;
+            }
+            Object controllerObj = null;
+            try {
+                controllerObj = method.getDeclaringClass().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+
+            Controller controller = method.getDeclaringClass().getAnnotation(Controller.class);
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+
+            String url = controller.path() + requestMapping.value();
+            HandlerKey handlerKey = new HandlerKey(url, requestMapping.method());
+
+            Object finalControllerObj = controllerObj;
+            handlerExecutions.put(handlerKey, new HandlerExecution() {
+                @Override
+                public ModelAndView handle(HttpServletRequest request,
+                    HttpServletResponse response) throws Exception {
+                    return (ModelAndView) method.invoke(finalControllerObj, request, response);
+                }
+            });
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
