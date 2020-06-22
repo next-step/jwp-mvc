cf6ec6480adff0dd2a2cff457747c04e21f11c3f
홍종완 <serverwizard89@gmail.com>
2020-06-22 21:05:04 +0900

    [Step1] 자바 reflection (#112)
    * [test(ReflectionTest)] Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
    
    * [feature(Junit3TestRunner)] test로 시작하는 메소드 실행
    
    * [feature(Junit4TestRunner)] Junit4Test에서 @MyTest 애노테이션이 설정되어 있는 메소드 실행
    
    * [test(ReflectionTest)] 자바 Reflection API를 활용해 Student 클래스의 name과 age 필드에 값을 할당한 후, getter 메소드를 통해 값을 확인한다
    
    * [test(QuestionTest)] 인자를 가진 Question 생성자의 인스턴스를 생성한다.
    
    * [test(BeanFactoryTest)] core.di.factory.example 패키지에서 @Controller, @Service, @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다.

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,49 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 
+@DisplayName("Reflection 테스트")
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.")
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(field -> logger.debug("Class 모든 필드 : {}", field));
+
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(constructor -> logger.debug("Class의 모든 생성자 : {}", constructor));
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(method -> logger.debug("Class의 모든 메소드 : {}", method));
+    }
+
+    @DisplayName("자바 Reflection API를 활용해 Student 클래스의 name과 age 필드에 값을 할당한 후, getter 메소드를 통해 값을 확인한다")
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+
+        Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+
+        Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+
+        Student student = new Student();
+        nameField.set(student, "홍종완");
+        ageField.set(student, 20);
+
+        logger.debug(student.toString());
     }
 
     @Test
