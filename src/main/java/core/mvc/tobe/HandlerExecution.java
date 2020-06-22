fcb5bf060c2fe34966a90fe85051fb2d6cd43d65
hongjongwan <serverwizard@naver.com>
2020-06-22 18:42:09 +0900

    [feature(AnnotationHandlerMapping, HandlerExecution, RequestMapping)] 애노테이션 기반 새로운 MVC 프레임워크 구현

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,23 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private final Class<?> clazz;
+    private final Method targetMethod;
+
+    public HandlerExecution(Class<?> clazz, Method targetMethod) {
+        this.clazz = clazz;
+        this.targetMethod = targetMethod;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        Constructor<?> defaultConstructor = clazz.getDeclaredConstructor();
+        Object targetObject = defaultConstructor.newInstance();
+
+        return (ModelAndView) targetMethod.invoke(targetObject, request, response);
     }
 }
