494e6f144a049c7a5bc50b108eccadbeb85bae33
hongjongwan <serverwizard@naver.com>
2020-06-23 03:01:12 +0900

    [refactor] DispatcherServlet에서 viewResolver 결정하는 로직 추가

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -1,23 +1,29 @@
 package core.mvc;
 
+import core.mvc.view.JspView;
+import core.mvc.view.JspViewResolver;
+import core.mvc.view.View;
+import core.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private AnnotationHandlerMapping annotationHandlerMapping;
+
+    // ViewResolver는 앞으로 추가 될 가능성이 높아 List로 선언한다.
+    private List<ViewResolver> viewResolvers = Collections.singletonList(new JspViewResolver());
 
     @Override
     public void init() throws ServletException {
@@ -26,27 +32,25 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         String requestUri = request.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", request.getMethod(), requestUri);
 
         try {
             HandlerExecution handlerExecution = annotationHandlerMapping.getHandler(request);
-            move(handlerExecution.handle(request, response), request, response);
+
+            ModelAndView modelAndView = handlerExecution.handle(request, response);
+
+            for (ViewResolver viewResolver : this.viewResolvers) {
+                if(viewResolver.supports(JspView.class)) {
+                    View view = viewResolver.resolveViewName(modelAndView.getViewName());
+                    view.render(modelAndView.getModel(), request, response);
+                }
+            }
 
         } catch (Throwable e) {
             logger.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
