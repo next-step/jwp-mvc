cc06a78efda8ac4106d727dce21b9bc220ea604d
liquidjoo <liquidjoo@gmail.com>
2020-06-22 23:30:35 +0900

    feat : 어노테이션 기반 HomeController

---
+++ src/main/java/core/mvc/tobe/controller/HomeController.java
@@ -0,0 +1,22 @@
+package core.mvc.tobe.controller;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.db.DataBase;
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("users", DataBase.findAll());
+        return new ModelAndView(new JspView("home.jsp"));
+    }
+
+}
