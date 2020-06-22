c12d70b42ece81534ab9d9d6a608359a6268af92
liquidjoo <liquidjoo@gmail.com>
2020-06-23 00:03:39 +0900

    feat : 어노테이션 기반 LogoutController

---
+++ src/main/java/core/mvc/tobe/controller/LogoutController.java
@@ -0,0 +1,23 @@
+package core.mvc.tobe.controller;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
+import next.controller.UserSessionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LogoutController {
+
+    @RequestMapping(value = "/users/logout", method = RequestMethod.POST)
+    public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
+        return new ModelAndView(new JspView("redirect:/"));
+    }
+}
---
+++ src/test/java/core/mvc/tobe/controller/LogoutControllerTest.java
@@ -0,0 +1,26 @@
+package core.mvc.tobe.controller;
+
+import core.mvc.tobe.AnnotationHandlerMapping;
+import core.mvc.tobe.HandlerExecution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+class LogoutControllerTest {
+    private AnnotationHandlerMapping handlerMapping;
+
+    @BeforeEach
+    public void setup() {
+        handlerMapping = new AnnotationHandlerMapping("core.mvc.tobe");
+        handlerMapping.initialize();
+    }
+
+    @Test
+    void logout() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users/logout");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        HandlerExecution execution = handlerMapping.getHandler(request);
+        execution.handle(request, response);
+    }
+}
