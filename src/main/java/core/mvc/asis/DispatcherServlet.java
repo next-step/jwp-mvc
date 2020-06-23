78aabd444aa8304f888eaa7f32c31641eb714701
iltaek <iltaekkwon@gmail.com>
2020-06-23 20:20:33 +0900

    refactor($production): 불필요한 ViewResolver 제거

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -2,10 +2,11 @@ package core.mvc.asis;
 
 import core.mvc.ModelAndView;
 import core.mvc.tobe.AnnotationHandlerMapping;
-import core.mvc.view.HandlerExecutionViewResolver;
+import core.mvc.tobe.HandlerExecution;
 import core.mvc.view.View;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 import javassist.NotFoundException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,16 +22,15 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final String ANNOTATION_HANDLER_BASE_PACKAGE = "next.controller";
+    private static final String ILLEGAL_HANDLER = "해당 요청을 처리할 handler가 없습니다: ";
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private AnnotationHandlerMapping handlerMapping;
-    private HandlerExecutionViewResolver viewResolver;
 
     @Override
     public void init() {
         handlerMapping = new AnnotationHandlerMapping(ANNOTATION_HANDLER_BASE_PACKAGE);
         handlerMapping.initialize();
-        viewResolver = new HandlerExecutionViewResolver();
     }
 
     @Override
@@ -39,7 +39,10 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             Object handler = handlerMapping.getHandler(req);
-            ModelAndView mav = viewResolver.resolve(handler, req, resp);
+            if (Objects.isNull(handler)) {
+                throw new NotFoundException(ILLEGAL_HANDLER + req.getRequestURI());
+            }
+            ModelAndView mav = ((HandlerExecution) handler).handle(req, resp);
             View view = mav.getView();
             view.render(mav.getModel(), req, resp);
         } catch (NotFoundException ne) {
