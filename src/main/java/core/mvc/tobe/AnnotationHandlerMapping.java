1011c3e179dec77ff704561a10cb6a819132c789
kjm <jm.kang@Qualson.com>
2020-06-23 17:25:17 +0900

    인터페이스 추가

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -3,13 +3,13 @@ package core.mvc.tobe;
 import com.google.common.collect.Maps;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMethod;
-import org.reflections.Reflections;
+import org.reflections8.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 import java.util.Set;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements RequestHandlerMapping {
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -18,10 +18,12 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
+    @Override
     public void initialize() {
         Reflections reflections = new Reflections(basePackage);
 
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class, true);
+
         controllers.stream()
                 .forEach(c -> {
                     RequestMappingHandlerUtils helper = new RequestMappingHandlerUtils(c);
@@ -29,6 +31,7 @@ public class AnnotationHandlerMapping {
                 });
     }
 
+    @Override
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
