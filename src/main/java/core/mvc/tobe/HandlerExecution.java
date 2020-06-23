d2f03896da5830eaabef64e63e0008547e2a41fc
kjs4395 <kjs4395@gmail.com>
2020-06-23 13:18:46 +0900

    feat : primitive 타입일때 변환

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -1,11 +1,13 @@
 package core.mvc.tobe;
 
+import ch.qos.logback.core.util.ContextUtil;
 import core.mvc.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 
+import javax.lang.model.type.PrimitiveType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.InvocationTargetException;
@@ -30,10 +32,63 @@ public class HandlerExecution {
 
         String[] valueNames = nameDiscoverer.getParameterNames(method);
 
+        //요청 들어온 파라미터 이름
         Object[] parameters = Stream.of(valueNames)
                 .map(request::getParameter)
                 .toArray();
 
+        // 메소드 파라미터 타입
+        Class<?>[] parameterTypes = method.getParameterTypes();
+
+        for(int i=0; i<parameterTypes.length; i++) {
+            Object parameter = parameters[i];
+            if(parameter.getClass().equals(parameterTypes[i])) {
+                continue;
+            }
+
+            if(parameterTypes[i].isPrimitive()) {
+                parameters[i] = castPrimitiveType(parameterTypes[i], parameter.toString());
+            }
+
+        }
+
         return (ModelAndView) method.invoke(clazz.newInstance(),parameters);
     }
+
+    private Object castPrimitiveType(Class<?> clazz, String value) {
+        if(clazz.equals(int.class)) {
+            return Integer.parseInt(value);
+        }
+
+        if(clazz.equals(byte.class)) {
+            return Byte.parseByte(value);
+        }
+
+        if(clazz.equals(short.class)) {
+            return Short.parseShort(value);
+        }
+
+        if(clazz.equals(long.class)) {
+            return Long.parseLong(value);
+        }
+
+        if(clazz.equals(float.class)) {
+            return Float.parseFloat(value);
+        }
+
+        if(clazz.equals(double.class)) {
+            return Double.parseDouble(value);
+        }
+
+        if(clazz.equals(boolean.class)) {
+            return Boolean.parseBoolean(value);
+        }
+
+        if(clazz.equals(char.class)) {
+            return value;
+        }
+
+        return value;
+    }
+
 }
