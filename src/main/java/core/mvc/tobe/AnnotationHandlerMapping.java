7f282e973305724d4a20da1d6e852464c52fea31
kjm <jm.kang@Qualson.com>
2020-06-24 12:00:17 +0900

    피드백 적용 - RequestMappingHandlerUtils과 AnnotationHandlerMapping으로 합침

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -2,12 +2,17 @@ package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
 import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import org.reflections8.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping implements RequestHandlerMapping {
     private Object[] basePackage;
@@ -26,8 +31,7 @@ public class AnnotationHandlerMapping implements RequestHandlerMapping {
 
         controllers.stream()
                 .forEach(c -> {
-                    RequestMappingHandlerUtils helper = new RequestMappingHandlerUtils(c);
-                    handlerExecutions.putAll(helper.makeRequestMappingHandlerMap());
+                    handlerExecutions.putAll(makeRequestMappingHandlerMap(c));
                 });
     }
 
@@ -36,5 +40,34 @@ public class AnnotationHandlerMapping implements RequestHandlerMapping {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    public Map<HandlerKey, HandlerExecution> makeRequestMappingHandlerMap(final Class<?> controller) {
+        Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+
+        Set<Method> methods = getMethodsAnnotatedWith(controller, RequestMapping.class);
+        methods.stream()
+                .forEach(m -> {
+                    try {
+                        RequestMapping r = m.getAnnotation(RequestMapping.class);
+                        HandlerKey key = new HandlerKey(r.value(), r.method());
+                        HandlerExecution execution = createMethodHandlerExecution(controller, m);
+                        handlerExecutions.put(key, execution);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                });
+
+        return handlerExecutions;
+    }
+
+    public Set<Method> getMethodsAnnotatedWith(final Class<?> controller, final Class<? extends Annotation> annotation) {
+        return Arrays.stream(controller.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(annotation))
+                .collect(Collectors.toSet());
+    }
+
+    public HandlerExecution createMethodHandlerExecution(final Class<?> controller, Method m) throws Exception {
+        return new HandlerExecution(controller.newInstance(), m);
     }
 }
