cd0c9112e34ebe6989f0d98721b403eea81e957b
kjs4395 <kjs4395@gmail.com>
2020-06-25 02:22:18 +0900

    feat : pathvariable 동작 구현

--- src/main/java/core/mvc/tobe/AnnotationParameterHelper.java
+++ src/main/java/core/mvc/tobe/AnnotationParameterHelper.java
@@ -1,4 +1,21 @@
 package core.mvc.tobe;
 
-public class AnnotationParameterHelper {
+import org.springframework.web.util.pattern.PathPattern;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+public class AnnotationParameterHelper implements HandlerMethodHelper{
+    @Override
+    public Object bindingProcess(Class<?> type, String name, PathPattern pathPattern,HttpServletRequest request) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Map<String, String> values  = pathPattern.matchAndExtract(PathPatternUtil.toPathContainer(request.getRequestURI())).getUriVariables();
+
+        String value = values.get(name);
+
+        if(type.isPrimitive()) {
+            return PrimitiveTypeUtil.castPrimitiveType(type,value);
+        }
+        return value;
+    }
 }
