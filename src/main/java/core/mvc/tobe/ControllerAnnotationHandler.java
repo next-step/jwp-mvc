2276f56fedb2bdf2781f8a72e52d713396820269
liquidjoo <liquidjoo@gmail.com>
2020-06-26 01:45:20 +0900

    refactor : ReflectionUtils 사용

--- src/main/java/core/mvc/tobe/ControllerAnnotationHandler.java
+++ src/main/java/core/mvc/tobe/ControllerAnnotationHandler.java
@@ -3,16 +3,13 @@ package core.mvc.tobe;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class ControllerAnnotationHandler {
     private static final Logger logger = LoggerFactory.getLogger(ControllerAnnotationHandler.class);
@@ -25,9 +22,9 @@ public class ControllerAnnotationHandler {
     }
 
     private static void initByRequestMappingMethod(final Class clazz) {
-        final Method[] methods = clazz.getDeclaredMethods();
-        for (final Method method : methods) {
-            final RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+        final Set<Method> allMethods = ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class));
+        for (final Method method : allMethods) {
+            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
             if (Objects.nonNull(requestMapping)) {
                 final ControllerHandlerExecution execution = new ControllerHandlerExecution(clazz, method);
                 applyExecution(requestMapping, execution);
@@ -36,18 +33,18 @@ public class ControllerAnnotationHandler {
     }
 
     private static void applyExecution(RequestMapping requestMapping, HandlerExecution handlerExecution) {
-            if (requestMapping.method().equals(RequestMethod.ALL)) {
-                Arrays.stream(RequestMethod.values())
-                        .forEach(requestMethod -> {
-                            if (!requestMethod.equals(RequestMethod.ALL)) {
-                                AnnotationHandlerMapping.handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMethod), handlerExecution);
-                                logger.info("Mapping Info - method : {}, value : {}, Execution : {}", requestMethod, requestMapping.value(), handlerExecution);
-                            }
-                        });
-                return;
-            }
-            final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
-            AnnotationHandlerMapping.handlerExecutions.put(handlerKey, handlerExecution);
-            logger.info("Mapping Info - method : {}, value : {}, Execution : {}", requestMapping.method(), requestMapping.value(), handlerExecution);
+        if (requestMapping.method().equals(RequestMethod.ALL)) {
+            Arrays.stream(RequestMethod.values())
+                    .forEach(requestMethod -> {
+                        if (!requestMethod.equals(RequestMethod.ALL)) {
+                            AnnotationHandlerMapping.handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMethod), handlerExecution);
+                            logger.info("Mapping Info - method : {}, value : {}, Execution : {}", requestMethod, requestMapping.value(), handlerExecution);
+                        }
+                    });
+            return;
+        }
+        final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
+        AnnotationHandlerMapping.handlerExecutions.put(handlerKey, handlerExecution);
+        logger.info("Mapping Info - method : {}, value : {}, Execution : {}", requestMapping.method(), requestMapping.value(), handlerExecution);
     }
 }
