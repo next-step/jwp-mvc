a0bb2966222d2a148eb1e6e154f2e6d9d12870c7
bactoria <bactoria@gmail.com>
2020-06-26 23:23:35 +0900

    feat : 핸들러를 찾지 못했을 경우 404 에러 페이지를 보여주는 기능 추가

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -5,6 +5,7 @@ import core.mvc.handlerMapping.AnnotationHandlerMapping;
 import core.mvc.handlerMapping.HandlerMapping;
 import core.mvc.support.*;
 import core.mvc.support.exception.HandlerNotFoundException;
+import core.mvc.view.JspView;
 import core.mvc.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,8 +48,13 @@ public class DispatcherServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), req.getRequestURI());
         try {
-            final Object handler = getHandler(req);
-            final ModelAndView mav = handle(handler, req, resp, handlerMethodArgumentResolverComposite);
+            ModelAndView mav = null;
+            try {
+                final Object handler = getHandler(req);
+                mav = handle(handler, req, resp, handlerMethodArgumentResolverComposite);
+            } catch (HandlerNotFoundException handlerNotFoundException) {
+                mav = new ModelAndView(new JspView("/error/page404.jsp"));
+            }
             render(mav, req, resp);
         } catch (Exception e) {
             logger.error("Exception is: ", e);
@@ -58,7 +64,7 @@ public class DispatcherServlet extends HttpServlet {
 
     private ModelAndView handle(Object handler, HttpServletRequest req, HttpServletResponse resp, HandlerMethodArgumentResolverComposite handlerMethodArgumentResolverComposite) throws Exception {
         if (handler instanceof HandlerExecution) {
-                return ((HandlerExecution) handler).handle(req, resp, handlerMethodArgumentResolverComposite);
+            return ((HandlerExecution) handler).handle(req, resp, handlerMethodArgumentResolverComposite);
         }
         throw new ServletException("handler not Found");
     }
---
+++ webapp/error/page404.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <%@ include file="/include/header.jspf" %>
+</head>
+<body>
+<%@ include file="/include/navigation.jspf" %>
+
+<div class="container" id="main">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+            페이지가 존재하지 않습니다.
+        </div>
+    </div>
+</div>
+
+<%@ include file="/include/footer.jspf" %>
+</body>
+</html>
