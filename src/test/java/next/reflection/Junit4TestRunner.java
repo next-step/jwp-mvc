cae2a9210ccafed27852fdfd6c16b32aec758aab
crystal_rim <remember2889@gmail.com>
2020-06-28 03:26:15 +0900

    Step1 리뷰 요청 (#130)
    * test(ReflectionTest): 클래스 정보 출력
    
    * test(Junit3TestRunner): 이름이 test로 시작하는 메소드 실행
    
    * test(Junit4TestRunner): @MyTest 애노테이션이 있는 메소드 실행
    
    * test(ReflectionTest): private field에 값 할당
    
    * test(ReflectionTest): 인자를 가진 생성자의 인스턴스 생성
    
    * test(ReflectionTest): 컴포넌트 스캔

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        Junit4Test junit4TestInstance = clazz.newInstance();
+        Class<MyTest> myTestClass = MyTest.class;
+
+        Arrays.stream(declaredMethods)
+                .filter(method -> method.isAnnotationPresent(myTestClass))
+                .forEach(myTestMethod -> {
+                    try {
+                        myTestMethod.invoke(junit4TestInstance);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,28 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +31,15 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        showAll(clazz.getDeclaredFields());
+        showAll(clazz.getDeclaredConstructors());
+        showAll(clazz.getMethods());
+    }
+
+    public <T extends Member> void showAll(T[] elements) {
+        Arrays.stream(elements)
+                .forEach(e -> logger.debug(e.getName()));
     }
 
     @Test
@@ -27,5 +54,63 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @DisplayName("private 필드인 name과 age에 값 할당")
+    @Test
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        // given
+        Class<Student> studentClass = Student.class;
+
+        Student student = new Student();
+
+        Field name = studentClass.getDeclaredField("name");
+        Field age = studentClass.getDeclaredField("age");
+
+        name.setAccessible(true);
+        age.setAccessible(true);
+        // when
+        name.set(student, "crystal");
+        age.set(student, 26);
+        // then
+        assertThat(student.getName()).isEqualTo("crystal");
+        assertThat(student.getAge()).isEqualTo(26);
+    }
+
+    @DisplayName("인자 3개를 가진 Question 생성자의 인스턴스 생성")
+    @Test
+    void create_instance_by_nonDefaultConstructor() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        // given
+        Class<Question> questionClass = Question.class;
+
+        Constructor<?>[] declaredConstructors = questionClass.getDeclaredConstructors();
+
+        Constructor<?> constructorWith3Parameters = Arrays.stream(declaredConstructors)
+                .filter(constructor -> constructor.getParameterCount() == 3)
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+        // when
+        Question question = (Question) constructorWith3Parameters.newInstance("crystal", "hi", "everyone!");
+        // then
+        assertThat(question.getWriter()).isEqualTo("crystal");
+        assertThat(question.getTitle()).isEqualTo("hi");
+        assertThat(question.getContents()).isEqualTo("everyone!");
+    }
+
+    @DisplayName("core.di.factory.example 패키지에 대한 컴포넌트 스캔 수행")
+    @Test
+    void test_componentScan() {
+        // given
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        List<Class<? extends Annotation>> targets = Arrays.asList(Controller.class, Service.class, Repository.class);
+        // when
+        Set<Class<?>> resultOfScan = targets.stream()
+                .map(reflections::getTypesAnnotatedWith)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+        // then
+        assertThat(resultOfScan).hasSize(4);
+        resultOfScan.forEach(component -> logger.debug(component.toString()));
     }
 }
