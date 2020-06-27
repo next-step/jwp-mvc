be001a320d56dc4b3a75fc54f0b2c5ce528c1754
SujungRim <remember2889@gmail.com>
2020-06-27 16:24:15 +0900

    test(ReflectionTest): 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Member;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +17,15 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        showAll(clazz.getDeclaredFields());
+        showAll(clazz.getDeclaredConstructors());
+        showAll(clazz.getMethods());
+    }
+
+    public <T extends Member> void showAll(T[] elements) {
+        Arrays.stream(elements)
+                .forEach(e -> logger.debug(e.getName()));
     }
 
     @Test
