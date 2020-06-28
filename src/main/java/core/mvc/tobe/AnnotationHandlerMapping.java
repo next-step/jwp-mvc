9778666f18bc216e6a3dec4c93396d44f9df4bff
SujungRim <remember2889@gmail.com>
2020-06-28 19:28:39 +0900

    feat(AnnotationHandlerMapping, HandlerExecution): Controller 및 RequestMapping 어노테이션 기반으로 MVC 개선

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,58 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private static final Logger logger = getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Reflections reflections;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
+        this.reflections = new Reflections(basePackage);
     }
 
     public void initialize() {
+        Set<Class<?>> controllerClasses = this.reflections.getTypesAnnotatedWith(Controller.class, true);
 
+        for (Class<?> clazz : controllerClasses) {
+
+            Object controller = null;
+            try {
+                controller = clazz.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            Set<Method> methodsAnnotatedWithRequestMapping = Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toSet());
+
+            for (Method method : methodsAnnotatedWithRequestMapping) {
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                HandlerKey handlerKey = new HandlerKey(annotation.value(), annotation.method());
+
+                handlerExecutions.put(handlerKey, new HandlerExecution(method, controller));
+            }
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,22 @@ package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+
+    private final Method method;
+    private final Object controller;
+
+    public HandlerExecution(Method method, Object controller) {
+        this.method = method;
+        this.controller = controller;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
