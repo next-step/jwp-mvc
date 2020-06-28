0facec7b0e93c966c7bb9fb92c72f9bbd0b1a551
gch01410 <gch01410@naver.com>
2020-06-28 15:48:02 +0900

    refactor: handlerMapping 초기화

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package core.mvc.asis;
 
+import core.mvc.HandlerMapping;
 import core.mvc.ModelAndView;
+import core.mvc.exception.RequestMismatchException;
 import core.mvc.tobe.AnnotationHandlerMapping;
 import core.mvc.tobe.HandlerExecution;
 import org.slf4j.Logger;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
@@ -22,24 +26,38 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
-    private ManualRequestMapping rm;
-    private AnnotationHandlerMapping ahm;
+    private List<HandlerMapping> handlerMappings;
 
     @Override
     public void init() throws ServletException {
-        rm = new ManualRequestMapping();
-        rm.initialize();
-        ahm = new AnnotationHandlerMapping("next.controller");
-        ahm.initialize();
+        handlerMappings = Arrays.asList(
+                new ManualRequestMapping(),
+                new AnnotationHandlerMapping("next.controller"));
+        handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), req.getRequestURI());
 
-        Controller controller = (Controller) rm.getHandler(req);
-        if (Objects.isNull(controller)) {
-            HandlerExecution handlerExecution = ahm.getHandler(req);
+        Object handler = handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(req))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(RequestMismatchException::new);
+
+        if (handler instanceof Controller) {
+            Controller controller = (Controller) handler;
+            String viewName = null;
+            try {
+                viewName = controller.execute(req, resp);
+                move(viewName, req, resp);
+            } catch (Exception e) {
+                logger.error("Exception : {}", e.getMessage());
+                throw new ServletException(e);
+            }
+        } else {
+            HandlerExecution handlerExecution = (HandlerExecution) handler;
             try {
                 ModelAndView mav = handlerExecution.handle(req, resp);
                 mav.getView().render(mav.getModel(), req, resp);
@@ -47,14 +65,6 @@ public class DispatcherServlet extends HttpServlet {
                 logger.error("Exception : {}", e.getMessage());
                 throw new ServletException(e);
             }
-            return;
-        }
-        try {
-            String viewName = controller.execute(req, resp);
-            move(viewName, req, resp);
-        } catch (Throwable e) {
-            logger.error("Exception : {}", e.getMessage());
-            throw new ServletException(e);
         }
     }
 
---
+++ src/main/java/core/mvc/exception/RequestMismatchException.java
@@ -0,0 +1,4 @@
+package core.mvc.exception;
+
+public class RequestMismatchException extends RuntimeException {
+}
