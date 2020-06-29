9ce78fb666665f548c3be919b95efb04fc6a6240
kjm <jm.kang@Qualson.com>
2020-06-29 11:31:51 +0900

    redirectView 생성 테스트

---
+++ src/test/java/core/mvc/tobe/RedirectViewTest.java
@@ -0,0 +1,28 @@
+package core.mvc.tobe;
+
+import core.mvc.RedirectView;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RedirectViewTest {
+    private static final Logger logger = LoggerFactory.getLogger(RedirectViewTest.class);
+
+    @Test
+    void create() throws Exception {
+        String pathName = "redirect:/user";
+        RedirectView redirectView = new RedirectView(pathName);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        redirectView.render(null, request, response);
+
+        assertThat(response.getRedirectedUrl()).isEqualTo("/user");
+        assertThat(response.getForwardedUrl()).isEqualTo(null);
+    }
+}
