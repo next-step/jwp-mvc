54d860a1de2962584f960a01cb0faa35af9150e9
rimeorange <rimeorange83@gmail.com>
2020-06-30 00:39:32 +0900

    [step 1] - 요구사항3

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,44 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Junit4TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit4TestRunner.class);
+
+    @DisplayName("요구사항 3 - @Test Annotation 메소드 실행")
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = new Junit4Test();
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        /*for(Method method: methods){
+            if(method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test, null);
+            }
+        }*/
+
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(junit4Test, null);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+
     }
 }
