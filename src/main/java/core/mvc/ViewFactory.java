e3886f5a5064edb892ab41b386a4c01c37bbd7c7
kjm <jm.kang@Qualson.com>
2020-06-29 11:36:14 +0900

    view factory 테스트

---
+++ src/main/java/core/mvc/ViewFactory.java
@@ -0,0 +1,12 @@
+package core.mvc;
+
+public class ViewFactory {
+    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+
+    public static View create(String pathName) {
+        if (pathName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            return new RedirectView(pathName);
+        }
+        return new JspView(pathName);
+    }
+}
