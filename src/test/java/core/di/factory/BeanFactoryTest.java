ad3cb739185e7806dfae8c1919dbb09d55a7f663
Wonbo Shim <simwb@hanmail.net>
2020-06-30 07:45:31 +0900

    Step1 (#135)
    * [step 1] - 요구사항1, 요구사항4, 요구사항5
    
    * [step 1] - 요구사항2
    
    * [step 1] - 요구사항3
    
    * [step 1] - 요구사항6
    
    Co-authored-by: rimeorange <rimeorange83@gmail.com>

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -6,16 +6,23 @@ import core.annotation.Service;
 import core.annotation.web.Controller;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
+import next.reflection.ReflectionTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
+    private static final Logger logger = LoggerFactory.getLogger(BeanFactoryTest.class);
+
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -26,6 +33,17 @@ public class BeanFactoryTest {
         Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
         beanFactory = new BeanFactory(preInstanticateClazz);
         beanFactory.initialize();
+    }
+
+    @DisplayName("요구사항 6 - Component Scan")
+    @Test
+    void showAnnotation() {
+        Set<Class<?>> annotationClass = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+
+        for(Class<?> clazz : annotationClass) {
+            logger.debug("Annotaion : {} ", clazz);
+        }
+
     }
 
     @Test
