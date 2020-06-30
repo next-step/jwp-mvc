ccad680571ed7d939ed792350cbcd92eccdb2515
kjs4395 <kjs4395@gmail.com>
2020-06-30 16:07:37 +0900

    refactor : parameterInfo 생성자 인자 변경

--- src/main/java/core/mvc/tobe/MethodInfo.java
+++ src/main/java/core/mvc/tobe/MethodInfo.java
@@ -1,14 +1,8 @@
 package core.mvc.tobe;
 
-import core.annotation.web.RequestMapping;
 import core.mvc.tobe.helper.Helpers;
-import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
-import org.springframework.core.ParameterNameDiscoverer;
-import org.springframework.web.util.pattern.PathPattern;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +11,11 @@ import java.util.List;
  * Created By kjs4395 on 2020-06-25
  */
 public class MethodInfo {
-    private ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
     private List<ParameterInfo> parameters = new ArrayList<>();
 
     public MethodInfo(Method method) {
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        String[] names = nameDiscoverer.getParameterNames(method);
-        PathPattern pathPattern = PathPatternUtil.getPathPattern(method.getDeclaredAnnotation(RequestMapping.class).value());
-        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-
-        for (int i = 0; i < parameterTypes.length; i++) {
-            parameters.add(new ParameterInfo(parameterTypes[i], names[i], pathPattern, parameterAnnotations[i]));
+        for (int i = 0; i < method.getParameterTypes().length; i++) {
+            parameters.add(new ParameterInfo(method, i));
         }
     }
 
--- src/main/java/core/mvc/tobe/ParameterInfo.java
+++ src/main/java/core/mvc/tobe/ParameterInfo.java
@@ -1,16 +1,20 @@
 package core.mvc.tobe;
 
+import core.annotation.web.RequestMapping;
 import org.apache.commons.lang3.ClassUtils;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.web.util.pattern.PathPattern;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
  * Created By kjs4395 on 2020-06-25
  */
 public class ParameterInfo {
+    private ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
 
     private Class<?> type;
 
@@ -20,16 +24,17 @@ public class ParameterInfo {
 
     private boolean isAnnotated;
 
-    public ParameterInfo(Class<?> type, String name, PathPattern pathPattern, Annotation[] annotations) {
-        this.type = type;
-        this.valueName = name;
-        this.pathPattern = pathPattern;
-        this.isAnnotated = annotations.length > 0;
+    public ParameterInfo(Method method, int parameterIdx) {
+        this.type = method.getParameterTypes()[parameterIdx];
+        this.valueName = nameDiscoverer.getParameterNames(method)[parameterIdx];
+        this.pathPattern = PathPatternUtil.getPathPattern(method.getDeclaredAnnotation(RequestMapping.class).value());
+        this.isAnnotated = method.getParameterAnnotations()[parameterIdx].length > 0;
+
     }
 
     public Object resolveRequestValue(String value) {
         if (this.type.isPrimitive()) {
-            return PrimitiveTypeUtil.castPrimitiveType(type, value);
+            return PrimitiveTypeUtil.getValue(type, value);
         }
         return value;
     }
