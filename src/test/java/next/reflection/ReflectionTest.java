0f25a4f104c743ff4008f8938f39877b75c07999
flzl2008 <goddlaek@naver.com>
2020-06-30 19:05:12 +0900

    요구사항1 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,20 +15,36 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+        logger.debug("--- class ---");
         logger.debug(clazz.getName());
+
+        logger.debug("--- fields ---");
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field field : fields) {
+            logger.debug(field.getType().getName() + " " + field.getName());
+        }
+
+        logger.debug("--- constructors ---");
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor constructor : constructors) {
+            logger.debug(constructor.getName() + "(" + getParamInfo(constructor.getParameters()) + ")");
+        }
+
+        logger.debug("--- methods ---");
+        Method[] methods = clazz.getDeclaredMethods();
+        for(Method method : methods) {
+            logger.debug(method.getReturnType().getName() + " " + method.getName() + "(" + getParamInfo(method.getParameters()) + ")");
+        }
     }
 
-    @Test
-    @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+    private String getParamInfo(Parameter[] parameters) {
+        String paramInfo = "";
+        for(int i=0; i<parameters.length; i++) {
+            paramInfo += parameters[i].getType().getName();
+            if(i < parameters.length - 1) {
+                paramInfo += ", ";
             }
         }
+        return paramInfo;
     }
 }
