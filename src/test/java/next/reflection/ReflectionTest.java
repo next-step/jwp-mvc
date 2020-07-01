6d6f18bc7c11787b2440bee28390f5e897f85f86
HyunJungByun <wowyowowyo@gmail.com>
2020-07-02 07:45:50 +0900

    Step1 리뷰요청 (#139)
    * step1 요구사항1
    
    * 요구사항2 구현
    
    * 요구사항3 구현
    
    * 요구사항 4구현
    
    * 요구사항5 구현
    
    * 요구사항6 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,14 +5,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
     public void showClass() {
+        // Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        // 모든 필드 정보
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(s -> logger.debug("Field Modifiers number : " + s.getModifiers()
+                        + " / Field type : " + s.getType().getName()
+                        + " / Field name : " + s.getName()));
+        // 모든 생성자 정보
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(s -> {
+                    List<String> parameterTypes = Arrays.stream(s.getParameterTypes())
+                            .map(mn -> mn.getName())
+                            .collect(Collectors.toList());
+                    logger.debug("Constructor Modifiers number : " + s.getModifiers()
+                            + " / Constructor name : " + s.getName()
+                            + " / Constructor parameters type : " + parameterTypes.toString());
+                });
+        // 모든 메소드 정보
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(s -> {
+                    List<String> parameterTypes = Arrays.stream(s.getParameterTypes())
+                            .map(mn -> mn.getName())
+                            .collect(Collectors.toList());
+                    logger.debug("Method Modifiers number : " + s.getModifiers()
+                            + " / Method return type : " + s.getReturnType().getName()
+                            + " / Method name : " + s.getName()
+                            + " / Method parameters type : " + parameterTypes.toString());
+                });
     }
 
     @Test
@@ -27,5 +60,44 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    public void privateFieldAccess() throws Exception{
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+        Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+        Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+
+        Student student = new Student();
+        nameField.set(student, "rabbitfoot");
+        ageField.set(student, 20);
+        logger.debug("Student name : " + student.getName());
+        logger.debug("Student age : " + student.getAge());
+        logger.debug("Student toString : " + student.toString());
+    }
+
+    @Test
+    void parameterConstructor() {
+        Class<Question> clazz = Question.class;
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(s -> {
+                    try {
+                        if (s.getParameterCount() == 0)
+                            logger.debug("Question Constructor 0 parameter : " + s.newInstance());
+                        else if (s.getParameterCount() == 3)
+                            logger.debug("Question Constructor 3 parameter : " + s.newInstance("rabbitfoot1", "question3", "parameter3").toString());
+                        else if (s.getParameterCount() == 6)
+                            logger.debug("Question Constructor 6 parameter : " + s.newInstance(1, "rabbitfoot2", "question6", "parameter6", new Date(System.currentTimeMillis() - 36000), 1).toString());
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
