90f1890b93a31f7f20b820d8e6d6b67258911497
rabbitfoot <wowyowowyo@gmail.com>
2020-07-01 17:04:10 +0900

    요구사항3 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -3,7 +3,6 @@ package next.reflection;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Junit3TestRunner {
@@ -11,7 +10,6 @@ public class Junit3TestRunner {
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
-        Method[] methods = clazz.getDeclaredMethods();
         Arrays.stream(clazz.getDeclaredMethods())
                 .filter(m -> m.getName().startsWith("test"))
                 .forEach(s -> {
--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,26 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(MyTest.class))
+                .forEach(s -> {
+                    try {
+                        s.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
