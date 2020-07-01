3afb3bf4fccc9c22c40cbaf0e83ad33cadee1396
rabbitfoot <wowyowowyo@gmail.com>
2020-07-01 16:52:44 +0900

    요구사항2 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .forEach(s -> {
+                    try {
+                        s.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
