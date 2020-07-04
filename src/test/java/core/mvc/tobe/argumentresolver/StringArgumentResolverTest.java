7f670fce77122e9a11084313b934062425799c76
LeeSeungHee <palmseung@gmail.com>
2020-07-04 14:42:11 +0900

    Test/Feat (StringArgumentResolver) : add test/feat for resolve()

--- src/test/java/core/mvc/tobe/argumentresolver/StringArgumentResolverTest.java
+++ src/test/java/core/mvc/tobe/argumentresolver/StringArgumentResolverTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,6 +48,36 @@ public class StringArgumentResolverTest {
             MethodParameter methodParameter = new MethodParameter(method, i);
             assertThat(stringArgumentResolver.support(methodParameter)).isFalse();
         }
+    }
+
+    @DisplayName("String 파라미터가 들어오면, 해당 Parameter를 결정해서 Object로 반환한다")
+    @Test
+    void resolve() {
+        //given
+        Class clazz = TestUserController.class;
+        Method method = getMethod("create_string", clazz.getDeclaredMethods());
+        int length = method.getParameters().length;
+        StringArgumentResolver stringArgumentResolver = new StringArgumentResolver();
+
+        //when
+        List<Object> parameters = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            MethodParameter methodParameter = new MethodParameter(method, i);
+            Object parameter = stringArgumentResolver.resolve(methodParameter, createRequest(), new MockHttpServletResponse());
+            parameters.add(parameter);
+        }
+
+        //then
+        assertThat(parameters).hasSize(2);
+        assertThat(parameters.contains("javajigi")).isTrue();
+        assertThat(parameters.contains("password")).isTrue();
+    }
+
+    private MockHttpServletRequest createRequest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter("userId", "javajigi");
+        request.addParameter("password", "password");
+        return request;
     }
 
     private Method getMethod(String name, Method[] methods) {
