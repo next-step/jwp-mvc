0218ea66a42bc9d6a518288cc5ef2ff6160f39dc
gch01410 <gch01410@naver.com>
2020-07-05 14:54:48 +0900

    feat: 컨트롤러의 메서드의 파라미터를 매핑해서 주입

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -2,15 +2,19 @@ package core.mvc.tobe;
 
 import core.mvc.Handler;
 import core.mvc.ModelAndView;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class HandlerExecution implements Handler {
 
     private Method method;
     private Object instance;
+    private ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
 
     public HandlerExecution(Method method, Object instance) {
         this.method = method;
@@ -19,6 +23,9 @@ public class HandlerExecution implements Handler {
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(instance, request, response);
+        Object[] args = Arrays.stream(nameDiscoverer.getParameterNames(method))
+                .map(request::getParameter)
+                .toArray();
+        return (ModelAndView) method.invoke(instance, args);
     }
 }
