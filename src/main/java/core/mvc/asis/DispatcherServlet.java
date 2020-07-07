c75d8c24b27292cd5a857582623a1e46143d75ce
Jayden <jayden.lee@uneedcomms.com>
2020-07-07 15:39:02 +0900

    feat: 요구사항.1 Question 클래스 정보를 reflection 으로 조회한다

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -2,6 +2,7 @@ package core.mvc.asis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +17,32 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        Stream.of(clazz.getDeclaredFields())
+                .map(field -> "[Field] Modifiers: " + field.getModifiers()
+                        + ", Name: " + field.getName())
+                .forEach(logger::info);
+
+        Stream.of(clazz.getDeclaredMethods())
+                .map(method -> {
+                    String parameterTypes = of(method.getParameterTypes())
+                            .map(Class::getTypeName)
+                            .collect(joining(","));
+                    return "[Method] Modifiers: " + method.getModifiers()
+                            + ", Name: " + method.getName()
+                            + ", ParameterType: " + parameterTypes;
+                }).forEach(logger::info);
+
+
+        Stream.of(clazz.getDeclaredConstructors())
+                .map(constructor -> {
+                    String parameterTypes = of(constructor.getParameterTypes())
+                            .map(Class::getTypeName)
+                            .collect(joining(","));
+
+                    return "[Constructor] Modifiers: " + constructor.getModifiers()
+                            + ", Name: " + constructor.getName()
+                            + ", ParameterType: " + parameterTypes;
+                }).forEach(logger::info);
     }
 
     @Test
