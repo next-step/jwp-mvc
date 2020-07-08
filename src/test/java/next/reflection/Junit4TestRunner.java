5309045ab6b7350f8da08a6bf34360240a05c768
Jayden <jayden.lee@uneedcomms.com>
2020-07-08 16:15:24 +0900

    feat: 요구사항.3 @Test 애노테이션 메소드 실행 - 힌트 참고

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -7,9 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Junit4TestRunner {
@@ -20,19 +17,33 @@ public class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
         Method[] declaredMethods = clazz.getDeclaredMethods();
-        Stream.of(declaredMethods)
-                .forEach(method -> {
-                    Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
-                        Stream.of(declaredAnnotations)
-                                .filter(annotation -> annotation.annotationType().getSimpleName().equals("MyTest"))
-                                .findFirst()
-                                .ifPresent(e -> {
-                                    try {
-                                        method.invoke(clazz.newInstance());
-                                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException ex) {
-                                        ex.printStackTrace();
-                                    }
-                                });
-                });
+        Stream.of(declaredMethods).forEach(method -> {
+            Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
+            Stream.of(declaredAnnotations)
+                    .filter(annotation -> annotation.annotationType().getSimpleName().equals("MyTest"))
+                    .findFirst()
+                    .ifPresent(e -> {
+                        try {
+                            method.invoke(clazz.newInstance());
+                        } catch (IllegalAccessException | InvocationTargetException | InstantiationException ex) {
+                            ex.printStackTrace();
+                        }
+                    });
+        });
+    }
+
+    @Test
+    public void isAnnotationPresent() {
+        Class<Junit4Test> clazz = Junit4Test.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        Stream.of(declaredMethods).forEach(method -> {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                try {
+                    method.invoke(clazz.newInstance());
+                } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                    e.printStackTrace();
+                }
+            };
+        });
     }
 }
