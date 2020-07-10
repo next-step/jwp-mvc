3bc9db8c757d7994703a80f7f6492c7982ef8ac3
Jayden <jayden.lee@uneedcomms.com>
2020-07-10 16:58:36 +0900

    feat: 요구사항 1 - 애노테이션 기반 MVC 프레임워크

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,20 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,12 +26,30 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage, TypeAnnotationsScanner.class);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
+        controllers.stream()
+                .forEach(this::initMapping);
     }
 
-    public HandlerExecution getHandler(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
-        return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    private void initMapping(Class<?> controller) {
+        final Method[] methods = controller.getMethods();
+        final List<Method> methodsWithRequestMapping = asList(methods).stream()
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class)).collect(toList());
+
+        methodsWithRequestMapping.forEach(method -> getMethodConsumer(controller, method));
     }
+
+    private void getMethodConsumer(final Class<?> controller, Method method) {
+        RequestMapping declaredAnnotation = method.getDeclaredAnnotation(RequestMapping.class);
+        final HandlerKey key = new HandlerKey(declaredAnnotation.value(), declaredAnnotation.method());
+        handlerExecutions.put(key, new AnnotationControllerExecution(controller, method));
+    }
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
+    }
+
 }
