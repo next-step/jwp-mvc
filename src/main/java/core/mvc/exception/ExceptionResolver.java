719af490319ebde54fd2cc727573517388fbfc58
SujungRim <remember2889@gmail.com>
2020-07-10 13:14:45 +0900

    refactor(ExceptionResolver): 불필요한 ModelAndView 제거

--- src/main/java/core/mvc/exception/ExceptionResolver.java
+++ src/main/java/core/mvc/exception/ExceptionResolver.java
@@ -1,13 +1,10 @@
 package core.mvc.exception;
 
-import core.mvc.ModelAndView;
-
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface ExceptionResolver {
-    ModelAndView resolveException(ServletException ex, HttpServletRequest req, HttpServletResponse resp) throws IOException;
+    void resolveException(ServletException ex, HttpServletRequest req, HttpServletResponse resp) throws IOException;
 }
