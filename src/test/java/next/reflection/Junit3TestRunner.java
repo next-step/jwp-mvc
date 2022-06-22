e53ce2b680814bad54cc4ce5af558b9c9194278e
sjgwon <sjgwon@woowahan.com>
2022-06-22 22:45:16 +0900

    feat: 요구사항 2 - test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,21 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(it -> it.getName().startsWith("test"))
+                .forEach(it -> {
+                    try {
+                        it.invoke(clazz.newInstance());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
