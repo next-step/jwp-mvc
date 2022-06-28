ef9e4172bdbc67dd1affc9243856877943cdff6b
Pyro <rhwjddhks@gmail.com>
2022-06-28 16:37:54 +0900

    test: 자바 Reflection API를 활용해 Question 인스턴스를 생성하는 테스트 작성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -2,6 +2,7 @@ package next.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,7 +38,6 @@ public class ReflectionTest {
     }
 
     @Test
-    @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
@@ -68,6 +68,24 @@ public class ReflectionTest {
         assertAll(
                 () -> assertThat(student.getName()).isEqualTo(name),
                 () -> assertThat(student.getAge()).isEqualTo(age)
+        );
+    }
+
+    @DisplayName("Question 클래스의 인스턴스를 자바 Reflection API 를 활용해 Question 인스턴스를 생성한다.")
+    @Test
+    void createQuestionInstance() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> clazz = Question.class;
+        Constructor constructor = clazz.getConstructors()[0];
+
+        String writer = "조영호";
+        String title = "객체지향의 사실과 오해";
+        String contents = "역할, 책임, 협력";
+        Question question = (Question) constructor.newInstance(writer, title, contents);
+
+        assertAll(
+                () -> assertThat(question.getWriter()).isEqualTo(writer),
+                () -> assertThat(question.getTitle()).isEqualTo(title),
+                () -> assertThat(question.getContents()).isEqualTo(contents)
         );
     }
 }
