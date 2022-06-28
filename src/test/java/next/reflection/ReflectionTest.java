81514defba7bd735f3dae65a5f58939bd38fb332
Pyro <rhwjddhks@gmail.com>
2022-06-28 14:07:21 +0900

    test: ReflectionTest 의 showClass() 메소드 구현
    Question 클래스의 필드, 생성자, 메소드에 관한 정보를 출력함

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,36 @@
 package next.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Question 클래스의 필드, 생성자, 메소드에 관한 정보를 logger 를 통해 출력한다.")
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("\n\n================================ Field");
+        for (Field field : clazz.getDeclaredFields()) {
+            logger.debug(field.toString());
+        }
+
+        logger.debug("\n\n================================ Constructor");
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            logger.debug(constructor.toString());
+        }
+
+        logger.debug("\n\n================================ Method");
+        for (Method method : clazz.getDeclaredMethods()) {
+            logger.debug(method.toString());
+        }
     }
 
     @Test
