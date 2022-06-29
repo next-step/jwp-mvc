36d7338e71efa3188fec4727c789dbb892d5726a
devson <sonyc5720@gmail.com>
2022-06-29 23:24:59 +0900

    요구사항 4 - private field에 값 할당
    자바 Reflection API를 활용해 다음 Student 클래스의 name과 age 필드에 값을 할당한 후 getter 메소드를 통해 값을 확인한다.

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -39,5 +42,22 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        final Student instance = clazz.getDeclaredConstructor().newInstance();
+
+        final Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+        nameField.set(instance, "손영철");
+
+        final Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+        ageField.set(instance, 32);
+
+        assertThat(instance.getName()).isEqualTo("손영철");
+        assertThat(instance.getAge()).isEqualTo(32);
     }
 }
