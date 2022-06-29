842f99a3120f4638c8be1e81ab231d3d0997302d
devson <sonyc5720@gmail.com>
2022-06-29 23:11:53 +0900

    요구사항 2 - test로 시작하는 메소드 실행
    Junit3에서는 test로 시작하는 메소드를 자동으로 실행한다.

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
-    public void run() throws Exception {
+    public void run() {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(this::isTestMethod)
+                .forEach(it -> this.invokeMethod(clazz, it));
+    }
+
+    private boolean isTestMethod(Method method) {
+        return method.getName().startsWith("test");
+    }
+
+    private Object invokeMethod(Class<Junit3Test> clazz, Method method) {
+        try {
+            return method.invoke(clazz.getDeclaredConstructor().newInstance());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
