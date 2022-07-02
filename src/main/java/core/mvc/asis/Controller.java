efbbd7477639a4eea2b91d06e69fce3b15d72d2e
devson <sonyc5720@gmail.com>
2022-07-02 10:11:34 +0900

    feat: Legacy Controller를 HandlerExecution를 상속받도록 변경

--- src/main/java/core/mvc/asis/Controller.java
+++ src/main/java/core/mvc/asis/Controller.java
@@ -1,8 +1,18 @@
 package core.mvc.asis;
 
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
+import core.mvc.tobe.HandlerExecution;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Controller {
+public interface Controller extends HandlerExecution {
+    @Override
+    default ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String viewName = this.execute(request, response);
+        return new ModelAndView(new JspView(viewName));
+    }
+
     String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception;
 }
