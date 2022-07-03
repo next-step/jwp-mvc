68798ef42faee6ec06cc820bfe8444598c8b6bd7
YD <7942yongdae@gmail.com>
2022-07-04 08:24:25 +0900

    Step1 - 자바 reflection (#162)
    * docs: 요구사항 정리
    
    * test: 요구사항 1 구현
    
    * test: 요구사항 2 구현
    
    * test: 요구사항 3 구현
    
    * test: 요구사항 4 구현
    
    * test: 요구사항 5 구현
    
    * test: 요구사항 6 구현

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,30 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            invokeWithAnnotation(clazz, method, MyTest.class);
+        }
+    }
+
+    private void invokeWithAnnotation(Class clazz, Method method, Class annotation) {
+        if (method.isAnnotationPresent(annotation)) {
+            invoke(clazz, method);
+        }
+    }
+
+    private void invoke(Class clazz, Method method) {
+        try {
+            method.invoke(clazz.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +18,21 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("getDeclaredFields() \n");
+        for (Field field : clazz.getDeclaredFields()) {
+            logger.debug(field.toString());
+        }
+
+        logger.debug("getDeclaredConstructors() \n");
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            logger.debug(constructor.toString());
+        }
+
+        logger.debug("getDeclaredMethods() \n");
+        for (Method method : clazz.getDeclaredMethods()) {
+            logger.debug(method.toString());
+        }
     }
 
     @Test
@@ -27,5 +47,38 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    void accessPrivateField() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+
+        String name = "YD";
+        int age = 27;
+
+        setAccessibleField(clazz, "name");
+        setAccessibleField(clazz, "age");
+
+        Student student = new Student();
+
+        setValue(clazz, student, "name", name);
+        setValue(clazz, student, "age", age);
+
+        assertAll(
+                () -> assertThat(student.getName()).isEqualTo(name),
+                () -> assertThat(student.getAge()).isEqualTo(age)
+        );
+    }
+
+    private void setAccessibleField(Class<Student> clazz, String fieldName) throws NoSuchFieldException {
+        Field target = clazz.getDeclaredField(fieldName);
+        target.setAccessible(true);
+    }
+
+    private void setValue(Class<Student> clazz, Object instance, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field target = clazz.getDeclaredField(fieldName);
+        target.setAccessible(true);
+
+        target.set(instance, value);
     }
 }
