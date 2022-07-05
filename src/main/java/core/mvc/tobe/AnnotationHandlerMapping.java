57b12e791117f16defba5cc053551983f96224c1
dongchul <sorkehdcjf@gmail.com>
2022-07-06 00:05:00 +0900

    feat: ControllerScanner 추가

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,17 +1,14 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
-import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import core.mvc.ModelAndView;
-import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
@@ -25,23 +22,23 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         for (Object basePackage : basePackages) {
-            Reflections reflections = new Reflections(((String) basePackage));
-            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+            ControllerScanner controllerScanner = new ControllerScanner((String) basePackage);
+            Map<Class, Object> controllers = controllerScanner.getControllers();
 
             initHandlerExcutions(controllers);
         }
     }
 
-    private void initHandlerExcutions(Set<Class<?>> controllers) {
-        for (Class<?> controller : controllers) {
+    private void initHandlerExcutions(Map<Class, Object> controllers ) {
+        for (Class<?> controller : controllers.keySet()) {
             List.of(controller.getDeclaredMethods()).stream()
                 .filter(method -> method.isAnnotationPresent(RequestMapping.class))
                 .forEach(method -> {
                     RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
                     List<HandlerKey> handlerKeys = initHandlerKeys(requestMapping);
 
-                    handlerKeys.forEach(handlerKey -> handlerExecutions.put(handlerKey, (request, response) -> (ModelAndView) method.invoke(controller.getDeclaredConstructor()
-                                                                                                                                                      .newInstance(), request, response)));
+                    handlerKeys.forEach(handlerKey -> handlerExecutions.put(handlerKey,
+                            (request, response) -> (ModelAndView) method.invoke(controllers.get(controller), request, response)));
                 });
         }
     }
