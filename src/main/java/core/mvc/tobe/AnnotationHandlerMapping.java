2aa336f01e227d2aede01041e8daefb3f3fc66e5
chyee <chyee@coupang.com>
2022-07-10 02:42:07 +0900

    feat -  implement annotation based handler mapping

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,24 +1,38 @@
 package core.mvc.tobe;
 
-import com.google.common.collect.Maps;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
-public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+public class AnnotationHandlerMapping implements HandlerMapping {
+    private final Object[] basePackage;
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = new HashMap<>();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
+    @Override
     public void initialize() {
-
+        Reflections reflections = new Reflections(basePackage);
+        ControllerScanner controllerScanner = new ControllerScanner(reflections);
+        controllerScanner.getControllerMap().forEach((clazz, controller) ->
+                ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class))
+                        .forEach(method -> {
+                            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                            String url = requestMapping.value();
+                            RequestMethod requestMethod = requestMapping.method();
+                            handlerExecutions.put(new HandlerKey(url, requestMethod), new HandlerExecution(controller, method));
+                        }));
     }
 
+    @Override
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
