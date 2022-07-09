91e9f79cc45809eaa8c174194943805fb7fb50ac
chyee <chyee@coupang.com>
2022-07-09 18:27:05 +0900

    feat - log Question class fields, methods and constructors

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,14 +5,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    // 필드, 메서드, 생성자를 출력한다.
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("start logging declared fields");
+        Arrays.stream(clazz.getDeclaredFields()).forEach(field -> logger.debug(field.getName()));
+
+        logger.debug("start logging declared methods");
+        Arrays.stream(clazz.getDeclaredMethods()).forEach(method -> logger.debug(method.getName()));
+
+        logger.debug("start logging declared constructors");
+        Arrays.stream(clazz.getDeclaredConstructors()).forEach(field -> logger.debug(field.getName()));
     }
 
     @Test
