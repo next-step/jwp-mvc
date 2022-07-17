f37182427ab9f73fd524571eef754156ed5bd7ae
devson <sonyc5720@gmail.com>
2022-07-17 16:37:07 +0900

    refactor: `HandlerExecution`에 `@FunctionalInterface` 추가

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -5,6 +5,7 @@ import core.mvc.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@FunctionalInterface
 public interface HandlerExecution {
     ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
