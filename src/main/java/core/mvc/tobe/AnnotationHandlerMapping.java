4afcbc884d8bfd89e91731bd8ae26d0fda2edc86
고정완 <g@gojeong-wan-ui-MacBookPro.local>
2022-07-18 15:08:54 +0900

    feat: 요구사항1, 새로운 MVC 프레임워크 구현
    AnnotationHandlerMappingTest 를 통과하도록,
    AnnotationHandlerMapping 과 HandlerExecution 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,9 +1,15 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 public class AnnotationHandlerMapping {
@@ -16,12 +22,41 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-
+        new Reflections(basePackage)
+                .getTypesAnnotatedWith(Controller.class)
+                .forEach(this::putHandlerExecution);
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    private void putHandlerExecution(Class<?> controllerClass) {
+        Object declaredObject;
+        try {
+            declaredObject = controllerClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException |
+                 IllegalAccessException |
+                 InvocationTargetException |
+                 NoSuchMethodException e) {
+            return;
+        }
+
+        ReflectionUtils.getAllMethods(
+                controllerClass,
+                ReflectionUtils.withAnnotation(RequestMapping.class)
+        ).forEach(method -> handlerExecutions.put(
+                createHandlerKey(method),
+                new HandlerExecution(declaredObject, method)
+        ));
+    }
+
+    private HandlerKey createHandlerKey(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        String url = requestMapping.value();
+        RequestMethod requestMethod = requestMapping.method();
+        return new HandlerKey(url, requestMethod);
     }
 }
