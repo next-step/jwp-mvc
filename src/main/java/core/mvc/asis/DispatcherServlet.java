749b4c98dce8aa0bf1ebe236144001b56058a6e6
고정완 <g@gojeong-wan-ui-MacBookPro.local>
2022-07-18 16:04:50 +0900

    refactor: RequestMapping 클래스를 LegacyHandlerMapping  클래스로 이름을 변경

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -17,12 +17,12 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
-    private RequestMapping rm;
+    private LegacyHandlerMapping legacyHandlerMapping;
 
     @Override
     public void init() throws ServletException {
-        rm = new RequestMapping();
-        rm.initMapping();
+        legacyHandlerMapping = new LegacyHandlerMapping();
+        legacyHandlerMapping.initMapping();
     }
 
     @Override
@@ -30,7 +30,7 @@ public class DispatcherServlet extends HttpServlet {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
-        Controller controller = rm.findController(requestUri);
+        Controller controller = legacyHandlerMapping.findController(requestUri);
         try {
             String viewName = controller.execute(req, resp);
             move(viewName, req, resp);
--- src/main/java/core/mvc/asis/LegacyHandlerMapping.java
+++ src/main/java/core/mvc/asis/LegacyHandlerMapping.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestMapping implements HandlerMapping {
+public class LegacyHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private Map<String, Controller> mappings = new HashMap<>();
 
