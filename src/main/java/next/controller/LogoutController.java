63d49ad082dbe7347347c0ba39483978def1501e
ghojeong <rhwjddhks@gmail.com>
2022-07-21 20:34:06 +0900

    refactor: LogoutController 에서의 리턴 타입을 String 에서 ModelAndView 로 변경

--- src/main/java/next/controller/LogoutController.java
+++ src/main/java/next/controller/LogoutController.java
@@ -2,6 +2,7 @@ package next.controller;
 
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
+import core.mvc.view.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,9 +11,9 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class LogoutController {
     @RequestMapping("/users/logout")
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         HttpSession session = req.getSession();
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
-        return "redirect:/";
+        return ModelAndView.from("redirect:/");
     }
 }
