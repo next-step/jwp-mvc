8eac8f346e175338c858d7fd8a3b8bf5bfbee36f
ghojeong <rhwjddhks@gmail.com>
2022-07-21 20:34:34 +0900

    refactor: ProfileController 에서의 리턴 타입을 String 에서 ModelAndView 로 변경

--- src/main/java/next/controller/ProfileController.java
+++ src/main/java/next/controller/ProfileController.java
@@ -3,6 +3,7 @@ package next.controller;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.db.DataBase;
+import core.mvc.view.ModelAndView;
 import next.model.User;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,13 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class ProfileController {
     @RequestMapping("/users/profile")
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId);
         if (user == null) {
             throw new NullPointerException("사용자를 찾을 수 없습니다.");
         }
         req.setAttribute("user", user);
-        return "/user/profile.jsp";
+        return ModelAndView.from("/user/profile.jsp");
     }
 }
