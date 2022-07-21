eb0558ca8fc066397e79714a72089005267ee0dd
ghojeong <rhwjddhks@gmail.com>
2022-07-21 20:34:59 +0900

    refactor: UpdateFormUserController 에서의 리턴 타입을 String 에서 ModelAndView 로 변경

--- src/main/java/next/controller/UpdateFormUserController.java
+++ src/main/java/next/controller/UpdateFormUserController.java
@@ -3,6 +3,7 @@ package next.controller;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.db.DataBase;
+import core.mvc.view.ModelAndView;
 import next.model.User;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,13 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 public class UpdateFormUserController {
 
     @RequestMapping("/users/updateForm")
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId);
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
         req.setAttribute("user", user);
-        return "/user/updateForm.jsp";
+        return ModelAndView.from("/user/updateForm.jsp");
     }
 }
