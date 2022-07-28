b921235019cddd6efa69a32a64fbbc4b542c816b
황정현 <jhhwang@wemakeprice.com>
2022-07-28 15:10:44 +0900

    feat 요구사항 3

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,26 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
+
     @Test
+    @DisplayName("@MyTest 애노테이션이 있는 메소드 실행")
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            runningMyTestAnnotation(clazz, method);
+        }
+
+    }
+
+    private void runningMyTestAnnotation(Class<Junit4Test> clazz, Method method) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        if (method.isAnnotationPresent(MyTest.class)) {
+            method.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
     }
 }
