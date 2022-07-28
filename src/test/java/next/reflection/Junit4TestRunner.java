4063f81d297b61ba050d4bee5af8c644337b58ae
seondongpyo <seondongpyo@gmail.com>
2022-07-28 23:32:30 +0900

    feat: Junit4Test 클래스에서 MyTest 애노테이션으로 설정되어 있는 메서드만 실행한다

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class Junit4TestRunner {
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Junit4TestRunner {
+
+    @DisplayName("요구사항 3 - @MyTest 애노테이션으로 설정되어 있는 메소드만 실행한다.")
     @Test
-    public void run() throws Exception {
+    void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            method.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
+
+        assertThat(methods).hasSize(2);
+        assertThat(methods.get(0).getName()).isEqualTo("one");
+        assertThat(methods.get(1).getName()).isEqualTo("two");
     }
 }
