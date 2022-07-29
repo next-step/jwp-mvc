98917c1126a589ba61b254a5d1a9f8c74025d5c6
seondongpyo <seondongpyo@gmail.com>
2022-07-29 22:08:35 +0900

    feat: `@Controller`, `@Service`, `@Repository` 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -1,21 +1,27 @@
 package core.di.factory;
 
-import com.google.common.collect.Sets;
-import core.annotation.Repository;
-import core.annotation.Service;
-import core.annotation.web.Controller;
-import core.di.factory.example.MyQnaService;
-import core.di.factory.example.QnaController;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 
-public class BeanFactoryTest {
+import com.google.common.collect.Sets;
+
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import core.di.factory.example.JdbcQuestionRepository;
+import core.di.factory.example.JdbcUserRepository;
+import core.di.factory.example.MyQnaService;
+import core.di.factory.example.QnaController;
+
+class BeanFactoryTest {
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -28,8 +34,23 @@ public class BeanFactoryTest {
         beanFactory.initialize();
     }
 
+    @SuppressWarnings("unchecked")
+    @DisplayName("요구사항 6 - @Controller, @Service, @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다.")
     @Test
-    public void di() throws Exception {
+    void componentScan() {
+        Set<Class<?>> classes = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        classes.forEach(System.out::println);
+
+        assertThat(classes).containsExactlyInAnyOrder(
+            QnaController.class,
+            JdbcQuestionRepository.class,
+            JdbcUserRepository.class,
+            MyQnaService.class
+        );
+    }
+
+    @Test
+    void di() {
         QnaController qnaController = beanFactory.getBean(QnaController.class);
 
         assertNotNull(qnaController);
