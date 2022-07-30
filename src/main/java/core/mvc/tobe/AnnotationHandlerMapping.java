6f1f0b3ec8469387a8d341812f42ad9c00aaad5f
seondongpyo <seondongpyo@gmail.com>
2022-07-30 23:22:31 +0900

    feat: AnnotationHandlerMapping의 initialize 메서드 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,43 @@
 package core.mvc.tobe;
 
-import com.google.common.collect.Maps;
-import core.annotation.web.RequestMethod;
+import static org.reflections.ReflectionUtils.*;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+
+import org.reflections.Reflections;
+
+import com.google.common.collect.Maps;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
-
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        reflections.getTypesAnnotatedWith(Controller.class)
+            .forEach(this::mappingHandlerExecutions);
+    }
 
+    private void mappingHandlerExecutions(Class<?> controller) {
+        Set<Method> methods = get(Methods.of(controller, withAnnotation(RequestMapping.class)));
+        methods.forEach(method -> {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            HandlerKey handlerKey = new HandlerKey(annotation.value(), annotation.method());
+            HandlerExecution handlerExecution = new HandlerExecution(method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        });
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
