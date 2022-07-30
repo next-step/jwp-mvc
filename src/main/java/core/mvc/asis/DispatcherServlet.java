c057de2af6a4fc7cece5b0084268c3248b5e97dc
seondongpyo <seondongpyo@gmail.com>
2022-07-31 00:34:56 +0900

    feat: DispatcherServlet에 AnnotationHandlerMapping 적용

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,7 +1,7 @@
 package core.mvc.asis;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +9,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.mvc.ModelAndView;
+import core.mvc.tobe.AnnotationHandlerMapping;
+import core.mvc.tobe.HandlerExecution;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
@@ -18,11 +24,15 @@ public class DispatcherServlet extends HttpServlet {
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private RequestMapping rm;
+    private AnnotationHandlerMapping annotationHandlerMapping;
 
     @Override
     public void init() throws ServletException {
         rm = new RequestMapping();
         rm.initMapping();
+
+        annotationHandlerMapping = new AnnotationHandlerMapping();
+        annotationHandlerMapping.initialize();
     }
 
     @Override
@@ -32,12 +42,24 @@ public class DispatcherServlet extends HttpServlet {
 
         Controller controller = rm.findController(requestUri);
         try {
-            String viewName = controller.execute(req, resp);
-            move(viewName, req, resp);
+            handleRequest(controller, req, resp);
         } catch (Throwable e) {
-            logger.error("Exception : {}", e);
+            logger.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private void handleRequest(Controller controller, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if (controller != null) {
+            String viewName = controller.execute(req, resp);
+            move(viewName, req, resp);
+            return;
+        }
+
+        HandlerExecution handlerExecution = annotationHandlerMapping.getHandler(req);
+        ModelAndView mav = handlerExecution.handle(req, resp);
+        Map<String, Object> model = mav.getModel();
+        mav.getView().render(model, req, resp);
     }
 
     private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
