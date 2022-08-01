7ce6a1532619df8031f4c097ff62d5a0c2e4912c
HyungjuJang <dacapolife87@gmail.com>
2022-08-01 23:42:56 +0900

    feat : reflection 사용하여 Question 클래스 정보 출력하기

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -19,6 +21,17 @@ public class ReflectionTest {
     @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
+        Constructor[] declaredConstructors = clazz.getDeclaredConstructors();
+        for (Constructor constructor : declaredConstructors) {
+            Class[] parameterTypes = constructor.getParameterTypes();
+            logger.debug("paramer length : {}", parameterTypes.length);
+            for (Class paramType : parameterTypes) {
+                logger.debug("param type : {}", paramType);
+            }
+        }
+
+        logger.debug("-----------------------------------------------------------");
+
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
@@ -26,6 +39,50 @@ public class ReflectionTest {
             for (Class paramType : parameterTypes) {
                 logger.debug("param type : {}", paramType);
             }
+        }
+    }
+
+    @Test
+    public void field() {
+        Class<Question> clazz = Question.class;
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            logger.debug("Modifier : {}, Tyep : {}, name : {}, ", field.getModifiers(), field.getType(), field.getName());
+        }
+
+        logger.debug("-----------------------------------------------------------");
+
+        Field[] fields = clazz.getFields();
+        for (Field field : fields) {
+            logger.debug("Modifier : {}, Tyep : {}, name : {}, ", field.getModifiers(), field.getType(), field.getName());
+        }
+    }
+
+    @Test
+    public void method() {
+        Class<Question> clazz = Question.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            logger.debug("Modifier : {}, ReturnType : {},  Name : {}", method.getModifiers(), method.getReturnType(), method.getName());
+            logger.debug("param length : {}, param Count : {}", parameterTypes.length, method.getParameterCount());
+            for (Class paramType : parameterTypes) {
+                logger.debug("param type : {}", paramType);
+            }
+            logger.debug("------------------------");
+        }
+
+        logger.debug("-----------------------------------------------------------");
+
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            logger.debug("Modifier : {}, ReturnType : {},  Name : {}", method.getModifiers(), method.getReturnType(), method.getName());
+            logger.debug("param length : {}, param Count : {}", parameterTypes.length, method.getParameterCount());
+            for (Class paramType : parameterTypes) {
+                logger.debug("param type : {}", paramType);
+            }
+            logger.debug("------------------------");
         }
     }
 }
