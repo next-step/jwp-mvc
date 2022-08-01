43398e5900cc49fe3554b7c6e3780da46d9b5b61
황정현 <jhhwang@wemakeprice.com>
2022-08-01 12:19:08 +0900

    test 중복코드 제거 및 stream 활용

--- src/test/java/core/di/factory/ComponentScanTest.java
+++ src/test/java/core/di/factory/ComponentScanTest.java
@@ -3,14 +3,21 @@ package core.di.factory;
 import core.annotation.Repository;
 import core.annotation.Service;
 import core.annotation.web.Controller;
+import core.di.factory.example.QnaController;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.reflections.scanners.Scanners.TypesAnnotated;
 
@@ -19,34 +26,26 @@ public class ComponentScanTest {
     private static final Logger logger = LoggerFactory.getLogger(ComponentScanTest.class);
     private static final String PAKAGE = "core.di.factory.example";
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideArgumentOfAnnotation")
     @DisplayName("core.di.factory.example 패키지에서 @Controller 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다.")
-    void findComponent_controller() {
-        Reflections reflections = new Reflections(PAKAGE, TypesAnnotated);
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class<?> aClass : typesAnnotatedWith) {
-            logger.debug("className : {}", aClass.getName());
-        }
+    void findComponent_controller(Class<? extends Annotation> clazz, Class<? extends Annotation> actualClass) {
+        Set<Class<?>> typesAnnotatedWith = getClasses(clazz);
+
+        Assertions.assertThat(typesAnnotatedWith).contains(actualClass);
     }
 
-    @Test
-    @DisplayName("core.di.factory.example 패키지에서 @Service애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다.")
-    void findComponent_service() {
+    private Set<Class<?>> getClasses(Class<? extends Annotation> clazz) {
         Reflections reflections = new Reflections(PAKAGE, TypesAnnotated);
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Service.class);
-        for (Class<?> aClass : typesAnnotatedWith) {
-            logger.debug("className : {}", aClass.getName());
-        }
+        return reflections.getTypesAnnotatedWith(clazz);
     }
 
-    @Test
-    @DisplayName("core.di.factory.example 패키지에서 @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력한다.")
-    void findComponent_repository() {
-        Reflections reflections = new Reflections(PAKAGE, TypesAnnotated);
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Repository.class);
-        for (Class<?> aClass : typesAnnotatedWith) {
-            logger.debug("className : {}", aClass.getName());
-        }
+    private static Stream<Arguments> provideArgumentOfAnnotation() {
+        return Stream.of(
+                Arguments.of(Controller.class, QnaController.class),
+                Arguments.of(Service.class, "core.di.factory.example.MyQnaService"),
+                Arguments.of(Repository.class, "core.di.factory.example.JdbcUserRepository"),
+                Arguments.of(Repository.class, "core.di.factory.example.JdbcUserRepository")
+        );
     }
-
 }
