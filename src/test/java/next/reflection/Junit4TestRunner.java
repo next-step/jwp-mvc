a54252a8f56695c9530033b427fa84695c9a7b27
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-02 11:05:38 +0900

    feat: @Test 애노테이션 메소드 실행 구현

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,25 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit4Test newInstance = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method method : methods) {
+            runMethod(newInstance, method);
+        }
+    }
+
+    private void runMethod(Junit4Test clazz, Method method) throws InvocationTargetException, IllegalAccessException {
+        if (method.isAnnotationPresent(MyTest.class)) {
+            method.invoke(clazz);
+        }
     }
 }
