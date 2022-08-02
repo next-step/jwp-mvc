43414d6f3fc4cdc941650989895e04eabe80a4d7
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-02 10:59:40 +0900

    feat: test로 시작하는 메소드 실행 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,29 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
+
     @Test
+    @DisplayName("Junit3Test 클래스에서 test로 시작하는 메서드 실행")
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit3Test newInstance = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method method : methods) {
+            runMethod(newInstance, method);
+        }
+    }
+
+    private void runMethod(Junit3Test clazz, Method method) throws InvocationTargetException, IllegalAccessException {
+        if (method.getName().startsWith("test")) {
+            method.invoke(clazz);
+        }
     }
 }
