78f26380b126a4eb3ab48fb832f18991c0306ff9
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-02 11:07:16 +0900

    refactor: displayName 수정

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -3,10 +3,12 @@ package next.reflection;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit4TestRunner {
     @Test
+    @DisplayName("@MyTest 어노테이션 사용 메서드 실행")
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         Method[] methods = clazz.getDeclaredMethods();
