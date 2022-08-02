d36ca9936d14698f63f6a2afda8fa359a9ab16ee
ldonghee <ehdgml3206@gmail.com>
2022-08-02 22:22:01 +0900

    1단계 - 자바 reflection (#177)
    * docs: 요구사항 정리
    
    * feat: Question 클래스 정보 출력 기능 추가
    
    * feat: test로 시작하는 메소드 실행 구현
    
    * feat: @Test 애노테이션 메소드 실행 구현
    
    * refactor: displayName 수정
    
    * feat: private field에 값 할당 구현
    
    * feat: 인자를 가진 생성자의 인스턴스 생성 구현
    
    * feat: component scan 구현
    
    Co-authored-by: donghui.lee <donghui.lee@nhnsoft.com>

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,27 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit4TestRunner {
     @Test
+    @DisplayName("@MyTest 어노테이션 사용 메서드 실행")
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit4Test newInstance = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method method : methods) {
+            runMethod(newInstance, method);
+        }
+    }
+
+    private void runMethod(Junit4Test clazz, Method method) throws InvocationTargetException, IllegalAccessException {
+        if (method.isAnnotationPresent(MyTest.class)) {
+            method.invoke(clazz);
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,31 +1,81 @@
 package next.reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Constructor;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
+    @DisplayName("Question 클래스의 필드, 생성자, 메서드 정보 출력")
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug("Field : {}", field.toString());
+        }
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor constructor : constructors) {
+            logger.debug("Constructor : {}", constructor.toString());
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("Method : {}", method.toString());
+        }
     }
 
     @Test
-    @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
+    @DisplayName("Student 클래스 private field Getter 메서드를 통한 값 출력")
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Field nameField = clazz.getDeclaredField("name");
+        Field ageField = clazz.getDeclaredField("age");
+
+        Student student = new Student();
+        setField(nameField, student, "DHLEE");
+        setField(ageField, student, 32);
+
+        assertThat(student.getName()).isEqualTo("DHLEE");
+        assertThat(student.getAge()).isEqualTo(32);
+    }
+
+    public void setField(Field field, Object instance, Object value) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(instance, value);
+    }
+
+    @Test
+    @DisplayName("Question 클래스 인자를 가진 생성자의 인스턴스 생성")
+    public void createInstanceWithArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
+        logger.debug(clazz.getName());
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
         for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
+            logger.debug("Constructor : {}", constructor.toString());
         }
+
+        Date date = new Date();
+        Object actual1 = constructors[0].newInstance("DHLEE", "Title", "Contents");
+        Object actual2 = constructors[1].newInstance(1, "DHLEE", "Title", "Contents", date, 3);
+
+        assertThat(actual1).isEqualTo(new Question("DHLEE", "Title", "Contents"));
+        assertThat(actual2).isEqualTo(new Question(1, "DHLEE", "Title", "Contents", date, 3));
     }
 }
