fe133c5993b8b480b9facdc3b4dbd87f18e26aaf
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-02 10:23:11 +0900

    feat: Question 클래스 정보 출력 기능 추가

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,31 +1,36 @@
 package next.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Constructor;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
+    @DisplayName("Question 클래스의 필드, 생성자, 메서드 정보 출력")
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
-    }
 
-    @Test
-    @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug("Field : {}", field.toString());
+        }
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
         for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
+            logger.debug("Constructor : {}", constructor.toString());
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("Method : {}", method.toString());
         }
     }
 }
