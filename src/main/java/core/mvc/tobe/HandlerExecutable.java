c9aa310ab6f941cee71c20b0cf1c9fe393290a28
YONGJU <odetsuo@gmail.com>
2022-08-03 21:01:09 +0900

    feat: HandlerExecutable 추상화
    실행 결과를 ModelAndView 타입으로 반환하는 인터페이스

---
+++ src/main/java/core/mvc/tobe/HandlerExecutable.java
@@ -0,0 +1,10 @@
+package core.mvc.tobe;
+
+import core.mvc.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface HandlerExecutable {
+
+    ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
