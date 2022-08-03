a6f9916866adee10f05c073e579d94e7b8db95eb
YONGJU <odetsuo@gmail.com>
2022-08-03 21:36:25 +0900

    feat: 기존 컨트롤러의 실행 결과를 ModelAndView로 반환
    기존 컨트롤러의 실행 결과를 신규 MVC 프레임워크
    HandlerExecution의 결과와 동일하게 만들기 위해
    HandlerExecutable을 구현하여 HandlerExecution과 동일한 타입으로 만든다

---
+++ src/main/java/core/mvc/tobe/ControllerExecution.java
@@ -0,0 +1,59 @@
+package core.mvc.tobe;
+
+import core.mvc.ModelAndView;
+import core.mvc.asis.Controller;
+import java.util.Enumeration;
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ControllerExecution implements HandlerExecutable {
+
+    private final Controller controller;
+
+    public ControllerExecution(final Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final String viewName = controller.execute(request, response);
+        final ModelAndView modelAndView = new ModelAndView(new JspView(viewName));
+
+        copyRequestAttributesIntoModelObjects(request, modelAndView);
+
+        return modelAndView;
+    }
+
+    private static void copyRequestAttributesIntoModelObjects(final HttpServletRequest request, final ModelAndView modelAndView) {
+        final Enumeration<String> attributeNames = request.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            final String attributeName = attributeNames.nextElement();
+            final Object attributeValue = request.getAttribute(attributeName);
+            copyAttribute(modelAndView, attributeName, attributeValue);
+        }
+    }
+
+    private static void copyAttribute(final ModelAndView modelAndView, final String attributeName, final Object attributeValue) {
+        if (Objects.nonNull(attributeValue)) {
+            modelAndView.addObject(attributeName, attributeValue);
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final ControllerExecution that = (ControllerExecution) o;
+        return Objects.equals(controller, that.controller);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(controller);
+    }
+}
