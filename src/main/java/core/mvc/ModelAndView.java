30f0ac7c3835b24d06c5284917b89800c765f295
YONGJU <odetsuo@gmail.com>
2022-08-03 22:01:47 +0900

    feat: ModelAndView가 메시지를 수신할 수 있도록 변경

--- src/main/java/core/mvc/ModelAndView.java
+++ src/main/java/core/mvc/ModelAndView.java
@@ -4,10 +4,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ModelAndView {
+
     private View view;
-    private Map<String, Object> model = new HashMap<String, Object>();
+    private final Map<String, Object> model = new HashMap<>();
 
     public ModelAndView() {
     }
@@ -29,8 +32,8 @@ public class ModelAndView {
         return Collections.unmodifiableMap(model);
     }
 
-    public View getView() {
-        return view;
+    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        view.render(model, request, response);
     }
 
     @Override
@@ -42,11 +45,11 @@ public class ModelAndView {
             return false;
         }
         final ModelAndView that = (ModelAndView) o;
-        return Objects.equals(getView(), that.getView()) && Objects.equals(getModel(), that.getModel());
+        return Objects.equals(view, that.view) && Objects.equals(getModel(), that.getModel());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getView(), getModel());
+        return Objects.hash(view, getModel());
     }
 }
