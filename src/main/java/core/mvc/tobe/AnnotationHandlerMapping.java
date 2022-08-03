e217f9e3b507be310a68438448d5ec31874e10e3
황정현 <jhhwang@wemakeprice.com>
2022-08-03 18:50:27 +0900

    feat 리플렉션 활용

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,18 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.springframework.http.HttpMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
+
+import static org.reflections.scanners.Scanners.TypesAnnotated;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +24,20 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Class<Controller> controllerAnnotation = Controller.class;
+        Class<RequestMapping> requestMappingAnnotation = RequestMapping.class;
 
+        Reflections reflections = new Reflections(basePackage, TypesAnnotated);
+        Set<Class<?>> controllerAnnotatedWith = reflections.getTypesAnnotatedWith(controllerAnnotation);
+
+        for (Class<?> aClass : controllerAnnotatedWith) {
+            Method[] methods = aClass.getMethods();
+            for (Method method : methods) {
+                RequestMapping requestMapping = method.getAnnotation(requestMappingAnnotation);
+
+                handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMapping.method()), new HandlerExecution(aClass, method));
+            }
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
