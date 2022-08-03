0c68125a25f78c2536c8e633d5597d118837f08c
catsbi <killfine1988@gmail.com>
2022-08-03 15:46:20 +0900

    feat: test로 시작하는 메소드만 실행하도록 로직 작성

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,29 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit3TestRunner {
+
+    @DisplayName("메서드 접두사가 test인 메서드만 실행시킨다.")
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Constructor<Junit3Test> constructor = clazz.getConstructor();
+
+        List<Method> availMethods = Arrays.stream(clazz.getMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        for (Method method : availMethods) {
+            method.invoke(constructor.newInstance());
+        }
     }
 }
