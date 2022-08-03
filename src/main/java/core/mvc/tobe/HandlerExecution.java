c3562185ebca34e61d2d535120d0b7696f59257d
황정현 <jhhwang@wemakeprice.com>
2022-08-03 18:51:16 +0900

    feat 메서드 받아 실행

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,20 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
 
 public class HandlerExecution {
+    private Object object;
+    private Method method;
+
+    public HandlerExecution(Object object, Method method) {
+        this.object = object;
+        this.method= method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+
+        return (ModelAndView) method.invoke(object, request, response);
     }
 }
