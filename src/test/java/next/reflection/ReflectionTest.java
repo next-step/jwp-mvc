f983b61f205da96ba74a0daad0ad28711fd2c734
catsbi <killfine1988@gmail.com>
2022-08-03 15:46:02 +0900

    feat: 클래스 정보 출력 로직 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,13 +1,20 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
+import java.lang.reflect.Member;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    public static final String DELIMITER = ",";
 
     @Test
     public void showClass() {
@@ -27,5 +34,27 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+
+    @DisplayName("Question클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
+    @Test
+    public void printAllByQuestion() {
+        Class<Question> questionClass = Question.class;
+
+        String constructors = extractAndJoining(questionClass.getDeclaredConstructors(),DELIMITER);
+        String methods = extractAndJoining(questionClass.getDeclaredMethods(),DELIMITER);
+        String fields = extractAndJoining(questionClass.getDeclaredFields(), DELIMITER);
+
+        logger.info("생성자 목록: {}", constructors);
+        logger.info("메서드 목록: {}", methods);
+        logger.info("필드 목록: {}", fields);
+
+    }
+
+    private static String extractAndJoining(Member[] objects, String delimiter) {
+        return Arrays.stream(objects)
+                .map(Member::getName)
+                .collect(Collectors.joining(delimiter));
     }
 }
