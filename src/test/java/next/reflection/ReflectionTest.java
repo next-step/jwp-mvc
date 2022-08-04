d485c727ccc29ab81688a7293e4ed5ac53ccab89
devyonghee <devyonghee@gmail.com>
2022-08-05 01:50:29 +0900

    test(ReflectionTest): Question 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,19 +5,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
-public class ReflectionTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
-    public void showClass() {
-        Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+    void showClass() {
+        //given
+        Class<Question> questionClass = Question.class;
+        //when, then
+        assertAll(
+                () -> assertThat(questionClass.getDeclaredFields())
+                        .extracting(Field::getName)
+                        .containsExactlyInAnyOrder("questionId", "writer", "title", "contents", "createdDate", "countOfComment"),
+                () -> assertThat(questionClass.getDeclaredConstructors())
+                        .extracting(Constructor::getName)
+                        .containsExactlyInAnyOrder("next.reflection.Question", "next.reflection.Question"),
+                () -> assertThat(questionClass.getDeclaredMethods())
+                        .extracting(Method::getName)
+                        .containsExactlyInAnyOrder(
+                                "getQuestionId", "getWriter", "getTitle", "getContents", "getCreatedDate",
+                                "getTimeFromCreateDate", "getCountOfComment", "update", "toString", "hashCode", "equals"
+                        )
+        );
     }
 
     @Test
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
+    void constructor() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
