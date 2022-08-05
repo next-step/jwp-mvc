0d5d3d99e966d471d011d1cbb2983907a08716fd
lkimilhol <lkimilhol@gmail.com>
2022-08-05 19:15:04 +0900

    test: Junit4Test 클래스의 MyClass 애노테이션이 있을 시 메서드 실행되도록 작성

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,24 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
-public class Junit4TestRunner {
+class Junit4TestRunner {
     @Test
-    public void run() throws Exception {
+    void run() {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getConstructor().newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                             InstantiationException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
