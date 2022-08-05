fb7c72428f4ec8c6a3160ff4f21a7258699965da
황정현 <jhhwang@wemakeprice.com>
2022-08-05 14:02:47 +0900

    feat handler 예외 추가

--- src/main/java/core/mvc/tobe/adapter/NotExistHandlerException.java
+++ src/main/java/core/mvc/tobe/adapter/NotExistHandlerException.java
@@ -1,2 +1,23 @@
-package core.mvc.tobe.adapter;public class NotExistHandlerException {
+package core.mvc.tobe.adapter;
+
+public class NotExistHandlerException extends RuntimeException {
+    public NotExistHandlerException() {
+        super();
+    }
+
+    public NotExistHandlerException(String message) {
+        super(message);
+    }
+
+    public NotExistHandlerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotExistHandlerException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotExistHandlerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
