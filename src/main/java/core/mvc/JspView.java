6edaaf176eb9a6706b8cf30ce594357f05461c9a
황정현 <jhhwang@wemakeprice.com>
2022-08-05 13:59:59 +0900

    feat view sub 클래스 구현

--- src/main/java/core/mvc/JspView.java
+++ src/main/java/core/mvc/JspView.java
@@ -1,2 +1,26 @@
-package core.mvc;public class JspView {
+package core.mvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+public class JspView implements View {
+    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+    private String viewPath;
+
+    public JspView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (viewPath.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            response.sendRedirect(viewPath.substring(DEFAULT_REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher(viewPath);
+        rd.forward(request, response);
+    }
 }
