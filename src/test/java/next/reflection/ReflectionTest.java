2d2b40664b5079852bb1d9ed978c7e3f6ff62eef
yonghee <54215394+devyonghee@users.noreply.github.com>
2022-08-06 13:44:42 +0900

    1단계 - 자바 reflection (#185)
    * doc(README): 1단계 요구사항 정리
    
    * test(ReflectionTest): Question 클래스 정보 출력
    
    * test(Junit3TestRunner.java): test 로 시작하는 메소드 실행
    
    * test(Junit4TestRunner.java): @MyTest 애노테이션 설정 메소드 호출
    
    * test(Junit4TestRunner.java): @MyTest 애노테이션 설정 메소드 호출
    
    * test(ReflectionTest.java): private field 값 할당
    
    * test(ReflectionTest.java): 인자가 있는 생성자로 Question 인스턴스 생성
    
    * test(BeanFactoryTest.java): controller, service, repository 애노테이션 설정된 클래스 찾기
    
    * chore(ReflectionTest.java): fix typo

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,31 +1,90 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class ReflectionTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
-    public void showClass() {
-        Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+    @DisplayName("Question 정보 출력")
+    void showClass() {
+        //given
+        Class<Question> questionClass = Question.class;
+        //when, then
+        assertAll(
+                () -> assertThat(questionClass.getDeclaredFields())
+                        .extracting(Field::getName)
+                        .containsExactlyInAnyOrder("questionId", "writer", "title", "contents", "createdDate", "countOfComment"),
+                () -> assertThat(questionClass.getDeclaredConstructors())
+                        .extracting(Constructor::getName)
+                        .containsExactlyInAnyOrder("next.reflection.Question", "next.reflection.Question"),
+                () -> assertThat(questionClass.getDeclaredMethods())
+                        .extracting(Method::getName)
+                        .containsExactlyInAnyOrder(
+                                "getQuestionId", "getWriter", "getTitle", "getContents", "getCreatedDate",
+                                "getTimeFromCreateDate", "getCountOfComment", "update", "toString", "hashCode", "equals"
+                        )
+        );
     }
 
     @Test
+    @DisplayName("private field 값 할당")
+    void privateFieldAccess() throws Exception {
+        //given
+        Student student = new Student();
+        int updatedAge = 5;
+        String updatedName = "name";
+
+        Class<Student> studentClass = Student.class;
+        Field ageField = studentClass.getDeclaredField("age");
+        Field nameField = studentClass.getDeclaredField("name");
+        //when
+        ageField.setAccessible(true);
+        nameField.setAccessible(true);
+        ageField.set(student, updatedAge);
+        nameField.set(student, updatedName);
+        //then
+        assertAll(
+                () -> assertThat(student.getAge()).isEqualTo(updatedAge),
+                () -> assertThat(student.getName()).isEqualTo(updatedName)
+        );
+    }
+
+    @Test
+    @DisplayName("인자가 있는 생성자로 인스턴스 생성")
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
-        }
+    void constructor() throws Exception {
+        //given
+        Class<Question> questionClass = Question.class;
+        List<Class<?>> targetConstructorTypes = List.of(Long.TYPE, String.class, String.class, String.class, Date.class, Integer.TYPE);
+        Constructor<?> targetConstructor = Stream.of(questionClass.getDeclaredConstructors())
+                .filter(constructor -> targetConstructorTypes.equals(List.of(constructor.getParameterTypes())))
+                .findAny()
+                .get();
+
+        int tenId = 10;
+        String writer = "writer";
+        String title = "title";
+        String contents = "contents";
+        Date nowCreatedAt = Date.from(Instant.now());
+        int fiveCountOfComment = 5;
+        //when
+        Object tenIdQuestion = targetConstructor.newInstance(tenId, writer, title, contents, nowCreatedAt, fiveCountOfComment);
+        //then
+        assertThat(tenIdQuestion).isEqualTo(new Question(tenId, writer, title, contents, nowCreatedAt, fiveCountOfComment));
     }
 }
