d2fa334fa7108233be8373caeda187ca1e4e414b
devyonghee <devyonghee@gmail.com>
2022-08-07 03:19:44 +0900

    feat(HandlerExecution.java): 핸들러 실행 호출 추가

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,60 @@
 package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Set;
 
 public class HandlerExecution {
+
+    private static final Class<HttpServletRequest> SUPPORTED_REQUEST_TYPE = HttpServletRequest.class;
+    private static final Class<HttpServletResponse> SUPPORTED_RESPONSE_TYPE = HttpServletResponse.class;
+    private static final Set<Class<?>> SUPPORTED_PARAMETER_TYPES = Set.of(SUPPORTED_REQUEST_TYPE, SUPPORTED_RESPONSE_TYPE);
+    private static final Class<?> SUPPORTED_RETURN_TYPE = ModelAndView.class;
+
+    private final Object instance;
+    private final Method executedMethod;
+
+    public HandlerExecution(Object instance, Method executedMethod) {
+        Assert.notNull(instance, "'instance' must not be null");
+        Assert.notNull(executedMethod, "'executedMethod' must not be null");
+        validateMethod(instance, executedMethod);
+        this.instance = instance;
+        this.executedMethod = executedMethod;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        if (SUPPORTED_REQUEST_TYPE.equals(executedMethod.getParameterTypes()[0])) {
+            return (ModelAndView) executedMethod.invoke(instance, request, response);
+        }
+        return (ModelAndView) executedMethod.invoke(instance, response, request);
+    }
+
+    private void validateMethodOfInstance(Object instance, Method executedMethod) {
+        if (!List.of(instance.getClass().getDeclaredMethods()).contains(executedMethod)) {
+            throw new IllegalArgumentException(String.format("instance(%s) must have executedMethod(%s)", instance, executedMethod));
+        }
+    }
+
+    private void validateMethod(Object instance, Method executedMethod) {
+        validateMethodOfInstance(instance, executedMethod);
+        validateParameterTypes(executedMethod);
+        validateReturnType(executedMethod);
+    }
+
+    private void validateReturnType(Method executedMethod) {
+        if (!SUPPORTED_RETURN_TYPE.equals(executedMethod.getReturnType())) {
+            throw new IllegalArgumentException(String.format("executedMethod(%s) must be returned %s type", executedMethod, SUPPORTED_RETURN_TYPE));
+        }
+    }
+
+    private void validateParameterTypes(Method executedMethod) {
+        if (!SUPPORTED_PARAMETER_TYPES.equals(Set.of(executedMethod.getParameterTypes()))) {
+            throw new IllegalArgumentException(String.format("executedMethod(%s) must be had %s types", executedMethod, SUPPORTED_PARAMETER_TYPES));
+        }
     }
 }
---
+++ src/test/java/core/mvc/tobe/HandlerExecutionTest.java
@@ -0,0 +1,103 @@
+package core.mvc.tobe;
+
+import core.mvc.ModelAndView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("핸들러 실행자")
+class HandlerExecutionTest {
+
+    private static final HandlerExecutionTest INVOKED_INSTANCE = new HandlerExecutionTest();
+
+    @Test
+    @DisplayName("요청 매핑 메소드로 생성")
+    void instance() {
+        assertAll(
+                () -> assertThatNoException().isThrownBy(() -> new HandlerExecution(INVOKED_INSTANCE, requestMappingMethod())),
+                () -> assertThatNoException().isThrownBy(() -> new HandlerExecution(INVOKED_INSTANCE, inversionParameterTypesRequestMappingMethod()))
+        );
+    }
+
+    @Test
+    @DisplayName("대상 객체와 메소드는 필수")
+    void instance_null_thrownIllegalArgumentException() {
+        assertAll(
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> new HandlerExecution(INVOKED_INSTANCE, null)),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> new HandlerExecution(null, requestMappingMethod()))
+        );
+    }
+
+    @Test
+    @DisplayName("메소드의 파라미터 타입은 반드시 HttpServletRequest, HttpServletResponse")
+    void instance_invalidParameterType_thrownIllegalArgumentException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new HandlerExecution(INVOKED_INSTANCE, objectTypeParameterMethod()));
+    }
+
+    @Test
+    @DisplayName("메소드의 반환 타입은 반드시 ModelAndView")
+    void instance_invalidReturnType_thrownIllegalArgumentException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new HandlerExecution(INVOKED_INSTANCE, objectReturnTypeMethod()));
+    }
+
+    @Test
+    @DisplayName("요청 정보로 메소드 호출")
+    void handle() throws Exception {
+        //given
+        HandlerExecution execution = new HandlerExecution(INVOKED_INSTANCE, requestMappingMethod());
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        //when
+        execution.handle(request, response);
+        //then
+        assertAll(
+                () -> verify(request).getMethod(),
+                () -> verify(response).getStatus()
+        );
+    }
+
+    private Method requestMappingMethod() throws NoSuchMethodException {
+        Method requestMappingMethod = HandlerExecutionTest.class.getDeclaredMethod("requestMappingMethod", HttpServletRequest.class, HttpServletResponse.class);
+        requestMappingMethod.setAccessible(true);
+        return requestMappingMethod;
+    }
+
+    private Method objectReturnTypeMethod() throws NoSuchMethodException {
+        return HandlerExecutionTest.class.getDeclaredMethod("objectReturnTypeMethod", HttpServletRequest.class, HttpServletResponse.class);
+    }
+
+    private Method inversionParameterTypesRequestMappingMethod() throws NoSuchMethodException {
+        return HandlerExecutionTest.class.getDeclaredMethod("requestMappingMethod", HttpServletResponse.class, HttpServletRequest.class);
+    }
+
+    private Method objectTypeParameterMethod() throws NoSuchMethodException {
+        return HandlerExecutionTest.class.getDeclaredMethod("objectTypeParameterMethod", Object.class);
+    }
+
+    private ModelAndView objectTypeParameterMethod(Object object) {
+        return new ModelAndView();
+    }
+
+    private Object objectReturnTypeMethod(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+    private ModelAndView requestMappingMethod(HttpServletRequest request, HttpServletResponse response) {
+        request.getMethod();
+        response.getStatus();
+        return new ModelAndView();
+    }
+
+    private ModelAndView requestMappingMethod(HttpServletResponse response, HttpServletRequest request) {
+        return new ModelAndView();
+    }
+}
