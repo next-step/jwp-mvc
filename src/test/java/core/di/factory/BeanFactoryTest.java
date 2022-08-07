47272129c92ed10d118c05d03c66927f1bfa9fb9
ohminho <ohminho@woowahan.com>
2022-08-07 22:44:31 +0900

    feat: Controller, Service, Repository 찾아 출력하기

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -9,6 +9,8 @@ import core.di.factory.example.QnaController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
@@ -16,6 +18,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
+
+    private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
+
     private Reflections reflections;
     private BeanFactory beanFactory;
 
@@ -38,6 +43,23 @@ public class BeanFactoryTest {
         MyQnaService qnaService = qnaController.getQnaService();
         assertNotNull(qnaService.getUserRepository());
         assertNotNull(qnaService.getQuestionRepository());
+    }
+
+    @Test
+    void showComponents() {
+        showComponent(Controller.class);
+
+        showComponent(Service.class);
+
+        showComponent(Repository.class);
+    }
+
+    private void showComponent(Class<? extends Annotation> clazz) {
+        Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(clazz);
+
+        for (Class<?> controllerType : controllerTypes) {
+            log.debug("{}: {}", clazz.getName(), controllerType.getName());
+        }
     }
 
     @SuppressWarnings("unchecked")
