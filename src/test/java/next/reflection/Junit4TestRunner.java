49cac163c563747e6a9ed7e08d7ae8d5967bf44c
ohminho <ohminho@woowahan.com>
2022-08-07 15:44:07 +0900

    feat: Junit4TestRunner 에서 @MyTest 어노테이션이 붙어있는 메서드만 실행되도록 수정

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,17 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
-public class Junit4TestRunner {
+import java.lang.reflect.Method;
+
+public class Junit4TestRunner extends AbstractTestRunner {
+
     @Test
     public void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        super.run(Junit4Test.class);
+    }
+
+    @Override
+    public boolean isTestMethod(Method method) {
+        return method.isAnnotationPresent(MyTest.class);
     }
 }
