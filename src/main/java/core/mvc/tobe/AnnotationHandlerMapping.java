e9bade3b149c4b865a5fcf60fb34b0d53cd6c393
devyonghee <devyonghee@gmail.com>
2022-08-07 09:15:34 +0900

    feat(AnnotationHandlerMapping.java): 컨트롤러 메소드 실행

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,27 +1,65 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.springframework.beans.factory.BeanCreationException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private static final Class<?>[] EMPTY_CLASS_ARRAY = {};
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
     public void initialize() {
-
+        handlerExecutions.putAll(handlerKeyExecutionsMap());
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    private Map<HandlerKey, HandlerExecution> handlerKeyExecutionsMap() {
+        Map<Class<?>, Object> controllerClassInstances = controllerClassInstances();
+        return controllerClassInstances.keySet()
+                .stream()
+                .flatMap(controllerClass -> Stream.of(controllerClass.getDeclaredMethods()))
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toMap(this::toHandlerKey, method ->
+                        new HandlerExecution(controllerClassInstances.get(method.getDeclaringClass()), method)));
+    }
+
+    private Map<Class<?>, Object> controllerClassInstances() {
+        return new Reflections(basePackage).getTypesAnnotatedWith(Controller.class)
+                .stream()
+                .collect(Collectors.toMap(controllerClass -> controllerClass, this::newInstance));
+    }
+
+    private Object newInstance(Class<?> controllerClass) {
+        try {
+            return controllerClass.getDeclaredConstructor(EMPTY_CLASS_ARRAY).newInstance();
+        } catch (Exception e) {
+            throw new BeanCreationException(controllerClass.getName(), String.format("an error occurred during instance(%s) creation", controllerClass), e);
+        }
+    }
+
+    private HandlerKey toHandlerKey(Method method) {
+        RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+        return new HandlerKey(requestMapping.value(), requestMapping.method());
     }
 }
--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -3,23 +3,25 @@ package core.mvc.tobe;
 import core.db.DataBase;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AnnotationHandlerMappingTest {
+class AnnotationHandlerMappingTest {
     private AnnotationHandlerMapping handlerMapping;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         handlerMapping = new AnnotationHandlerMapping("core.mvc.tobe");
         handlerMapping.initialize();
     }
 
     @Test
-    public void create_find() throws Exception {
+    @DisplayName("컨트롤러 요청 메소드 실행")
+    void create_find() throws Exception {
         User user = new User("pobi", "password", "포비", "pobi@nextstep.camp");
         createUser(user);
         assertThat(DataBase.findUserById(user.getUserId())).isEqualTo(user);
