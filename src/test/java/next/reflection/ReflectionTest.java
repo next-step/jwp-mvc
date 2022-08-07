d3b931299f330b1557a65c36dc4868cc1c8535e9
itdar <thesockerr@gmail.com>
2022-08-07 14:21:47 +0900

    refactor: 요구사항 1 리팩토링

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,5 +1,6 @@
 package next.reflection;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
@@ -9,63 +10,76 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Constructor;
 
 public class ReflectionTest {
-//    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    private static final String DEPTH_1 = "\t";
-    private static final String DEPTH_2 = "\t\t";
-    private static final String DEPTH_3 = "\t\t\t";
+    private static final String DEPTH_1 = "\t\t";
+    private static final String DEPTH_2 = "\t\t\t\t";
+    private static final String DEPTH_3 = "\t\t\t\t\t\t";
 
     @Test
-    public void showClass() {
+    public void showAboutClass() {
         Class<Question> clazz = Question.class;
 
-        // fields
-        System.out.println(" ========= Field ========= ");
-        System.out.println(" DeclaredFields ");
-        Arrays.stream(clazz.getDeclaredFields())
-            .map(Field::getName)
-            .forEach(s -> {
-                System.out.print(DEPTH_1 + s + ", ");
-            });
-        System.out.println("\n");
+        printFields(clazz);
+        printConstructors(clazz);
+        printMethods(clazz);
+    }
 
-        System.out.println(" ========= Constructor ========= ");
-        Arrays.stream(clazz.getConstructors())
-                .forEach(constructor -> {
-                    System.out.print("constructor: ");
-                    System.out.println(DEPTH_1 + constructor.getName());
-                    System.out.print(DEPTH_2 + "parameterTypes: ");
-                    Arrays.stream(constructor.getParameterTypes())
-                            .forEach(paramType -> {
-                                System.out.print(paramType + ", ");
-                            });
-                    System.out.println();
-                    System.out.println(DEPTH_2 + "annotations: ");
-                    Arrays.stream(constructor.getDeclaredAnnotations())
-                        .forEach(s -> {
-                            System.out.println(DEPTH_2 + s);
-                        });
-                });
-        System.out.println("");
+    private void printFields(Class<Question> clazz) {
+        logger.debug(" ========= Field ========= ");
+        logger.debug(" DeclaredFields: ");
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            logger.debug(DEPTH_1 + declaredField);
+            Annotation[] declaredAnnotations = declaredField.getDeclaredAnnotations();
+            logger.debug(DEPTH_2 + " Field Annotations: ");
+            for (Annotation declaredAnnotation : declaredAnnotations) {
+                logger.debug(DEPTH_2 + declaredAnnotation);
+            }
+        }
+        logger.debug("");
+    }
 
-        System.out.println(" ========= Method (Only declared) ========= ");
+    private void printMethods(Class<Question> clazz) {
+        logger.debug(" ========= Method (Only declared) ========= ");
         Arrays.stream(clazz.getDeclaredMethods())
-                .forEach(method -> {
-                    System.out.print("method: ");
-                    System.out.println(DEPTH_1 + method.getName());
-                    System.out.print(DEPTH_2 + "parameterTypes: ");
-                    Arrays.stream(method.getParameterTypes())
-                        .forEach(paramType -> {
-                            System.out.print(paramType + ", ");
-                        });
-                    System.out.println();
-                    System.out.println(DEPTH_2 + "annotations: ");
-                    Arrays.stream(method.getDeclaredAnnotations())
-                        .forEach(s -> {
-                            System.out.println(DEPTH_2 + s);
-                        });
-                });
-        System.out.println();
+            .forEach(method -> {
+                logger.debug("method: ");
+                logger.debug(DEPTH_1 + method.getName());
+
+                logger.debug(DEPTH_2 + "parameterTypes: ");
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                for (Class<?> parameterType : parameterTypes) {
+                    logger.debug(DEPTH_3 + parameterType);
+                }
+
+                logger.debug(DEPTH_2 + "annotations: ");
+                Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
+                for (Annotation declaredAnnotation : declaredAnnotations) {
+                    logger.debug(DEPTH_3 + declaredAnnotation);
+                }
+            });
+    }
+
+    private void printConstructors(Class<Question> clazz) {
+        logger.debug(" ========= Constructor ========= ");
+        Arrays.stream(clazz.getConstructors())
+            .forEach(constructor -> {
+                logger.debug("constructor: ");
+                logger.debug(DEPTH_1 + constructor.getName());
+
+                logger.debug(DEPTH_2 + "parameterTypes: ");
+                Class<?>[] parameterTypes = constructor.getParameterTypes();
+                for (Class<?> parameterType : parameterTypes) {
+                    logger.debug(DEPTH_3 + parameterType + ", ");
+                }
+
+                logger.debug(DEPTH_2 + "annotations: ");
+                Annotation[] declaredAnnotations = constructor.getDeclaredAnnotations();
+                for (Annotation declaredAnnotation : declaredAnnotations) {
+                    logger.debug(DEPTH_2 + declaredAnnotation);
+                }
+            });
     }
 
     @Test
@@ -75,9 +89,9 @@ public class ReflectionTest {
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-//            logger.debug("paramer length : {}", parameterTypes.length);
+            logger.debug("paramer length : {}", parameterTypes.length);
             for (Class paramType : parameterTypes) {
-//                logger.debug("param type : {}", paramType);
+                logger.debug("param type : {}", paramType);
             }
         }
     }
