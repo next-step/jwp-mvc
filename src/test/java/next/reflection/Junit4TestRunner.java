63458f35375ae4bcd11b1965f4849ba392c8713d
mintjordy <mintjordy@woowahan.com>
2022-08-08 01:16:00 +0900

    test: 요구사항3 추가 (Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,22 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
+
+    @DisplayName("Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(MyTest.class)) {
+                declaredMethod.invoke(junit4Test);
+            }
+        }
     }
 }
