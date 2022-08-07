dd3887b5d0b9e665ef8aa36106b63705873eb8e0
ohminho <ohminho@woowahan.com>
2022-08-07 15:06:19 +0900

    feat: 클래스 정보 출력하기

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +15,46 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        logger.debug("class name : {}", clazz.getName());
+        line();
+        showFields(clazz.getDeclaredFields());
+        line();
+        showConstructors(clazz.getConstructors());
+        line();
+        showMethods(clazz.getDeclaredMethods());
+    }
+
+    private void showFields(Field[] fields) {
+        logger.debug("fields count: {}", fields.length);
+        for (Field field : fields) {
+            logger.debug("type : {}, name: {}", field.getType(), field.getName());
+        }
+    }
+
+    private void showConstructors(Constructor<?>[] constructors) {
+        for (Constructor<?> constructor : constructors) {
+            logger.debug("constrictor parameter count: {}", constructor.getParameterCount());
+            showParameters(constructor.getParameters());
+        }
+    }
+
+    private void showMethods(Method[] methods) {
+        logger.debug("methods count: {}", methods.length);
+
+        for (Method method : methods) {
+            logger.debug("returnType: {}, name: {}, parameter count: {}", method.getReturnType(), method.getName(), method.getParameterCount());
+            showParameters(method.getParameters());
+        }
+    }
+
+    private void showParameters(Parameter[] parameters) {
+        for (Parameter parameter : parameters) {
+            logger.debug("type: {}, name: {}", parameter.getType(), parameter.getName());
+        }
+    }
+
+    private void line() {
+        logger.debug("-----------------------------------------");
     }
 
     @Test
