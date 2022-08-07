ee12fe78d7f8b586f0ee5bdcf3951fb5261514b0
devyonghee <devyonghee@gmail.com>
2022-08-07 15:36:57 +0900

    feat(DispatcherServlet.java): 기존 컨트롤러 애노테이션 기반 동작 변경

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,53 +1,44 @@
 package core.mvc.asis;
 
+import core.annotation.web.Controller;
+import core.di.factory.BeanFactory;
+import core.mvc.tobe.AnnotationHandlerMapping;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+    private static final String REGISTERED_BASE_PACKAGE = "next";
 
-    private RequestMapping rm;
+    private AnnotationHandlerMapping handlerMapping;
 
     @Override
-    public void init() throws ServletException {
-        rm = new RequestMapping();
-        rm.initMapping();
+    public void init() {
+        BeanFactory beanFactory = new BeanFactory(new Reflections(REGISTERED_BASE_PACKAGE).getTypesAnnotatedWith(Controller.class));
+        beanFactory.initialize();
+        handlerMapping = new AnnotationHandlerMapping(beanFactory, REGISTERED_BASE_PACKAGE);
+        handlerMapping.initialize();
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestUri = req.getRequestURI();
-        logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
-
-        Controller controller = rm.findController(requestUri);
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        logger.debug("Method : {}, Request URI : {}", req.getMethod(), req.getRequestURI());
         try {
-            String viewName = controller.execute(req, resp);
-            move(viewName, req, resp);
+            handlerMapping.getHandler(req)
+                    .handle(req, resp)
+                    .viewRender(req, resp);
         } catch (Throwable e) {
             logger.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
