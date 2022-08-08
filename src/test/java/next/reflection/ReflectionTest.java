cbe039154505763c6123e14ab81d7f1b027a586c
hongbin <fusis1@naver.com>
2022-08-08 22:56:32 +0900

    feat: 요구사항1 필드, 생성자, 메소드 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,28 @@
 package next.reflection;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Sets;
+
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +31,10 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug(Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug(Arrays.toString(clazz.getConstructors()));
+        logger.debug(Arrays.toString(clazz.getMethods()));
     }
 
     @Test
