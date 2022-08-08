5e689173281b444e88bb35fabd45763bafef5185
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-08 22:01:22 +0900

    refactor: Controller Scan 클래스 적용

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,26 +1,19 @@
 package core.mvc.tobe;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.reflections.Reflections;
-
 import com.google.common.collect.Maps;
 
-import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 
 public class AnnotationHandlerMapping {
+    private static final int EMPTY_METHOD_LENGTH = 0;
     private Object[] basePackage;
-
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
@@ -28,38 +21,30 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        Reflections reflections = new Reflections(basePackage);
-        // 컴포넌트 스캔
-        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
-
-        controllers.forEach(controller -> {
-            // 메소드 찾기
-            Method[] declaredMethods = controller.getDeclaredMethods();
-            List<Method> methods = Arrays.stream(declaredMethods)
-                                         .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                                         .collect(Collectors.toList());
-
-            try {
-                // 객체 생성 및 등록
-                Object instance = controller.getDeclaredConstructor().newInstance();
-                methods.forEach(method -> handlerExecutions.put(getHandlerKey(method), new HandlerExecution(instance, method)));
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-        });
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        for (Class<?> controller : controllerScanner.getControllers()) {
+            addHandlerExecutions(controllerScanner, controller);
+        }
     }
 
-    public static HandlerKey getHandlerKey(Method method) {
-        String path = method.getAnnotation(RequestMapping.class).value();
-        RequestMethod requestMethod = method.getAnnotation(RequestMapping.class).method();
+    private void addHandlerExecutions(ControllerScanner controllerScanner, Class<?> controller) {
+        Object handler = controllerScanner.getHandlerInstance(controller);
+        Set<Method> methods = controllerScanner.getMethods(controller);
 
-        return new HandlerKey(path, requestMethod);
+        methods.forEach(method -> addHandlerExecution(handler, method));
+    }
+
+    private void addHandlerExecution(Object handler, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] methods = requestMapping.method();
+
+        if (methods.length == EMPTY_METHOD_LENGTH) {
+            methods = RequestMethod.values();
+        }
+
+        for (RequestMethod requestMethod : methods) {
+            handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMethod), new HandlerExecution(handler, method));
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
