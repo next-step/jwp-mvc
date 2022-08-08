65d14a5eb9eb447703824ce4618b43be8788577c
hongbin <fusis1@naver.com>
2022-08-08 22:57:46 +0900

    feat: @Test 애노테이션 메소드 실행기능

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,25 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        var instance = clazz.newInstance();
+
+        var methods = Arrays.stream(clazz.getDeclaredMethods())
+            .filter(it -> it.isAnnotationPresent(MyTest.class))
+            .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            method.invoke(instance);
+        }
     }
 }
