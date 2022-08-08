123ae3e8c8f637d1ac4abec1ab762072069b1aa0
Taejin Seok <46241671+mintjordy@users.noreply.github.com>
2022-08-08 10:48:16 +0900

    [1단계] - 자바 reflection (#192)
    * test: 요구사항1 추가 (Question의 필드, 생성자, 메서드 정보를 로깅하는 메스트코드 추가)
    
    * test: 요구사항2 추가 (Junit3Test 클래스중, test로 시작하는 메서드만 싪행시키는 테스트코드 작성)
    
    * test: 요구사항3 추가 (Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
    
    * test: 요구사항4 추가 (declaredField를 통해 private 필드에 값 할당)
    
    * test: 요구사항5 추가 (Question 인스턴스 생성)
    
    * test: 요구사항6 추가 (@Controller, @Service, @Repository 어노테이션 붙은 클래스 정보출력)
    
    Co-authored-by: mintjordy <mintjordy@woowahan.com>

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,25 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
+
+    @DisplayName("Junit3Test 클래스중, test로 시작하는 메서드만 실행한다.")
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Constructor<Junit3Test> declaredConstructor = clazz.getDeclaredConstructor();
+        Junit3Test junit3Test = declaredConstructor.newInstance();
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            String methodName = declaredMethod.getName();
+            if (methodName.startsWith("test")) {
+                declaredMethod.invoke(junit3Test);
+            }
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,25 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +27,18 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        for (Field field : clazz.getFields()) {
+            logger.info("[Question - All Field]: {}", field);
+        }
+
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            logger.info("[Question - All Constructor]: {}", constructor);
+        }
+
+        for (Method method : clazz.getMethods()) {
+            logger.info("[Question - All Method]: {}", method);
+        }
     }
 
     @Test
@@ -27,5 +53,72 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @DisplayName("Student class에서 private field에 (name, age) 값 할당")
+    @Test
+    public void privateFieldAccess() throws Exception {
+        Student student = new Student();
+        Class<Student> clazz = Student.class;
+
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "죠르디");
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 20);
+
+        assertThat(student.getName()).isEqualTo("죠르디");
+        assertThat(student.getAge()).isEqualTo(20);
+    }
+
+    @DisplayName("Question 인스턴스 생성")
+    @Test
+    void createQuestionInstance() throws Exception {
+        Class<Question> clazz = Question.class;
+        for (Constructor<Question> constructor : (Constructor<Question>[]) clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterCount() == 3) {
+                Question question = constructor.newInstance("writerId", "title", "content");
+                logger.debug("Constructor paramCount3: " + question);
+            }
+
+            if (constructor.getParameterCount() == 6) {
+                Question question = constructor.newInstance(1L, "writerId", "title", "content", Date.from(Instant.now()), 3);
+                logger.debug("Constructor paramCount6: " + question);
+            }
+        }
+    }
+
+    @DisplayName("@Controller, @Service, @Repository 애노테이션 설정 클래스 출력")
+    @Test
+    void componentScanTest() {
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        Set<Class<?>> classes = getClassWithAnnotation(reflections, Controller.class, Service.class, Repository.class);
+
+        for (Class<?> clazz : classes) {
+            logger.info("[class name] = {}", clazz.getName());
+
+            logger.info("[class annotation]");
+            for (Annotation annotation : clazz.getAnnotations()) {
+                logger.info("[annotation name] = {}", annotation);
+            }
+
+            logger.info("[class method]");
+            for (Method method : clazz.getDeclaredMethods()) {
+                logger.info("[method name] = {}", method.getName());
+            }
+        }
+
+        assertThat(classes).hasSize(4);
+    }
+
+    private Set<Class<?>> getClassWithAnnotation(Reflections reflections, Class<? extends Annotation>... classes) {
+        return Arrays.stream(classes)
+                .flatMap(aClass -> reflections.getTypesAnnotatedWith(aClass)
+                        .stream().distinct()
+                )
+                .collect(Collectors.toSet());
     }
 }
