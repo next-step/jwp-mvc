a24756fc8bc6e23934de52d35808b8abbfd97638
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-08 13:18:11 +0900

    feat: 요구사항1번 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,22 @@
 package core.mvc.tobe;
 
-import com.google.common.collect.Maps;
-import core.annotation.web.RequestMethod;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+
+import org.reflections.Reflections;
+
+import com.google.common.collect.Maps;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +28,38 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        // 컴포넌트 스캔
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
+        controllers.forEach(controller -> {
+            // 메소드 찾기
+            Method[] declaredMethods = controller.getDeclaredMethods();
+            List<Method> methods = Arrays.stream(declaredMethods)
+                                         .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                                         .collect(Collectors.toList());
+
+            try {
+                // 객체 생성 및 등록
+                Object instance = controller.getDeclaredConstructor().newInstance();
+                methods.forEach(method -> handlerExecutions.put(getHandlerKey(method), new HandlerExecution(instance, method)));
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static HandlerKey getHandlerKey(Method method) {
+        String path = method.getAnnotation(RequestMapping.class).value();
+        RequestMethod requestMethod = method.getAnnotation(RequestMapping.class).method();
+
+        return new HandlerKey(path, requestMethod);
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
