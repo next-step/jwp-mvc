cfbc79a669431b6b600c6876198c5da723171ed5
devyonghee <devyonghee@gmail.com>
2022-08-09 00:10:14 +0900

    refactor(DispatcherServlet.java): 메서드 시그니처 변경

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,19 +1,21 @@
 package core.mvc.asis;
 
+import core.mvc.ForwardView;
 import core.mvc.HandlerMapping;
+import core.mvc.ModelAndView;
+import core.mvc.RedirectView;
+import core.mvc.View;
 import core.mvc.tobe.AnnotationHandlerMapping;
 import core.mvc.tobe.ControllerScanner;
 import core.mvc.tobe.HandlerExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -26,7 +28,7 @@ public class DispatcherServlet extends HttpServlet {
     private List<HandlerMapping> mappings;
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         RequestMapping requestMapping = new RequestMapping();
         requestMapping.initMapping();
         AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(new ControllerScanner("next.controller"));
@@ -38,30 +40,7 @@ public class DispatcherServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
-
-        Object handler = handler(req);
-        execute(handler, req, resp);
-    }
-
-    private void execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws ServletException {
-        try {
-            renderView(handler, req, resp);
-        } catch (Throwable e) {
-            logger.error("Exception : {}", e);
-            throw new ServletException(e.getMessage());
-        }
-    }
-
-    private void renderView(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        if (handler instanceof Controller) {
-            render(req, resp, (Controller) handler);
-            return;
-        }
-        if (handler instanceof HandlerExecution) {
-            ((HandlerExecution) handler).handle(req, resp).viewRender(req, resp);
-            return;
-        }
-        throw new UnsupportedOperationException(String.format("handler(type: %s) is not supported type", handler.getClass()));
+        renderView(handler(req), req, resp);
     }
 
     private Object handler(HttpServletRequest request) throws ServletException {
@@ -72,16 +51,30 @@ public class DispatcherServlet extends HttpServlet {
                 .orElseThrow(() -> new ServletException(String.format("can not found request mapping: request(%s)", request)));
     }
 
-    private void render(HttpServletRequest req, HttpServletResponse resp, Controller controller) throws Exception {
-        move(controller.execute(req, resp), req, resp);
+    private void renderView(Object handler, HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+            modelAndView(handler, req, resp).viewRender(req, resp);
+        } catch (Throwable e) {
+            logger.error("Exception : {}", e);
+            throw new ServletException(e.getMessage());
+        }
     }
 
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
+    private ModelAndView modelAndView(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if (handler instanceof Controller) {
+            String executedResult = ((Controller) handler).execute(req, resp);
+            return ModelAndView.from(view(executedResult));
         }
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
+        if (handler instanceof HandlerExecution) {
+            return ((HandlerExecution) handler).handle(req, resp);
+        }
+        throw new UnsupportedOperationException(String.format("handler(type: %s) is not supported type", handler.getClass()));
+    }
+
+    private View view(String viewName) {
+        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            return RedirectView.from(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+        }
+        return ForwardView.from(viewName);
     }
 }
