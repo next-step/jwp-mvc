3dfe2369bb574ee8a44d8c6f8890a75866062603
HyungjuJang <dacapolife87@gmail.com>
2022-08-09 22:02:54 +0900

    refactor : AnnotationHandlerMapping에서 Controller Scanner를 사용하도록 수정

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -4,12 +4,13 @@ import com.google.common.collect.Maps;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
-import org.reflections.Reflections;
+import org.reflections.ReflectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,43 +26,35 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        Reflections reflections = new Reflections(basePackage);
-        try {
-            initializeHandlerExecution(reflections);
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("AnnotationHandlerMapping initiallize Exception!! : {}", e.getMessage());
-        }
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        initializeHandlerExecution(controllerScanner.getController());
     }
 
-    private void initializeHandlerExecution(Reflections reflections) throws InstantiationException, IllegalAccessException {
-        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class<?> controllerClass : controllerClasses) {
-            Controller controllerAnnotation = controllerClass.getAnnotation(Controller.class);
-            String controllerLevelPath = controllerAnnotation.value();
-
-            requestMethodScan(controllerLevelPath, controllerClass);
-        }
-    }
-
-    private void requestMethodScan(String controllerLevelPath, Class<?> controllerClass) throws InstantiationException, IllegalAccessException {
-        Object controllerInstance = controllerClass.newInstance();
-        Method[] methods = controllerClass.getDeclaredMethods();
+    private void initializeHandlerExecution(Map<Class<?>, Object> controller) {
+        Set<Method> methods = getMethods(controller);
         for (Method method : methods) {
-            addHendlerExecution(controllerInstance, controllerLevelPath, method);
+            Class<?> declaringClass = method.getDeclaringClass();
+            Controller controllerAnnotation = declaringClass.getAnnotation(Controller.class);
+            addHendlerExecution(controller.get(declaringClass), controllerAnnotation.value(), method);
         }
+    }
+
+    private Set<Method> getMethods(Map<Class<?>, Object> controller) {
+        Set<Method> methods = new HashSet<>();
+        for (Class<?> clazz : controller.keySet()) {
+            methods.addAll(ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class)));
+        }
+        return methods;
     }
 
     private void addHendlerExecution(Object controllerInstance, String controllerLevelPath, Method method) {
-        if(method.isAnnotationPresent(RequestMapping.class)) {
-            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-            String path = requestMapping.value();
-            RequestMethod requestMethod = requestMapping.method();
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        String path = requestMapping.value();
+        RequestMethod requestMethod = requestMapping.method();
 
-            HandlerKey handlerKey = new HandlerKey(controllerLevelPath + path, requestMethod);
-            logger.info("Add RequestMapping URL : {}, method : {}", handlerKey, method);
-            handlerExecutions.put(handlerKey, new HandlerExecution(controllerInstance, method));
-        }
+        HandlerKey handlerKey = new HandlerKey(controllerLevelPath + path, requestMethod);
+        logger.info("Add RequestMapping URL : {}, method : {}", handlerKey, method);
+        handlerExecutions.put(handlerKey, new HandlerExecution(controllerInstance, method));
     }
 
     @Override
