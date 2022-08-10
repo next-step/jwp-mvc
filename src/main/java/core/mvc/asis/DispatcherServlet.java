15661b0c2e71531e4a5b09bd2b98b385d50beac7
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-10 11:41:14 +0900

    refactor: Mapping 추상화 인터페이스 적용

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package core.mvc.asis;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import core.mvc.HandlerMapping;
 import core.mvc.JspView;
 import core.mvc.ModelAndView;
 import core.mvc.tobe.AnnotationHandlerMapping;
@@ -22,16 +25,18 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String BASE_PACKAGE = "next.controller";
 
-    private RequestMapping rm;
-    private AnnotationHandlerMapping annotationHandlerMapping;
+    private List<HandlerMapping> mappings = new ArrayList<>();
 
 
     @Override
     public void init() {
-        rm = new RequestMapping();
-        // rm.initMapping();
-        annotationHandlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
+        LegacyHandlerMapping legacyHandlerMapping = new LegacyHandlerMapping();
+        legacyHandlerMapping.initMapping();
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
         annotationHandlerMapping.initialize();
+
+        mappings.add(legacyHandlerMapping);
+        mappings.add(annotationHandlerMapping);
     }
 
     @Override
@@ -39,21 +44,29 @@ public class DispatcherServlet extends HttpServlet {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
-        Controller controller = rm.findController(requestUri);
+        Object handler = getHandler(req);
         try {
-            handle(controller, req, resp);
+            handle(handler, req, resp);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void handle(Controller controller, HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        if (controller != null) {
+    private Object getHandler (HttpServletRequest request) {
+        return mappings.stream()
+                       .map(mapping -> mapping.getHandler(request))
+                       .findFirst()
+                       .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 handler 입니다."));
+    }
+
+    private void handle(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if (handler instanceof HandlerExecution) {
+            HandlerExecution handlerException = (HandlerExecution)handler;
+            render(req, resp, handlerException.handle(req, resp));
+        } else if (handler instanceof Controller) {
+            Controller controller = (Controller)handler;
             String viewName = controller.execute(req, resp);
             render(req, resp, new ModelAndView(new JspView(viewName)));
-        } else {
-            HandlerExecution handler = annotationHandlerMapping.getHandler(req);
-            render(req, resp, handler.handle(req, resp));
         }
     }
 
