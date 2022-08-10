ade0bf8cd3d86eea353b51cfaac304eea5335462
mintjordy <mintjordy@woowahan.com>
2022-08-11 01:11:22 +0900

    refactor: 인터페이스 기반 컨트롤러를 애노테이션 기반 컨트롤러로 변경

--- src/main/java/core/mvc/tobe/handler/mapping/ManualHandlerMapping.java
+++ src/main/java/core/mvc/tobe/handler/mapping/ManualHandlerMapping.java
@@ -17,16 +17,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     private Map<String, Controller> mappings = new HashMap<>();
 
     public void initMapping() {
-        mappings.put("/users/form", new ForwardController("/user/form.jsp"));
-        mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-        mappings.put("/users", new ListUserController());
-        mappings.put("/users/login", new LoginController());
-        mappings.put("/users/profile", new ProfileController());
-        mappings.put("/users/logout", new LogoutController());
-        mappings.put("/users/create", new CreateUserController());
-        mappings.put("/users/updateForm", new UpdateFormUserController());
-        mappings.put("/users/update", new UpdateUserController());
-
         logger.info("Initialized Request Mapping!");
         mappings.keySet().forEach(path -> {
             logger.info("Path : {}, Controller : {}", path, mappings.get(path).getClass());
---
+++ src/main/java/next/controller/UserController.java
@@ -0,0 +1,111 @@
+package next.controller;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static core.annotation.web.RequestMethod.GET;
+import static core.annotation.web.RequestMethod.POST;
+
+@Controller
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/users/create", method = POST)
+    public String createUser(HttpServletRequest req, HttpServletResponse resp) {
+        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("User : {}", user);
+
+        DataBase.addUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/users", method = GET)
+    public String getUsers(HttpServletRequest req, HttpServletResponse resp) {
+        if (!UserSessionUtils.isLogined(req.getSession())) {
+            return "redirect:/users/loginForm";
+        }
+
+        req.setAttribute("users", DataBase.findAll());
+        return "/user/list.jsp";
+    }
+    @RequestMapping(value = "/users/profile", method = GET)
+    public String profileInfo(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return "/user/profile.jsp";
+    }
+
+    @RequestMapping(value = "/users/loginForm", method = GET)
+    public String loginForm(HttpServletRequest req, HttpServletResponse resp) {
+        return "/user/login.jsp";
+    }
+
+    @RequestMapping(value = "/users/login", method = POST)
+    public String login(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            req.setAttribute("loginFailed", true);
+            return "/user/login.jsp";
+        }
+        if (user.matchPassword(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
+            return "redirect:/";
+        } else {
+            req.setAttribute("loginFailed", true);
+            return "/user/login.jsp";
+        }
+    }
+
+    @RequestMapping(value = "/users/logout", method = GET)
+    public String logout(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/users/form", method = GET)
+    public String joinForm(HttpServletRequest req, HttpServletResponse resp) {
+        return "/user/form.jsp";
+    }
+
+    @RequestMapping(value = "/users/updateForm", method = GET)
+    public String userUpdateForm(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return "/user/updateForm.jsp";
+    }
+
+    @RequestMapping(value = "/users/update", method = POST)
+    public String updateUserInfo(HttpServletRequest req, HttpServletResponse resp) {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
+        User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("Update User : {}", updateUser);
+        user.update(updateUser);
+        return "redirect:/";
+    }
+}
