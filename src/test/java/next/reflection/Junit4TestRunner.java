37d2579348d34fc8087e2ca3c26e3025e2e755ea
etff <wish2rich@gmail.com>
2022-08-10 22:21:09 +0900

    test: Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,20 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        clazz.getDeclaredMethods();
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(MyTest.class)) {
+                declaredMethod.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
+
     }
 }
