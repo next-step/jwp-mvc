6bda3e1fb6e20a45b355caf2b8e62ca8c8ad21f8
lkimilhol <lkimilhol@gmail.com>
2022-08-11 01:20:48 +0900

    feat: AnnotationHandlerMapping 구현
    - 테스트케이스 동작 확인
    - HandlerMethodArgumentResolverTest 코드 참조하여 method.invoke 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,27 +1,53 @@
 package core.mvc.tobe;
 
-import com.google.common.collect.Maps;
-import core.annotation.web.RequestMethod;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+
+import org.reflections.ReflectionUtils;
+
+import com.google.common.collect.Maps;
+
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private final Object[] basePackage;
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
     public void initialize() {
+        ControllerScanner controllerScanner = new ControllerScanner();
+        Set<Class<?>> controllerClass = controllerScanner.findController(this.basePackage);
 
+        controllerClass.forEach(clazz -> {
+            ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class)).forEach(method -> {
+                try {
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    HandlerKey handlerKey = createHandlerKey(requestMapping);
+                    Object instance = clazz.getConstructor().newInstance();
+                    handlerExecutions.put(handlerKey, new HandlerExecution(instance, method));
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        });
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    private HandlerKey createHandlerKey(RequestMapping rm) {
+        return new HandlerKey(rm.value(), rm.method());
     }
 }
