d6d27fce83dda9e66187e665f7c0d1434357d5b8
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-11 02:24:06 +0900

    refactor: HandlerMappings, HandlerAdapters 일급 객체 적용

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,10 +1,5 @@
 package core.mvc.asis;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,10 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import core.mvc.HandlerAdapter;
-import core.mvc.HandlerMapping;
+import core.mvc.HandlerAdapters;
 import core.mvc.ModelAndView;
-import core.mvc.tobe.AnnotationHandlerMapping;
-import core.mvc.tobe.HandlerExecutionHandlerAdapter;
+import core.mvc.HandlerMappings;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
@@ -25,31 +19,23 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String BASE_PACKAGE = "next.controller";
 
-    private List<HandlerMapping> mappings = new ArrayList<>();
-    private List<HandlerAdapter> handlerAdapters = new ArrayList<>();
+    private HandlerMappings handlerMappings;
+    private HandlerAdapters handlerAdapters;
 
 
     @Override
     public void init() {
-        LegacyHandlerMapping legacyHandlerMapping = new LegacyHandlerMapping();
-        legacyHandlerMapping.initMapping();
-        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
-        annotationHandlerMapping.initialize();
-
-        mappings.add(legacyHandlerMapping);
-        mappings.add(annotationHandlerMapping);
-
-        handlerAdapters.add(new ControllerHandlerAdapter());
-        handlerAdapters.add(new HandlerExecutionHandlerAdapter());
+        handlerMappings = new HandlerMappings(BASE_PACKAGE);
+        handlerAdapters = new HandlerAdapters();
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
-        Object mappingHandler = getHandler(req);
-        HandlerAdapter handlerAdapter = getHandlerAdapter(mappingHandler);
+        Object mappingHandler = handlerMappings.getHandler(req);
+        HandlerAdapter handlerAdapter = handlerAdapters.getHandlerAdapter(mappingHandler);
 
         try {
             ModelAndView mv = handlerAdapter.handle(req, resp, mappingHandler);
@@ -57,21 +43,6 @@ public class DispatcherServlet extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private Object getHandler (HttpServletRequest request) {
-        return mappings.stream()
-                       .filter(mapping -> mapping.isSupported(request))
-                       .findFirst()
-                       .map(mapping -> mapping.getHandler(request))
-                       .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 handler 입니다."));
-    }
-
-    private HandlerAdapter getHandlerAdapter(Object handler) {
-        return handlerAdapters.stream()
-                              .filter(handlerAdapter -> handlerAdapter.supports(handler))
-                              .findFirst()
-                              .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 Adapter 입니다."));
     }
 
     private void render(HttpServletRequest req, HttpServletResponse resp, ModelAndView modelAndView) throws Exception {
