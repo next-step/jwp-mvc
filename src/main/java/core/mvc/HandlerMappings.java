9f685f98e4a5135353d8a55f22af3a595dd0b999
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-11 02:23:34 +0900

    feat: HandlerMappings 일급 객체 추출

---
+++ src/main/java/core/mvc/HandlerMappings.java
@@ -0,0 +1,35 @@
+package core.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import core.mvc.asis.LegacyHandlerMapping;
+import core.mvc.tobe.AnnotationHandlerMapping;
+
+public class HandlerMappings {
+
+	private static final String NOT_FOUND_HANDLER = "지원하지 않는 handler 입니다.";
+
+	private List<HandlerMapping> mappings = new ArrayList<>();
+
+	public HandlerMappings(Object... basePackage) {
+		LegacyHandlerMapping legacyHandlerMapping = new LegacyHandlerMapping();
+		AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(basePackage);
+
+		legacyHandlerMapping.initMapping();
+		annotationHandlerMapping.initialize();
+
+		mappings.add(legacyHandlerMapping);
+		mappings.add(annotationHandlerMapping);
+	}
+
+	public Object getHandler(HttpServletRequest request) {
+		return mappings.stream()
+					   .filter(mapping -> mapping.isSupported(request))
+					   .findFirst()
+					   .map(mapping -> mapping.getHandler(request))
+					   .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_HANDLER));
+	}
+}
