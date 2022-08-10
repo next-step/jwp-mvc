c275df796506b4999b90c1befed63fe503e4d386
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-11 02:24:32 +0900

    refactor: Legacy Mapping 주석 처리

--- src/main/java/core/mvc/asis/LegacyHandlerMapping.java
+++ src/main/java/core/mvc/asis/LegacyHandlerMapping.java
@@ -9,30 +9,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import core.mvc.HandlerMapping;
-import next.controller.CreateUserController;
-import next.controller.HomeController;
-import next.controller.ListUserController;
-import next.controller.LoginController;
-import next.controller.LogoutController;
-import next.controller.ProfileController;
-import next.controller.UpdateFormUserController;
-import next.controller.UpdateUserController;
 
 public class LegacyHandlerMapping implements HandlerMapping {
 	private static final Logger logger = LoggerFactory.getLogger(LegacyHandlerMapping.class);
 	private Map<String, Controller> mappings = new HashMap<>();
 
-	void initMapping() {
-		mappings.put("/", new HomeController());
-		mappings.put("/users/form", new ForwardController("/user/form.jsp"));
-		mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-		mappings.put("/users", new ListUserController());
-		mappings.put("/users/login", new LoginController());
-		mappings.put("/users/profile", new ProfileController());
-		mappings.put("/users/logout", new LogoutController());
-		mappings.put("/users/create", new CreateUserController());
-		mappings.put("/users/updateForm", new UpdateFormUserController());
-		mappings.put("/users/update", new UpdateUserController());
+	public void initMapping() {
+//		mappings.put("/", new HomeController());
+//		mappings.put("/users/form", new ForwardController("/user/form.jsp"));
+//		mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
+//		mappings.put("/users", new ListUserController());
+//		mappings.put("/users/login", new LoginController());
+//		mappings.put("/users/profile", new ProfileController());
+//		mappings.put("/users/logout", new LogoutController());
+//		mappings.put("/users/create", new CreateUserController());
+//		mappings.put("/users/updateForm", new UpdateFormUserController());
+//		mappings.put("/users/update", new UpdateUserController());
 
 		logger.info("Initialized Request Mapping!");
 		mappings.keySet().forEach(path -> {
@@ -40,7 +32,7 @@ public class LegacyHandlerMapping implements HandlerMapping {
 		});
 	}
 
-	void put(String url, Controller controller) {
+	private void put(String url, Controller controller) {
 		mappings.put(url, controller);
 	}
 
