a5e610a2e22f3b11a907dd312d61eed6f0ed61f6
thxwelchs <thxwelchs@gmail.com>
2022-08-11 17:52:13 +0900

    doc: Step1 요구사항 1 구현

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # 프레임워크 구현
+
 ## 진행 방법
+
 * 프레임워크 구현에 대한 요구사항을 파악한다.
 * 요구사항에 대한 구현을 완료한 후 자신의 github 아이디에 해당하는 브랜치에 Pull Request(이하 PR)를 통해 코드 리뷰 요청을 한다.
 * 코드 리뷰 피드백에 대한 개선 작업을 하고 다시 PUSH한다.
 * 모든 피드백을 완료하면 다음 단계를 도전하고 앞의 과정을 반복한다.
 
 ## 온라인 코드 리뷰 과정
+
 * [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/next-step/nextstep-docs/tree/master/codereview)
+
+# 요구사항 정리
+
+## Step1
+
+### 요구사항 1
+
+- 클래스 정보 출력
+  - src/test/java > next.reflection > ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +15,29 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        for (Field field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+            logger.debug("field name : {}, field type : {}", field.getName(), field.getType());
+        }
+
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            Class[] parameterTypes = constructor.getParameterTypes();
+            logger.debug("constructor paramer length : {}", parameterTypes.length);
+            for (Class paramType : parameterTypes) {
+                logger.debug("constructor param type : {}", paramType);
+            }
+        }
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            logger.debug("method name : {}", method.getName());
+            Class[] parameterTypes = method.getParameterTypes();
+            logger.debug("method parameter length : {}", parameterTypes.length);
+            for (Class paramType : parameterTypes) {
+                logger.debug("method param type : {}", paramType);
+            }
+            logger.debug("method return type : {}", method.getReturnType());
+        }
     }
 
     @Test
