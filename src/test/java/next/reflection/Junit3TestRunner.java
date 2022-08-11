80f16ccfbb0709cf30ec755a653b4b2fe4dcbd2a
etff <wish2rich@gmail.com>
2022-08-12 02:22:37 +0900

    Step1 자바 reflection (#203)
    * docs: step1 요구사항 정리
    
    * test: reflection test를 구현하라
    
    - field, constructor, method
    
    * test: test로 시작하는 메소드 실행
    
    * test: Test 애노테이션 메소드 실행
    
    * test: private field에 값 할당
    
    * test: 인자를 가진 생성자의 인스턴스 생성
    
    * test: component scan

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,17 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
-public class Junit3TestRunner {
+import java.lang.reflect.Method;
+
+class Junit3TestRunner {
     @Test
-    public void run() throws Exception {
+    void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            if (declaredMethod.getName().startsWith("test")) {
+                declaredMethod.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,19 +1,44 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
-    public void showClass() {
+    void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        logger.debug("name: {}", clazz.getName());
+
+        // 클래스의 모든 필드 출력
+        for (Field declaredField : clazz.getDeclaredFields()) {
+            logger.debug("declaredField: {}", declaredField.getName());
+        }
+
+        // 생성자 정보 출력
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            logger.debug("constructor {} ", constructor.getName());
+        }
+
+        // 메소드에 대한 정보를 출력
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            logger.debug("method {}", declaredMethod.getName());
+        }
     }
+
 
     @Test
     @SuppressWarnings("rawtypes")
@@ -27,5 +52,37 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Student student = new Student();
+        Field name = Student.class.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "test");
+        Field age = Student.class.getDeclaredField("age");
+        age.setAccessible(true);
+        age.set(student, 10);
+
+        assertThat(student.getName()).isEqualTo("test");
+        assertThat(student.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    void createConstructor() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<Question> clazz = Question.class;
+        Constructor<Question> declaredConstructor = clazz.getDeclaredConstructor(String.class, String.class, String.class);
+        Question question = declaredConstructor.newInstance("tester", "title", "content");
+
+        assertThat(question).isNotNull();
+    }
+
+    @Test
+    void componentScan() {
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        logger.debug("controller: {}", reflections.getTypesAnnotatedWith(Controller.class));
+        logger.debug("service: {}", reflections.getTypesAnnotatedWith(Service.class));
+        logger.debug("repository: {}", reflections.getTypesAnnotatedWith(Repository.class));
     }
 }
