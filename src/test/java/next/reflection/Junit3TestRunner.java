61ffa3b8e5d34a5ec984ba8d8e0f0a32e4974c7e
thxwelchs <thxwelchs@gmail.com>
2022-08-11 18:03:08 +0900

    doc: Step1 요구사항 2 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,32 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Junit3TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
     public void run() throws Exception {
+        Junit3Test junit3Test = new Junit3Test();
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        List<Method> onlyStartsWithTestMethods = Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        for (Method method : onlyStartsWithTestMethods) {
+            logger.debug("method name : {} ", method.getName());
+            method.invoke(junit3Test);
+        }
     }
 }
