d7c055e8aeffcf5b7e2ea671f8e1871750a2ad1d
thxwelchs <thxwelchs@gmail.com>
2022-08-11 18:26:43 +0900

    doc: Step1 요구사항 5 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,9 +6,14 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -72,5 +77,40 @@ public class ReflectionTest {
 
         assertThat(nameField.get(student)).isEqualTo("thxwelchs");
         assertThat(ageField.get(student)).isEqualTo(1);
+    }
+
+    @Test
+    void newInstance() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> questionClass = Question.class;
+
+        Constructor<Question> questionConstructor1 = questionClass.getDeclaredConstructor(String.class, String.class, String.class);
+        Constructor<Question> questionConstructor2 = questionClass.getDeclaredConstructor(long.class, String.class, String.class, String.class, Date.class, int.class);
+
+        long questionId = 1L;
+        String writer = "thxwelchs";
+        String title = "this is title";
+        String contents = "this is contents";
+        Date createdDate = Date.from(LocalDateTime.of(2022, 8, 11, 0, 0, 0).atZone(ZoneId.systemDefault()).toInstant());
+        int countOfComment = 100;
+
+        Question question1 = questionConstructor1.newInstance(writer, title, contents);
+        Question question2 = questionConstructor2.newInstance(questionId, writer, title, contents, createdDate, countOfComment);
+
+        assertAll(
+                () -> assertThat(question1.getQuestionId()).isZero(),
+                () -> assertThat(question1.getWriter()).isEqualTo(writer),
+                () -> assertThat(question1.getTitle()).isEqualTo(title),
+                () -> assertThat(question1.getContents()).isEqualTo(contents),
+                () -> assertThat(question1.getCountOfComment()).isZero()
+        );
+
+        assertAll(
+                () -> assertThat(question2.getQuestionId()).isEqualTo(1L),
+                () -> assertThat(question2.getWriter()).isEqualTo(writer),
+                () -> assertThat(question2.getTitle()).isEqualTo(title),
+                () -> assertThat(question2.getContents()).isEqualTo(contents),
+                () -> assertThat(question2.getCreatedDate()).isEqualTo(createdDate),
+                () -> assertThat(question2.getCountOfComment()).isEqualTo(100)
+        );
     }
 }
