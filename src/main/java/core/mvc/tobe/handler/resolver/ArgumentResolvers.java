1b3baabf4a0b8c037cf9645f91628630ea461370
mintjordy <mintjordy@woowahan.com>
2022-08-13 13:02:35 +0900

    feat: Annotation 기반 핸들러에서 requestParameter값의 이름으로 설정한 문자열 타입 메서드 인자를 받을 수 있도록 설정 추가

--- src/main/java/core/mvc/tobe/handler/resolver/ArgumentResolvers.java
+++ src/main/java/core/mvc/tobe/handler/resolver/ArgumentResolvers.java
@@ -1,22 +1,38 @@
 package core.mvc.tobe.handler.resolver;
 
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Objects;
 
 public class ArgumentResolvers {
 
+    private final ParameterNameDiscoverer parameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
     public Object[] resolveParameters(Method method, HttpServletRequest request, HttpServletResponse response) {
         Parameter[] parameters = method.getParameters();
+        String[] parameterNames = parameterNameDiscoverer.getParameterNames(method);
+
         Object[] arguments = new Object[parameters.length];
+
         for (int i = 0; i < parameters.length; i++) {
             Parameter parameter = parameters[i];
+            String parameterName = parameterNames[i];
+            String value = request.getParameter(parameterName);
+
             if (HttpServletRequest.class.isAssignableFrom(parameter.getType())) {
                 arguments[i] = request;
 
             } else if (HttpServletResponse.class.isAssignableFrom(parameter.getType())) {
                 arguments[i] = response;
+            } else if (Objects.nonNull(value)) {
+                if (String.class.isAssignableFrom(parameter.getType())) {
+                    arguments[i] = value;
+                }
             } else {
                 throw new NoExistsArgumentResolverException("Controller 실행에 필요한 매개변수에 값을 할당할 argumentResolver가 존재하지 않습니다.");
             }
--- src/test/java/core/mvc/tobe/handler/resolver/ArgumentResolversTest.java
+++ src/test/java/core/mvc/tobe/handler/resolver/ArgumentResolversTest.java
@@ -57,7 +57,7 @@ class ArgumentResolversTest {
                 .isEmpty();
     }
 
-    @DisplayName("메서드의 인가자 데이터 바인딩을 지원하지 않는 타입인 경우, 예외발생")
+    @DisplayName("메서드의 인자가 데이터 바인딩을 지원하지 않는 타입인 경우, 예외발생")
     @Test
     void resolveParameters_exception() {
         ArgumentResolvers argumentResolvers = new ArgumentResolvers();
@@ -66,6 +66,19 @@ class ArgumentResolversTest {
         assertThatThrownBy(() -> argumentResolvers.resolveParameters(methodWithRequestOrResponse, REQUEST, RESPONSE))
                 .isInstanceOf(NoExistsArgumentResolverException.class)
                 .hasMessage("Controller 실행에 필요한 매개변수에 값을 할당할 argumentResolver가 존재하지 않습니다.");
+    }
+
+    @DisplayName("메서드의 인자가 requestParameter에 동일한 이름으로 저장된 문자열인 경우 해당 requestParameter 값을 바인딩한다.")
+    @Test
+    void resolveParameters_string_value() {
+        MockHttpServletRequest requestWithParameter = new MockHttpServletRequest();
+        requestWithParameter.addParameter("name", "jordy");
+
+        ArgumentResolvers argumentResolvers = new ArgumentResolvers();
+        Method methodWithRequestOrResponse = findByClassAndMethodName("stringArgumentMethod", Tester.class);
+
+        Object[] actual = argumentResolvers.resolveParameters(methodWithRequestOrResponse, requestWithParameter, RESPONSE);
+        assertThat(actual).containsExactly("jordy");
     }
 
     private Method findByClassAndMethodName(String noArgumentMethod, Class<?> clazz) {
@@ -89,6 +102,10 @@ class ArgumentResolversTest {
         }
 
         public void requestArgumentMethod(HttpServletRequest request) {
+
+        }
+
+        public void stringArgumentMethod(String name) {
 
         }
 
