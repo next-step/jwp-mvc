a0d697d4b8d45d5f3c326d13990ebc3557256689
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-14 00:10:21 +0900

    feat (3.Execute Test Method): Execute method with 'test' annotation attached
    * Junit4TestRunner: Junit4Test 클래스에 있는 메서드 중에 @MyTest 애노테이션이 붙어있는 메서드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,21 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.newInstance();
+        List<String> methodNames = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class)).map(Method::getName).collect(Collectors.toList());
+        for (String methodName : methodNames) {
+            Method declaredMethod = clazz.getDeclaredMethod(methodName);
+            declaredMethod.invoke(junit4Test);
+        }
     }
 }
