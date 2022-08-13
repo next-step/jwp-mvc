c0b7b61c5edd2676cce5be5f2b0aa579e23c95eb
mintjordy <mintjordy@woowahan.com>
2022-08-13 16:56:00 +0900

    refactor: Controller 메서드 인자값 할당 로직에 ArgumentResolvers 적용 및 누락 테스트 추가

--- src/main/java/core/mvc/tobe/handler/resolver/ArgumentResolvers.java
+++ src/main/java/core/mvc/tobe/handler/resolver/ArgumentResolvers.java
@@ -7,11 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.Objects;
+import java.util.List;
 
 public class ArgumentResolvers {
 
     private final ParameterNameDiscoverer parameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
+    private final List<ArgumentResiolver> argumentResolvers = List.of(
+            new HttpServletArgumentResolver(),
+            new StringTypeRequestParameterArgumentResolver(),
+            new IntegerTypeRequestParameterArgumentResolver(),
+            new LongTypeRequestParameterArgumentResolver()
+    );
 
     public Object[] resolveParameters(Method method, HttpServletRequest request, HttpServletResponse response) {
         Parameter[] parameters = method.getParameters();
@@ -24,44 +31,17 @@ public class ArgumentResolvers {
             String parameterName = parameterNames[i];
             String value = request.getParameter(parameterName);
 
-            if (HttpServletRequest.class.isAssignableFrom(parameter.getType())) {
-                arguments[i] = request;
+            NamedParameter namedParameter = new NamedParameter(parameter, parameterName);
 
-            } else if (HttpServletResponse.class.isAssignableFrom(parameter.getType())) {
-                arguments[i] = response;
-            } else if (Objects.nonNull(value) && isDefaultType(parameter)) {
-                if (String.class.isAssignableFrom(parameter.getType())) {
-                    arguments[i] = value;
-                } else if (isIntOrLongType(parameter)) {
-                    if (isIntOrInteger(parameter)) {
-                        arguments[i] = Integer.parseInt(value);
-                    } else if(isLong(parameter)) {
-                        arguments[i] = Long.parseLong(value);
-                    }
-                }
-            } else {
-                throw new NoExistsArgumentResolverException("Controller 실행에 필요한 매개변수에 값을 할당할 argumentResolver가 존재하지 않습니다.");
-            }
+            Object argument = argumentResolvers.stream()
+                    .filter(resolver -> resolver.support(namedParameter))
+                    .map(resolver -> resolver.resolve(namedParameter, request, response))
+                    .findFirst()
+                    .orElseThrow(() -> new NoExistsArgumentResolverException("Controller 실행에 필요한 매개변수에 값을 할당할 argumentResolver가 존재하지 않습니다."));
+
+            arguments[i] = argument;
         }
 
         return arguments;
-    }
-
-    private boolean isDefaultType(Parameter parameter) {
-        return String.class.isAssignableFrom(parameter.getType()) || isIntOrLongType(parameter);
-    }
-
-    private boolean isIntOrLongType(Parameter parameter) {
-        return isIntOrInteger(parameter) || isLong(parameter);
-    }
-
-    private boolean isIntOrInteger(Parameter parameter) {
-        return Integer.TYPE.isAssignableFrom(parameter.getType()) ||
-                Integer.class.isAssignableFrom(parameter.getType());
-    }
-
-    private boolean isLong(Parameter parameter) {
-        return Long.TYPE.isAssignableFrom(parameter.getType()) ||
-                Long.class.isAssignableFrom(parameter.getType());
     }
 }
--- src/test/java/core/mvc/tobe/HandlerMethodArgumentResolverTest.java
+++ src/test/java/core/mvc/tobe/HandlerMethodArgumentResolverTest.java
@@ -1,12 +1,11 @@
 package core.mvc.tobe;
 
+import core.mvc.tobe.handler.mapping.HandlerExecution;
 import core.mvc.tobe.view.ModelAndView;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
-import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -14,31 +13,52 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HandlerMethodArgumentResolverTest {
-    private static final Logger logger = LoggerFactory.getLogger(HandlerMethodArgumentResolverTest.class);
+    private static final Class<?> CLAZZ = TestUserController.class;
 
-    private ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+    private HandlerExecution handlerExecution;
 
+    @DisplayName("문자열 인자에 requestParameter 값을 할당받은 Controller 메소드를 실행한다.")
     @Test
-    void string() throws Exception {
+    void create_string() throws Exception {
+        // given
         MockHttpServletRequest request = new MockHttpServletRequest();
         String userId = "javajigi";
         String password = "password";
         request.addParameter("userId", userId);
         request.addParameter("password", password);
 
-        Class clazz = TestUserController.class;
-        Method method = getMethod("create_string", clazz.getDeclaredMethods());
-        String[] parameterNames = nameDiscoverer.getParameterNames(method);
-        Object[] values = new Object[parameterNames.length];
-        for (int i = 0; i < parameterNames.length; i++) {
-            String parameterName = parameterNames[i];
-            logger.debug("parameter : {}", parameterName);
-            values[i] = request.getParameter(parameterName);
-        }
+        Object invoker = CLAZZ.getDeclaredConstructor().newInstance();
+        String testMethodName = "create_string";
+        Method method = getMethod(testMethodName, CLAZZ.getDeclaredMethods());
+        handlerExecution = new HandlerExecution(invoker, method);
 
-        ModelAndView mav = (ModelAndView) method.invoke(clazz.newInstance(), values);
-        assertThat(mav.getObject("userId")).isEqualTo(userId);
-        assertThat(mav.getObject("password")).isEqualTo(password);
+        // when
+        ModelAndView result = handlerExecution.handle(request, new MockHttpServletResponse());
+
+        // then
+        assertThat(result.getObject("userId")).isEqualTo(userId);
+        assertThat(result.getObject("password")).isEqualTo(password);
+    }
+
+    @DisplayName("숫자형 인자에 requestParameter 값을 할당받은 Controller 메소드를 실행한다.")
+    @Test
+    void create_int_long() throws Exception {
+        // given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter("id", "3000000000");
+        request.addParameter("age", "20");
+
+        Object invoker = CLAZZ.getDeclaredConstructor().newInstance();
+        String testMethodName = "create_int_long";
+        Method method = getMethod(testMethodName, CLAZZ.getDeclaredMethods());
+        handlerExecution = new HandlerExecution(invoker, method);
+
+        // when
+        ModelAndView result = handlerExecution.handle(request, new MockHttpServletResponse());
+
+        // then
+        assertThat(result.getObject("id")).isEqualTo(3000000000L);
+        assertThat(result.getObject("age")).isEqualTo(20);
     }
 
     private Method getMethod(String name, Method[] methods) {
