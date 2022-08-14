d293a3527e81198e58240dd8e9db4cb62ec71923
YONGJU <odetsuo@gmail.com>
2022-08-14 17:26:43 +0900

    refactor: 컨트롤러의 인자들을 필요한 파라미터로 변경

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -5,14 +5,12 @@ import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import core.db.DataBase;
 import core.mvc.ModelAndView;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView home() {
 
         return ModelAndView.jsp("home.jsp")
             .addObject("users", DataBase.findAll());
