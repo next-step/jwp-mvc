2f59766edd05fe3c10c2b8ef51bed7cb32e31ec1
Shin seongsu <tlstjdtn321@naver.com>
2022-08-16 04:15:08 +0900

    test: Reflection API를 이용해 private field에 값을 할당할 수 있다.

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +21,9 @@ import java.lang.reflect.Constructor;
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
     public static final String DELIMITER = ",";
+
+    private static final String NAME = "nextstep";
+    private static final int AGE = 10;
 
     @Test
     public void showClass() {
@@ -36,15 +40,38 @@ public class ReflectionTest {
         String methods = extractAndJoining(questionClass.getDeclaredMethods(), DELIMITER);
         String fields = extractAndJoining(questionClass.getDeclaredFields(), DELIMITER);
 
-        logger.info("생성자 목록: {}", constructors);
-        logger.info("메서드 목록: {}", methods);
-        logger.info("필드 목록: {}", fields);
+        assertThat(constructors).isEqualTo("next.reflection.Question,next.reflection.Question");
+        assertThat(methods).isEqualTo("equals,toString,hashCode,update,getContents,getQuestionId,getCreatedDate,getTimeFromCreateDate,getCountOfComment,getWriter,getTitle");
+        assertThat(fields).isEqualTo("questionId,writer,title,contents,createdDate,countOfComment");
     }
 
     private static String extractAndJoining(Member[] objects, String delimiter) {
         return Arrays.stream(objects)
             .map(Member::getName)
             .collect(Collectors.joining(delimiter));
+    }
+
+    @DisplayName("Reflection API를 이용해 private field에 값을 할당할 수 있다.")
+    @Test
+    public void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        Constructor<Student> constructor = clazz.getConstructor();
+
+        Student student = constructor.newInstance();
+
+        Field nameField = clazz.getDeclaredField("name");
+        Field ageField = clazz.getDeclaredField("age");
+
+        setField(nameField, student, NAME);
+        setField(ageField, student, AGE);
+
+        assertThat(student.getName()).isEqualTo(NAME);
+        assertThat(student.getAge()).isEqualTo(AGE);
+    }
+
+    private void setField(Field field, Object instance, Object value) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(instance, value);
     }
 
 }
