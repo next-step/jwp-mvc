bab8ddbe24b3917d4c4a04b638db489715e22dcf
Shin seongsu <tlstjdtn321@naver.com>
2022-08-16 04:30:32 +0900

    test: 인자를 가진 생성자의 인스턴스 생성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,8 +6,11 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -25,6 +28,8 @@ public class ReflectionTest {
     private static final String NAME = "nextstep";
     private static final int AGE = 10;
 
+    private static final String[] PARAM = new String[]{"nextstep", "리플렉션", "리플렉션 어떻게 하나요?"};
+
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
@@ -33,7 +38,7 @@ public class ReflectionTest {
 
     @DisplayName("Question클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
     @Test
-    public void printAllByQuestion() {
+    void printAllByQuestion() {
         Class<Question> questionClass = Question.class;
 
         String constructors = extractAndJoining(questionClass.getDeclaredConstructors(), DELIMITER);
@@ -53,7 +58,7 @@ public class ReflectionTest {
 
     @DisplayName("Reflection API를 이용해 private field에 값을 할당할 수 있다.")
     @Test
-    public void privateFieldAccess() throws Exception {
+    void privateFieldAccess() throws Exception {
         Class<Student> clazz = Student.class;
         Constructor<Student> constructor = clazz.getConstructor();
 
@@ -72,6 +77,29 @@ public class ReflectionTest {
     private void setField(Field field, Object instance, Object value) throws IllegalAccessException {
         field.setAccessible(true);
         field.set(instance, value);
+    }
+
+    @DisplayName("Reflection API를 활용해 Question 인스턴스를 생성한다.")
+    @Test
+    void createQuestionConstruct()
+        throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> clazz = Question.class;
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        Constructor<?> constructor = constructors[0];
+        Parameter[] parameters = constructor.getParameters();
+
+        Date now = new Date();
+
+        Object[] params = IntStream.range(0, parameters.length)
+            .mapToObj(idx -> parameters[idx].getType().cast(PARAM[idx]))
+            .toArray();
+
+
+        Question question1 = (Question) constructor.newInstance(params);
+        Question question2 = (Question) constructors[1].newInstance(1L, "nextstep", "리플렉션", "리플렉션은 어떻게사용하나요?", now, 10);
+
+        assertThat(question1).isEqualTo(new Question("nextstep", "리플렉션", "리플렉션 어떻게 하나요?"));
+        assertThat(question2).isEqualTo(new Question(1L, "nextstep", "리플렉션", "리플렉션은 어떻게사용하나요?", now, 10));
     }
 
 }
