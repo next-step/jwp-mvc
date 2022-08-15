50e53e67e51adee61b78bd5e7fa1cd6155146612
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-16 00:27:43 +0900

    feat (1. implement MVC framework): Implement MVC framework using reflection.
    * AnnotationHandlerMapping: reflection을 통해 @controller 애노테이션이 붙은 클래스를 찾고, 찾은 클래스로 HandlerExecutions Map 에 controller와 내부 메서드를 채워 넣는다.
    * HandlerExecution: 실행할 클래스와 메서드를 실행한다.

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,39 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private final Object[] basePackage;
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage, Scanners.TypesAnnotated);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
 
+        controllerClasses.forEach(controllerClass -> setHandlerExecutions(controllerClass, controllerClass.getDeclaredMethods()));
+    }
+
+    private void setHandlerExecutions(Class<?> controllerClass, Method[] declaredMethods) {
+        Arrays.stream(declaredMethods).forEach(declaredMethod -> {
+            RequestMapping requestMapping = declaredMethod.getAnnotation(RequestMapping.class);
+            handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMapping.method()), new HandlerExecution(controllerClass, declaredMethod));
+        });
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,20 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private final Class<?> controllerClass;
+    private final Method declaredMethods;
+
+    public HandlerExecution(Class<?> controllerClass, Method declaredMethods) {
+        this.controllerClass = controllerClass;
+        this.declaredMethods = declaredMethods;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        Constructor<?> constructor = controllerClass.getConstructor();
+        return (ModelAndView) declaredMethods.invoke(constructor.newInstance(), request, response);
     }
 }
