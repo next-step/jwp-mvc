cdf71290863fd9b82219bc332129120df1895bc5
wu2ee <gulb1602@gmail.com>
2022-08-15 19:49:51 +0900

    feat : 요구사항 3 - @MyTest 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,25 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Junit4TestRunner {
+    @DisplayName("Junit4Test 에서 @MyTest 애노테이션이 있는 메소드를 실행한다.")
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
+        List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        for (Method method : testMethods) {
+            method.invoke(junit4Test);
+        }
     }
 }
