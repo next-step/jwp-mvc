84cfbb608beec65dae6c4a07d23ccdb1fdd583fd
wu2ee <gulb1602@gmail.com>
2022-08-15 16:49:39 +0900

    feat : 요구사항 1 - Question 클래스 정보 출력

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,31 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
+    @DisplayName("Question 클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        logger.debug("----> Class Name          : {}", clazz.getName());
+        for (Field field : clazz.getDeclaredFields()) {
+            logger.debug("----> Class Fields        : {}", field);
+        }
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            logger.debug("----> Class Constructors  : {}", constructor);
+        }
+        for (Method method : clazz.getDeclaredMethods()) {
+            logger.debug("----> Class Methods       : {}", method);
+        }
     }
 
     @Test
