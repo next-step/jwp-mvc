4f3f947a9bcca8dd64e26f44f8d3ca958cf6bb28
koola97620 <koola97620@nate.com>
2022-08-15 17:01:12 +0900

    feat: @Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,32 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            executeTest(clazz, method);
+        }
+    }
+
+    private void executeTest(Class<Junit4Test> clazz, Method method) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Annotation[] annotations = method.getDeclaredAnnotations();
+        if (hasMyTest(annotations)) {
+            method.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
+    }
+
+    private boolean hasMyTest(Annotation[] annotations) {
+        return Arrays.stream(annotations)
+                .anyMatch(annotation -> annotation.annotationType().equals(MyTest.class));
     }
 }
