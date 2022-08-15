37998bdcc789ce6ffb5d6467185f245f9d893b6d
ohminho <ohminho@woowahan.com>
2022-08-15 15:46:25 +0900

    feat: Controller 에서 RequestMapping 을 찾아서 AnnotationHandlerMappingTest 성공 시킴

--- README.md
+++ README.md
@@ -17,3 +17,10 @@
 4. Student 클래스 private 필드에 값 할당
 5. Reflection API 를 통해 Question 인스턴스 생성
 6. core.di.factory.example 패키지에서 @Controller, @Service, @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력
+
+
+## Step2 - @MVC 구현
+
+1. @Contoller 어노테이션을 가진 클래스들을 찾아본다.
+2. @RequestMapping 어노테이션을 가진 메서드들을 찾아본다.
+3. @RequestMapping 메서드들의 요청 정보를 키로 핸들러를 저장해본다. 
--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,72 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private final Object[] basePackage;
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
     public void initialize() {
+        Set<Class<?>> controllers = findControllers();
 
+        Set<Method> methods = findRequestMappingMethods(controllers);
+
+        initHandlerExecutions(methods);
+    }
+
+    private Set<Method> findRequestMappingMethods(Set<Class<?>> controllers) {
+        return controllers.stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Class<?>> findControllers() {
+        return Arrays.stream(basePackage)
+                .map(Reflections::new)
+                .map(reflection -> reflection.getTypesAnnotatedWith(Controller.class))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+
+    private void initHandlerExecutions(Set<Method> methods) {
+        for (Method method : methods) {
+            putHandlerExecution(method);
+        }
+    }
+
+    private void putHandlerExecution(Method method) {
+        RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+
+        HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
+
+        HandlerExecution handlerExecution = new HandlerExecution(createHandlerInstance(method), method);
+
+        this.handlerExecutions.put(handlerKey, handlerExecution);
+    }
+
+    private Object createHandlerInstance(Method method) {
+        Class<?> declaringClass = method.getDeclaringClass();
+        try {
+            return declaringClass.newInstance();
+        } catch (Exception e) {
+            return new RuntimeException(e);
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
