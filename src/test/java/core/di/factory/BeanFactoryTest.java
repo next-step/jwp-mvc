48e6e3a0fd0dfcdc9834d357f0e5e21e0200d9b9
Shin seongsu <tlstjdtn321@naver.com>
2022-08-16 04:34:52 +0900

    test: @Controller, @Service, @Repository 클래스 찾는 테스트 코드 작성

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -4,15 +4,21 @@ import com.google.common.collect.Sets;
 import core.annotation.Repository;
 import core.annotation.Service;
 import core.annotation.web.Controller;
+import core.di.factory.example.JdbcQuestionRepository;
+import core.di.factory.example.JdbcUserRepository;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
@@ -26,6 +32,20 @@ public class BeanFactoryTest {
         Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
         beanFactory = new BeanFactory(preInstanticateClazz);
         beanFactory.initialize();
+    }
+
+    @DisplayName("core.di.factory.example 패키지에 있는 @Controller, @Service, @Repository 애노테이션이 설정되어 있는 클래스를 찾아 출력한다.")
+    @Test
+    void componentScan() {
+        Set<Class<?>> classes = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        classes.forEach(System.out::println);
+
+        assertThat(classes).containsExactlyInAnyOrder(
+            QnaController.class,
+            JdbcQuestionRepository.class,
+            JdbcUserRepository.class,
+            MyQnaService.class
+        );
     }
 
     @Test
