64d45de92d682f9d0a9b204c86e6bbbadc4d538d
miot2j <ck96117@gmail.com>
2022-08-17 01:40:30 +0900

    ✅ feat: 요구사항 1을 테스트 코드에 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +13,19 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        logger.debug("클래스의 모든 필드 정보: \n" + Arrays.toString(clazz.getDeclaredFields())
+            .replace(",", "\n")
+            .replace("next.reflection.", " ")
+            + "\n");
+        logger.debug("클래스의 모든 생성자 정보: \n" + Arrays.toString(clazz.getConstructors())
+            .replace("),", ")\n")
+            .replace("next.reflection.", " ")
+            + "\n");
+        logger.debug("클래스의 모든 메소드 정보: \n" + Arrays.toString(clazz.getDeclaredMethods())
+            .replace("),", ")\n")
+            .replace("next.reflection.", " ")
+            + "\n");
     }
 
     @Test
