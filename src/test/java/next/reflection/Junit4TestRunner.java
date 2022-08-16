a5bcb48504e89ea00c2b3a8b6867afac8436a775
JDragon <koola976@gmail.com>
2022-08-16 13:48:41 +0900

    Step1 - 자바 Reflection (#215)
    * feat: 클래스 정보 출력
    
    * feat: test로 시작하는 메소드 실행
    
    * feat: @Test 애노테이션 메소드 실행
    
    * refactor: isAnnotationPresent 사용
    
    * feat: private field 값 할당
    
    * feat: 인자를 가진 생성자의 인스턴스 생성
    
    * feat: componentScan

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,28 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            executeTest(clazz, method);
+        }
+    }
+
+    private void executeTest(Class<Junit4Test> clazz, Method method) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        if (hasMyTest(method)) {
+            method.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
+    }
+
+    private boolean hasMyTest(Method method) {
+        return method.isAnnotationPresent(MyTest.class);
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,30 @@
 package next.reflection;
 
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import core.di.factory.example.JdbcQuestionRepository;
+import core.di.factory.example.JdbcUserRepository;
+import core.di.factory.example.MyQnaService;
+import core.di.factory.example.QnaController;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +32,12 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        Arrays.stream(clazz.getDeclaredFields()).forEach(System.out::println);
+        System.out.println();
+        Arrays.stream(clazz.getConstructors()).forEach(System.out::println);
+        System.out.println();
+        Arrays.stream(clazz.getMethods()).forEach(System.out::println);
     }
 
     @Test
@@ -28,4 +53,59 @@ public class ReflectionTest {
             }
         }
     }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+
+        Student student = new Student();
+        name.set(student, "재성");
+        age.set(student, 20);
+
+        assertThat(student.getName()).isEqualTo("재성");
+        assertThat(student.getAge()).isEqualTo(20);
+    }
+
+    @Test
+    public void constructorWithParameters() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> clazz = Question.class;
+        Constructor constructor = findConstructorThreeParameters(clazz);
+
+        Question question = (Question) constructor.newInstance("writer", "title", "contents");
+
+        assertThat(question.getWriter()).isEqualTo("writer");
+        assertThat(question.getTitle()).isEqualTo("title");
+        assertThat(question.getContents()).isEqualTo("contents");
+    }
+
+    @Test
+    public void componentScan() {
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        Set<Class<?>> set = new HashSet<>();
+        set.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        set.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        set.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        assertThat(set).contains(
+                MyQnaService.class,
+                JdbcUserRepository.class,
+                QnaController.class,
+                JdbcQuestionRepository.class
+        );
+    }
+
+    private Constructor findConstructorThreeParameters(Class<Question> clazz) {
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        return Arrays.stream(constructors)
+                .filter(constructor -> constructor.getParameterTypes().length == 3)
+                .findAny()
+                .get();
+    }
+
 }
