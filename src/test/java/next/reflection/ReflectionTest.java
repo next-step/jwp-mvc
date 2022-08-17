ec5780d0a654098f7de7a76652d988cd919c426d
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-17 15:19:06 +0900

    feat: 클래스 정보 출력을 위한 show_class 테스트 구현 (요구사항 1)

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,18 +1,42 @@
 package next.reflection;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("Question 클래스의 모든 필드, 생성자, 메서드 정보 확인")
     @Test
-    public void showClass() {
+    public void show_class() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        assertAll(
+                () -> assertThat(clazz.getDeclaredFields())
+                        .extracting(Field::getName)
+                        .containsExactlyInAnyOrder("questionId", "writer", "title", "contents",
+                                "createdDate" ,"countOfComment"),
+                () -> assertThat(clazz.getDeclaredConstructors())
+                        .hasSize(2),
+                () -> assertThat(clazz.getDeclaredMethods())
+                        .extracting(Method::getName)
+                        .containsExactlyInAnyOrder("getQuestionId", "getWriter", "getTitle", "getContents",
+                                "getCreatedDate", "getTimeFromCreateDate", "getCountOfComment", "update",
+                                "toString", "hashCode", "equals")
+
+        );
     }
 
     @Test
