566e6846dc351d64d72c41aab5a92337693387fb
etff <wish2rich@gmail.com>
2022-08-17 21:03:49 +0900

    feat: annotation handler 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,16 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +22,24 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
+        for (Class<?> controllerClazz : controllers) {
+            try {
+                Object controller = controllerClazz.getDeclaredConstructor().newInstance();
+                Method[] methods = controllerClazz.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        RequestMapping mapping = method.getAnnotation(RequestMapping.class);
+                        handlerExecutions.put(new HandlerKey(mapping.value(), mapping.method()), new HandlerExecution(controller, method));
+                    }
+                }
+
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
