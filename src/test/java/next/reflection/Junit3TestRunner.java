783f55375ec5bf347fecf8f1bf05b33540f1734c
Shin seongsu <tlstjdtn321@naver.com>
2022-08-17 18:52:29 +0900

    🚀 1단계 - 자바 reflection (#220)
    * 요구사항 1 - 클래스 정보 출력
    
    * test: test로 시작하는 메소드 실행
    
    * test: @MyTest 애노테이션으로 설정되어 있는 메소드만 실행한다.
    
    * test: Reflection API를 이용해 private field에 값을 할당할 수 있다.
    
    * test: 인자를 가진 생성자의 인스턴스 생성
    
    * test: @Controller, @Service, @Repository 클래스 찾는 테스트 코드 작성
    
    * docs: 기능 문서 작성
    
    * fix: 멘토님께서 추천해주신 방식으로 하나의 로직으로 반복문 처리 하였습니다.
    
    * fix: 1. 구분자 없이 logger로 출력,  2. Question 비교 메서드 추가

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,38 @@
 package next.reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
+
+    private static final String START_METHOD_NAME = "test";
+
+    @DisplayName("JUnit3Test 클래스의 메서드 중에서 'test'로 시작하는 메서드만 실행한다.")
     @Test
-    public void run() throws Exception {
+    void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Constructor<Junit3Test> constructor = clazz.getConstructor();
+
+        List<Object> methods = Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.getName().startsWith(START_METHOD_NAME))
+            .map(method -> {
+                try {
+                    return method.invoke(constructor.newInstance());
+                } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .collect(Collectors.toList());
+
+        assertThat(methods).hasSize(2);
     }
 }
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,6 +1,23 @@
 package next.reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +25,12 @@ import java.lang.reflect.Constructor;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    public static final String DELIMITER = ",";
+
+    private static final String NAME = "nextstep";
+    private static final int AGE = 10;
+
+    private static final String[] PARAM = new String[]{"nextstep", "리플렉션", "리플렉션 어떻게 하나요?"};
 
     @Test
     public void showClass() {
@@ -15,17 +38,77 @@ public class ReflectionTest {
         logger.debug(clazz.getName());
     }
 
+    @DisplayName("Question클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
     @Test
-    @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
-        }
+    void printAllByQuestion() {
+        Class<Question> questionClass = Question.class;
+
+        List<String> constructors = extractAndJoining(questionClass.getDeclaredConstructors());
+        List<String> methods = extractAndJoining(questionClass.getDeclaredMethods());
+        List<String> fields = extractAndJoining(questionClass.getDeclaredFields());
+
+        logger.info("생성자 목록: {}", constructors);
+        logger.info("메서드 목록: {}", methods);
+        logger.info("필드 목록: {}", fields);
     }
+
+    private List<String> extractAndJoining(Member[] objects) {
+        return Arrays.stream(objects)
+            .map(Member::getName)
+            .collect(Collectors.toList());
+    }
+
+    @DisplayName("Reflection API를 이용해 private field에 값을 할당할 수 있다.")
+    @Test
+    void privateFieldAccess() throws Exception {
+        Class<Student> clazz = Student.class;
+        Constructor<Student> constructor = clazz.getConstructor();
+
+        Student student = constructor.newInstance();
+
+        Field nameField = clazz.getDeclaredField("name");
+        Field ageField = clazz.getDeclaredField("age");
+
+        setField(nameField, student, NAME);
+        setField(ageField, student, AGE);
+
+        assertThat(student.getName()).isEqualTo(NAME);
+        assertThat(student.getAge()).isEqualTo(AGE);
+    }
+
+    private void setField(Field field, Object instance, Object value) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(instance, value);
+    }
+
+    @DisplayName("Reflection API를 활용해 Question 인스턴스를 생성한다.")
+    @Test
+    void createQuestionConstruct()
+        throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> clazz = Question.class;
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        Constructor<?> constructor = constructors[0];
+        Parameter[] parameters = constructor.getParameters();
+
+        Date now = new Date();
+
+        Object[] params = IntStream.range(0, parameters.length)
+            .mapToObj(idx -> parameters[idx].getType().cast(PARAM[idx]))
+            .toArray();
+
+
+        Question question = (Question) constructor.newInstance(params);
+        Question questionAllConstruct = (Question) constructors[1].newInstance(1L, "nextstep", "리플렉션", "리플렉션은 어떻게사용하나요?", now, 10);
+
+        Question compareQuestion = (Question) new Question("nextstep", "리플렉션", "리플렉션 어떻게 하나요?");
+        Question compareQuestionAllConstruct = (Question) new Question(1L, "nextstep", "리플렉션", "리플렉션은 어떻게사용하나요?", now, 10);
+
+        validateEqulasQuestion(question, compareQuestion);
+        validateEqulasQuestion(questionAllConstruct, compareQuestionAllConstruct);
+    }
+
+    private void validateEqulasQuestion(Question question, Question compareQuestion) {
+        assertThat(question).isEqualTo(compareQuestion);
+    }
+
 }
