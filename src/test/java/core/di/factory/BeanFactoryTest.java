93e85b6511164a676d87fe9ade3cc9e43a6d8590
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-18 17:11:12 +0900

    feat: Controller, Service, Repository 어노테이션 설정된 클래스 찾는 테스트 구현 (요구사항 6)

--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -4,15 +4,20 @@ import com.google.common.collect.Sets;
 import core.annotation.Repository;
 import core.annotation.Service;
 import core.annotation.web.Controller;
+import core.di.factory.example.JdbcQuestionRepository;
+import core.di.factory.example.JdbcUserRepository;
 import core.di.factory.example.MyQnaService;
 import core.di.factory.example.QnaController;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BeanFactoryTest {
@@ -38,6 +43,19 @@ class BeanFactoryTest {
         MyQnaService qnaService = qnaController.getQnaService();
         assertNotNull(qnaService.getUserRepository());
         assertNotNull(qnaService.getQuestionRepository());
+    }
+
+    @DisplayName("core.di.factory.example 패키지에서 @Controller, @Service, @Repository 어노테이션 설정된 클래스 찾기")
+    @Test
+    void component_scan() {
+        assertAll(
+                () -> assertThat(reflections.getTypesAnnotatedWith(Controller.class))
+                        .containsExactly(QnaController.class),
+                () -> assertThat(reflections.getTypesAnnotatedWith(Service.class))
+                        .containsExactly(MyQnaService.class),
+                () -> assertThat(reflections.getTypesAnnotatedWith(Repository.class))
+                        .containsExactlyInAnyOrder(JdbcQuestionRepository.class, JdbcUserRepository.class)
+        );
     }
 
     @SuppressWarnings("unchecked")
