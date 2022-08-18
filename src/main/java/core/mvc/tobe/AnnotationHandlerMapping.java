9e7716196b78304de391550850fff3a0aa833d46
hongbin <fusis1@naver.com>
2022-08-18 20:38:58 +0900

    feat: 어노테이션기반 메서드 호출기능 개발

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,19 @@
 package core.mvc.tobe;
 
-import com.google.common.collect.Maps;
-import core.annotation.web.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+
+import org.reflections.Reflections;
+
+import com.google.common.collect.Maps;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +25,37 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        var reflections = new Reflections(basePackage);
+        var targetClasses = reflections.getTypesAnnotatedWith(Controller.class);
 
+        for (Class<?> targetClass : targetClasses) {
+            var targetObject = createTarget(targetClass);
+
+            var methods = Arrays.stream(targetClass.getMethods())
+                .filter(it -> it.getDeclaredAnnotation(RequestMapping.class) != null)
+                .collect(Collectors.toList());
+
+            var handlers = methods.stream()
+                .map(it -> Map.entry(createKey(it), new HandlerExecution(targetObject, it)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+            handlerExecutions.putAll(handlers);
+        }
+    }
+
+    private HandlerKey createKey(Method it) {
+        var declaredAnnotation = it.getDeclaredAnnotation(RequestMapping.class);
+
+        return new HandlerKey(declaredAnnotation.value(), declaredAnnotation.method());
+    }
+
+    private Object createTarget(Class<?> targetClass) {
+        try {
+            return targetClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("해당 클래스를 생성할 수 없습니다. " + targetClass.getName(), e);
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
