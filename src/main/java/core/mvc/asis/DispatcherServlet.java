226285751ace1b6e22fa2d2dbf63667d92558c59
Shin seongsu <tlstjdtn321@naver.com>
2022-08-18 17:04:34 +0900

    feat: annotation scan후, url 매핑된 handler 반환

--- src/main/java/core/mvc/asis/DispatcherServlet.java
+++ src/main/java/core/mvc/asis/DispatcherServlet.java
@@ -1,5 +1,13 @@
 package core.mvc.asis;
 
+import core.mvc.ModelAndView;
+import core.mvc.View;
+import core.mvc.tobe.AnnotationHandlerMapping;
+import core.mvc.tobe.HandlerExecution;
+import core.mvc.tobe.HandlerMapping;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,12 +25,22 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
+    private static final String NOT_FOUND_URL = "존재하지 않는 URL입니다.";
+
     private RequestMapping rm;
+    private AnnotationHandlerMapping annotationHandlerMapping;
+    private List<HandlerMapping> mappings = new ArrayList<>();
 
     @Override
     public void init() throws ServletException {
         rm = new RequestMapping();
         rm.initMapping();
+
+        annotationHandlerMapping = new AnnotationHandlerMapping();
+        annotationHandlerMapping.initialize();
+
+        mappings.add(rm);
+        mappings.add(annotationHandlerMapping);
     }
 
     @Override
@@ -30,12 +48,14 @@ public class DispatcherServlet extends HttpServlet {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
-        Controller controller = rm.findController(requestUri);
+        rm.findController(requestUri);
+        Object handler = getHandler(req);
         try {
-            String viewName = controller.execute(req, resp);
-            move(viewName, req, resp);
+            ModelAndView mav = execute(handler, req, resp);
+            View view = mav.getView();
+            view.render(mav.getModel(), req, resp);
         } catch (Throwable e) {
-            logger.error("Exception : {}", e);
+            logger.error("Exception : {}", e.getMessage());
             throw new ServletException(e.getMessage());
         }
     }
@@ -50,4 +70,20 @@ public class DispatcherServlet extends HttpServlet {
         RequestDispatcher rd = req.getRequestDispatcher(viewName);
         rd.forward(req, resp);
     }
+
+    private ModelAndView execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if (handler instanceof Controller) {
+            return ((Controller) handler).execute(req, resp);
+        }
+        return ((HandlerExecution) handler).handle(req, resp);
+    }
+
+    private Object getHandler(HttpServletRequest req) {
+        return mappings.stream()
+            .map(handlerMapping -> handlerMapping.getHandler(req))
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_URL));
+    }
+
 }
