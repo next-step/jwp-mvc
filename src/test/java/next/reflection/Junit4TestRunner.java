7d2ff7629ce48ca780dc9014ee2a85736bcd22c6
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-18 13:43:48 +0900

    feat: Test 어노테이션이 붙은메서드만 실행하는 테스트 구현 (요구사항 3)

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,35 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class Junit4TestRunner {
+
+    @DisplayName("Junit4Test에서 @MyTest 어노테이션이 있는 메서드만 실행")
     @Test
-    public void run() throws Exception {
+    void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+        List<Method> methodsContainAnnotation = Stream.of(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        for (Method method : methodsContainAnnotation) {
+            method.invoke(junit4Test);
+        }
+
+        assertAll(
+                () -> assertThat(methodsContainAnnotation.size()).isEqualTo(2),
+                () -> assertThat(methodsContainAnnotation.get(0).getAnnotation(MyTest.class)).isNotNull(),
+                () -> assertThat(methodsContainAnnotation.get(1).getAnnotation(MyTest.class)).isNotNull()
+        );
     }
 }
