ee5099a91a4747f0cf0ea01f9f26b88b8c8b5ca2
wu2ee <gulb1602@gmail.com>
2022-08-19 01:49:53 +0900

    feat : 요구사항 1 - 애노테이션 기반 MVC 프레임워크

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,18 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,7 +24,29 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        new Reflections(this.basePackage)
+                .getTypesAnnotatedWith(Controller.class)
+                .forEach(this::setHandlerExecutions);
+    }
 
+    private void setHandlerExecutions(Class<?> controllerClass) {
+        try {
+            Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+            List<Method> methods = getRequestMappingMethods(controllerClass);
+            methods.forEach(method -> handlerExecutions.put(createHandlerKey(method.getAnnotation(RequestMapping.class)), new HandlerExecution(controllerInstance, method)));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Method> getRequestMappingMethods(Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private HandlerKey createHandlerKey(RequestMapping requestMapping) {
+        return new HandlerKey(requestMapping.value(), requestMapping.method());
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
