8cc1d3dd810b33fa0baa5be748bbf26bb2e84bf7
kyun <kyun@dunamu.dev>
2022-08-18 21:25:10 +0900

    Step 1 (#221)
    * test: 자바 reflection 학습을 위한 test code 작성
    
    * refactor: 테스트코드 리팩토링

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -1,11 +1,18 @@
 package next.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 public class Junit3TestRunner {
     @Test
-    public void run() throws Exception {
+    public void run() throws InstantiationException, IllegalAccessException, InvocationTargetException {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,17 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
