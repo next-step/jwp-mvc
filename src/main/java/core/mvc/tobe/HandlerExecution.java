2bfee7e6e1e1b07d228d44aa81bcc9b452b720a6
Shin seongsu <tlstjdtn321@naver.com>
2022-08-18 17:05:48 +0900

    feat: 리플레션으로 annotation 초기화

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,21 @@ package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
 
+import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HandlerExecution {
+
+    private Object declaredObject;
+    private Method method;
+
+    public HandlerExecution(Object declaredObject, Method method) {
+        this.declaredObject = declaredObject;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(declaredObject, request, response);
     }
 }
