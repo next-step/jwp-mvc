6ad18fa9b3d4ad799766f4b2fc18f90d01b0c80b
Jae Eon <45224710+Miot2J@users.noreply.github.com>
2022-08-19 20:06:31 +0900

    Step1 java reflection (#222)
    * 📝 docs: 요구사항 1에 대한 README 작성
    
    * ✅ feat: 요구사항 1을 테스트 코드에 구현
    
    * 📝 docs: 요구사항 2 README 작성
    
    * ✨ feat: 클래스의 모든 메소드를 순회하며 test로 시작하는 메소드 실행하는 로직 구현
    
    * 📝 docs: 요구사항 3 README 작성
    
    * ✨feat: 요구사항3 구현
    
    * 📝 docs: 요구사항 4 README 작성
    
    * ✨ feat: 요구사항4 구현(Reflection API를 활용해 private field 할당)
    
    * 📝 docs: 요구사항 5 README 작성
    
    * ✨ feat: 요구사항 5 구현
    
    * 📝 docs: 요구사항 6 README 작성
    
    * ✨ feat: 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력하는 componentScan() 구현

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # 프레임워크 구현
-## 진행 방법
-* 프레임워크 구현에 대한 요구사항을 파악한다.
-* 요구사항에 대한 구현을 완료한 후 자신의 github 아이디에 해당하는 브랜치에 Pull Request(이하 PR)를 통해 코드 리뷰 요청을 한다.
-* 코드 리뷰 피드백에 대한 개선 작업을 하고 다시 PUSH한다.
-* 모든 피드백을 완료하면 다음 단계를 도전하고 앞의 과정을 반복한다.
 
-## 온라인 코드 리뷰 과정
-* [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/next-step/nextstep-docs/tree/master/codereview)
+## 요구사항 1 - 클래스 정보 출력
+
+- [x] 클래스의 모든 필드를 출력한다.
+- [x] 클래스의 모든 생성자를 출력한다.
+- [x] 클래스의 모든 메소드를 출력한다.
+
+## 요구사항 2 - `test`로 시작하는 메소드만 Java Reflection을 활용해 실행하도록 구현
+
+- [x] Junit3Test 클래스의 모든 메소드 중 test로 시작하는 메소드만 실행된다.
+
+## 요구사항 3 - `@Test` 애노테이션이 설정되어 있는 메소드를 자동으로 실행하도록 구현
+
+- [x] Junit4Test 클래스에서 @MyTest 애노테이션으로 설정되어 있는 메소드만 Java Reflection을 활용해 실행하도록 구현한다.
+
+## 요구사항4 - private field에 값 할당
+
+- [x] Student 클래스의 name과 age 필드에 값을 할당한 후 getter 메소드를 통해 값을 확인
+
+## 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
+
+- [x] Question 클래스의 인스턴스를 자바 Reflection API를 활용해 Question 인스턴스를 생성
+
+## - 요구사항 6 - component scan 구현
+
+- [ ] @Controller, @Service, @Repository 애노테이션이 설정되어 있는 모든 클래스를 찾아 출력
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Date;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,20 +15,56 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        logger.debug("클래스의 모든 필드 정보: \n" + Arrays.toString(clazz.getDeclaredFields())
+            .replace(",", "\n")
+            .replace("next.reflection.", " ")
+            + "\n");
+        logger.debug("클래스의 모든 생성자 정보: \n" + Arrays.toString(clazz.getConstructors())
+            .replace("),", ")\n")
+            .replace("next.reflection.", " ")
+            + "\n");
+        logger.debug("클래스의 모든 메소드 정보: \n" + Arrays.toString(clazz.getDeclaredMethods())
+            .replace("),", ")\n")
+            .replace("next.reflection.", " ")
+            + "\n");
     }
 
     @Test
     @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
+        Constructor[] constructors = clazz.getDeclaredConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+            logger.debug(constructor.toString());
+
+            if (parameterTypes.length == 3) {
+                Object question = constructor.newInstance("작성자", "제목", "내용");
+                logger.debug(question.toString());
+            }
+
+            if (parameterTypes.length == 6) {
+                Object question = constructor.newInstance(1, "작성자", "제목", "내용", new Date(), 1);
+                logger.debug(question.toString());
             }
         }
+    }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+
+        Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+        Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+
+        Student student = new Student();
+        ageField.set(student, 27);
+        nameField.set(student, "jason");
+
+        logger.debug(student.toString());
+        logger.debug(clazz.getName());
     }
 }
