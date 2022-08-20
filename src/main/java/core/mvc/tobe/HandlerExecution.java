eee0c3d44f1bc57169877d4c783741fb3581e0d4
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-20 19:17:36 +0900

    feat (1-3. Return controller as requested): Return controller as requested
    * HandlerExecution: Class<?> 대신 Object (Class) 형태로 전달하는 방식으로 수정함에 따라 인스턴스 변수 수정

--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -4,27 +4,19 @@ import core.mvc.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
-    private final Class<?> controllerClass;
-    private final Method declaredMethods;
+    private final Object declaredObject;
+    private final Method method;
 
-    // 임시 생성자 생성
-    public HandlerExecution(Object controllerClass, Method declaredMethods) {
-        this.controllerClass = null;
-        this.declaredMethods = declaredMethods;
-    }
-
-    public HandlerExecution(Class<?> controllerClass, Method declaredMethods) {
-        this.controllerClass = controllerClass;
-        this.declaredMethods = declaredMethods;
+    public HandlerExecution(Object declaredObject, Method method) {
+        this.declaredObject = declaredObject;
+        this.method = method;
     }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Constructor<?> constructor = controllerClass.getConstructor();
-        return getModelAndView(declaredMethods.invoke(constructor.newInstance(), request, response));
+        return getModelAndView(method.invoke(declaredObject, request, response));
     }
 
     private ModelAndView getModelAndView(Object invokeObject) {
