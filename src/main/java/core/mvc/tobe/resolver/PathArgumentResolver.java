faae143bff1b7cb4ad40595bab1a684bf33142f8
황정현 <jhhwang@wemakeprice.com>
2022-08-20 16:57:48 +0900

    feat pathvariable resolver

--- src/main/java/core/mvc/tobe/resolver/PathArgumentResolver.java
+++ src/main/java/core/mvc/tobe/resolver/PathArgumentResolver.java
@@ -1,2 +1,42 @@
-package core.mvc.tobe.resolver;public class PathArgumentResolver {
+package core.mvc.tobe.resolver;
+
+import core.mvc.tobe.resolver.method.MethodParameter;
+import org.springframework.http.server.PathContainer;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+public class PathArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasAnnotationWithPathVariable();
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, HttpServletRequest request, HttpServletResponse response) {
+        // /user/id
+        String path = methodParameter.pathVariableValue();
+        String parameterName = methodParameter.getParameterName();
+        Map<String, String> variables = parse(path)
+                .matchAndExtract(toPathContainer(request.getServletPath())).getUriVariables();
+        return variables.get(parameterName);
+    }
+
+    private PathPattern parse(String path) {
+        PathPatternParser pp = new PathPatternParser();
+        pp.setMatchOptionalTrailingSeparator(true);
+        return pp.parse(path);
+    }
+
+    private static PathContainer toPathContainer(String path) {
+        if (path == null) {
+            return null;
+        }
+
+        return PathContainer.parsePath(path);
+    }
 }
