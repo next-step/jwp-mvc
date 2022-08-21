0e763d812be0a5dbf05714e3064e24447eda9e82
Shin seongsu <tlstjdtn321@naver.com>
2022-08-22 08:42:38 +0900

    🚀 2단계 - @MVC 구현 (#224)
    * 요구사항 1 - 클래스 정보 출력
    
    * test: test로 시작하는 메소드 실행
    
    * test: @MyTest 애노테이션으로 설정되어 있는 메소드만 실행한다.
    
    * test: Reflection API를 이용해 private field에 값을 할당할 수 있다.
    
    * test: 인자를 가진 생성자의 인스턴스 생성
    
    * test: @Controller, @Service, @Repository 클래스 찾는 테스트 코드 작성
    
    * docs: 기능 문서 작성
    
    * fix: 멘토님께서 추천해주신 방식으로 하나의 로직으로 반복문 처리 하였습니다.
    
    * fix: 1. 구분자 없이 logger로 출력,  2. Question 비교 메서드 추가
    
    * docs: 기능 문서 작성
    
    * feat: jsp forward, redirect 설정
    
    * feat: forwardController 수정
    
    * feat: Controller ModelAndView 반환값 수정
    
    * feat: annotation scan후, url 매핑된 handler 반환
    
    * feat: 리플레션으로 annotation 초기화
    
    * feat: 기존 매핑 방식 수정
    
    * feat: user Controller, home controller 추가
    
    * fix: 기존 Controller 삭제
    
    * fix: 일급 객체 생성
    
    * fix: 메소드 분리 및 newInstance 수정

--- src/main/java/core/mvc/asis/ForwardController.java
+++ src/main/java/core/mvc/asis/ForwardController.java
@@ -1,20 +1,24 @@
 package core.mvc.asis;
 
+import core.mvc.JspView;
+import core.mvc.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ForwardController implements Controller {
+    private static final String EMPTY_URL = "forwardUrl is null. 이동할 URL을 입력하세요.";
+
     private String forwardUrl;
 
     public ForwardController(String forwardUrl) {
         this.forwardUrl = forwardUrl;
         if (forwardUrl == null) {
-            throw new NullPointerException("forwardUrl is null. 이동할 URL을 입력하세요.");
+            throw new NullPointerException(EMPTY_URL);
         }
     }
 
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        return forwardUrl;
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        return new ModelAndView(new JspView(forwardUrl));
     }
 }
--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,22 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -16,7 +26,53 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        try {
+            initializeHandlerExecution(reflections);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("AnnotationHandlerMapping initiallize Exception!! : {}", e.getMessage());
+        }
+    }
 
+    private void initializeHandlerExecution(Reflections reflections)
+        throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerClass : controllerClasses) {
+            Controller controllerAnnotation = controllerClass.getAnnotation(Controller.class);
+            String controllerLevelPath = controllerAnnotation.value();
+
+            requestMethodScan(controllerLevelPath, controllerClass);
+        }
+    }
+
+    private void requestMethodScan(String controllerLevelPath, Class<?> controllerClass)
+        throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+        Method[] methods = controllerClass.getDeclaredMethods();
+        for (Method method : methods) {
+            addHendlerExecution(controllerInstance, controllerLevelPath, method);
+        }
+    }
+
+    private void addHendlerExecution(Object controllerInstance, String controllerLevelPath, Method method) {
+        if(isExistAnnotaionPresent(method)) {
+            RequestMapping requestMapping = getRequestMapping(method);
+            addHandlerExecution(controllerInstance, controllerLevelPath, method, requestMapping);
+        }
+    }
+
+    private boolean isExistAnnotaionPresent(Method method) {
+        return method.isAnnotationPresent(RequestMapping.class);
+    }
+
+    private RequestMapping getRequestMapping(Method method) {
+        return method.getAnnotation(RequestMapping.class);
+    }
+
+    private void addHandlerExecution(Object controllerInstance, String controllerLevelPath, Method method, RequestMapping requestMapping) {
+        HandlerKey handlerKey = new HandlerKey(controllerLevelPath + requestMapping.value(), requestMapping.method());
+        handlerExecutions.put(handlerKey, new HandlerExecution(controllerInstance, method));
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
