24074bb4f31a4f75e05e14e1353dd968c4ef74d1
fistkim101 <fistkim101@gmail.com>
2022-08-23 18:29:34 +0900

    test : @MVC tobe 검증

--- src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
+++ src/test/java/core/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,6 +1,9 @@
 package core.mvc.tobe;
 
+import core.annotation.web.RequestMethod;
+import core.configuration.ApplicationContext;
 import core.db.DataBase;
+import core.web.view.ModelAndView;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +13,14 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnnotationHandlerMappingTest {
-    private AnnotationHandlerMapping handlerMapping;
+    private AnnotationHandlerMapping annotationHandlerMapping;
 
     @BeforeEach
     public void setup() {
-        handlerMapping = new AnnotationHandlerMapping("core.mvc.tobe");
-        handlerMapping.initialize();
+        ApplicationContext.getInstance().init();
+
+        annotationHandlerMapping = new AnnotationHandlerMapping();
+        annotationHandlerMapping.init();
     }
 
     @Test
@@ -25,22 +30,24 @@ public class AnnotationHandlerMappingTest {
         assertThat(DataBase.findUserById(user.getUserId())).isEqualTo(user);
 
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users");
+        HandlerKey handlerKey = new HandlerKey("/users", RequestMethod.GET);
         request.setParameter("userId", user.getUserId());
         MockHttpServletResponse response = new MockHttpServletResponse();
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        execution.handle(request, response);
+        HandlerExecution execution = (HandlerExecution) annotationHandlerMapping.getHandler(handlerKey);
+        ModelAndView modelAndView = execution.handle(request, response);
 
-        assertThat(request.getAttribute("user")).isEqualTo(user);
+        assertThat(modelAndView.getModel().get("user")).isEqualTo(user);
     }
 
     private void createUser(User user) throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users");
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users/create");
+        HandlerKey handlerKey = new HandlerKey("/users/create", RequestMethod.POST);
         request.setParameter("userId", user.getUserId());
         request.setParameter("password", user.getPassword());
         request.setParameter("name", user.getName());
         request.setParameter("email", user.getEmail());
         MockHttpServletResponse response = new MockHttpServletResponse();
-        HandlerExecution execution = handlerMapping.getHandler(request);
+        HandlerExecution execution = (HandlerExecution) annotationHandlerMapping.getHandler(handlerKey);
         execution.handle(request, response);
     }
 }
