b1a8120c19b09cdfcc9a29bf5d101cb165d141b6
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-23 22:58:49 +0900

    feat: 매개변수 예외처리 추가

--- src/main/java/core/mvc/tobe/ForwardControllerAdapter.java
+++ src/main/java/core/mvc/tobe/ForwardControllerAdapter.java
@@ -2,6 +2,7 @@ package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
 import core.mvc.asis.ForwardController;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ public class ForwardControllerAdapter implements HandlerAdapter {
 
     @Override
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Assert.notNull(handler, "Handler가 null이어선 안됩니다.");
         ForwardController controller = (ForwardController) handler;
         return controller.execute(request, response);
     }
