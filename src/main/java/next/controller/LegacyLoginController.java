34580aa7335ac5142191c30a6a574d8bd26a500c
wu2ee <gulb1602@gmail.com>
2022-08-25 01:09:33 +0900

    refactor : Controller 인터페이스 메서드 반환 타입 ModelAndView -> String 로 재변경 (중복 로직 최소화)

--- src/main/java/next/controller/LegacyLoginController.java
+++ src/main/java/next/controller/LegacyLoginController.java
@@ -12,21 +12,21 @@ import javax.servlet.http.HttpSession;
 
 public class LegacyLoginController implements Controller {
     @Override
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String userId = req.getParameter("userId");
         String password = req.getParameter("password");
         User user = DataBase.findUserById(userId);
         if (user == null) {
             req.setAttribute("loginFailed", true);
-            return new ModelAndView(new ResourceView("/user/login.jsp"));
+            return "/user/login.jsp";
         }
         if (user.matchPassword(password)) {
             HttpSession session = req.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
-            return new ModelAndView(new ResourceView("redirect:/"));
+            return "redirect:/";
         } else {
             req.setAttribute("loginFailed", true);
-            return new ModelAndView(new ResourceView("/user/login.jsp"));
+            return "/user/login.jsp";
         }
     }
 }
