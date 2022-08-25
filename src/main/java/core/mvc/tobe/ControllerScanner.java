b6a59fa37c11ed78f29533e83f30d8e5a31f8639
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-25 19:19:41 +0900

    feat: ControllerScanner 구현 (요구사항 1)

---
+++ src/main/java/core/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,41 @@
+package core.mvc.tobe;
+
+import core.annotation.web.Controller;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ControllerScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
+
+    private final Reflections reflections;
+
+    public ControllerScanner(Object... basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        try {
+            return instantiateControllers(controllerClasses);
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            logger.error("Fail to instantiate controllers : {}", e.getMessage());
+        }
+        return null;
+    }
+
+    private Map<Class<?>, Object> instantiateControllers(Set<Class<?>> controllerClasses)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Map<Class<?>, Object> controllers = new HashMap<>();
+        for (Class<?> controllerClass : controllerClasses) {
+            Object instance = controllerClass.getConstructor().newInstance();
+            controllers.put(controllerClass, instance);
+        }
+        return controllers;
+    }
+}
