8d4f6abc969c2cca0cb68f8ec6373afb7c11de6d
hiro <kiryuinh32@gmail.com>
2022-08-25 21:09:20 +0900

    show class info

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,14 +5,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
-    public void showClass() {
+    void showClass() {
         Class<Question> clazz = Question.class;
+
+        /**
+         *  클래스, 모든 필드, 생성자, 메소드에 대한 정보
+         */
         logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getFields();
+
+        for (Field field : fields) {
+            logger.debug("## field name: {}, field type: {}", field.getName(), field.getType());
+        }
+
+        Constructor<?>[] constructors = clazz.getConstructors();
+
+        for (Constructor<?> constructor : constructors) {
+            logger.debug("## constructor parameter count: {}", constructor.getParameterCount());
+        }
+
+        Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            logger.debug("## method name: {}", method.getName());
+            logger.debug("## method return type: {}", method.getReturnType());
+        }
+
     }
 
     @Test
