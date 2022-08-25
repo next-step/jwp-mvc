2e8915b959ae4ae8912916be2f36717a43286242
hiro <kiryuinh32@gmail.com>
2022-08-25 21:41:54 +0900

    component scan test

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,11 +6,12 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ReflectionTest {
+class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
@@ -44,7 +45,7 @@ public class ReflectionTest {
     }
 
     @Test
-    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
         Student student = new Student();
         Class<Student> studentClass = Student.class;
 
@@ -62,8 +63,28 @@ public class ReflectionTest {
     }
 
     @Test
+    void new_instance() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<?>[] declaredConstructors = Question.class.getDeclaredConstructors();
+
+        Class<Question> questionClass = Question.class;
+
+        Constructor<Question> constructor = questionClass.getConstructor(String.class, String.class, String.class);
+
+        String name = "my name";
+        String title = "my title";
+        String content = "my content";
+
+        Question question = constructor.newInstance(name, title, content);
+
+        assertThat(question.getWriter()).isEqualTo(name);
+        assertThat(question.getTitle()).isEqualTo(title);
+        assertThat(question.getContents()).isEqualTo(content);
+
+    }
+
+    @Test
     @SuppressWarnings("rawtypes")
-    public void constructor() throws Exception {
+    void constructor() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
