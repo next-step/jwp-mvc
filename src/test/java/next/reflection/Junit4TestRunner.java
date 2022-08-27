ca02cdfe7510d8d59bbf103e5323d4d073d3f7b5
akayj820 <akayj820@gmail.com>
2022-08-28 01:48:10 +0900

    feat: 요구사항 3 - @Test 애노테이션 메서드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,24 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method m : declaredMethods) {
+            if (containsMyTestAnnotation(m)) {
+                m.invoke(new Junit4Test());
+            }
+        }
+    }
+
+    public boolean containsMyTestAnnotation(Method method) {
+        Annotation[] annotations = method.getDeclaredAnnotations();
+        return Arrays.stream(annotations).anyMatch(a -> a.annotationType().getSimpleName().equals("MyTest"));
     }
 }
