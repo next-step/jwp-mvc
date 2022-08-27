21f8e3a4eaac00cd7f7fe1b83833eea94572f584
akayj820 <akayj820@gmail.com>
2022-08-28 01:29:48 +0900

    refactor: 요구사항 2 - run() 메서드 Junit3TestRunner 클래스로 이동

--- src/test/java/next/reflection/Junit3Test.java
+++ src/test/java/next/reflection/Junit3Test.java
@@ -1,22 +1,6 @@
 package next.reflection;
 
-import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class Junit3Test {
-    @Test
-    public void runner() throws InvocationTargetException, IllegalAccessException {
-        Class<Junit3Test> clazz = Junit3Test.class;
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method m : methods) {
-            if (m.getName().startsWith("test")) {
-                m.invoke(new Junit3Test());
-            }
-        }
-    }
-
     public void test1() throws Exception {
         System.out.println("Running Test1");
     }
