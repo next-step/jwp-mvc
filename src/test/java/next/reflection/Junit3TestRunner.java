031c47999d00c37c725e311381fc5e97fa4e141e
이정수 <lsecret@naver.com>
2022-08-27 11:59:21 +0900

    feat: test로 시작하는 메소드 실행

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,20 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            runtMethod(clazz, method);
+        }
+    }
+
+    private void runtMethod(Class<Junit3Test> clazz, Method method) throws Exception {
+        if (method.getName().startsWith("test")) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
