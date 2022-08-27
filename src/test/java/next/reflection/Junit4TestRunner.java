a73c8b7af623a380dfc8f72e62d714fa9f623e16
이정수 <lsecret@naver.com>
2022-08-27 12:03:57 +0900

    feat: @MyTest 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,22 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            runMethodByMyTestAnnotation(clazz, method);
+        }
+    }
+
+    private void runMethodByMyTestAnnotation(Class<Junit4Test> clazz, Method declaredMethod)
+            throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (declaredMethod.isAnnotationPresent(MyTest.class)) {
+            declaredMethod.invoke(clazz.newInstance());
+        }
     }
 }
