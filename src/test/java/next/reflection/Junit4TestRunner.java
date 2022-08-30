d011e302c9b5a3db92af320b952a0eefa23da339
akayj820 <33502292+akayj820@users.noreply.github.com>
2022-08-30 13:12:17 +0900

    1단계 - 자바 reflection (#251)
    * feat: 요구사항 1 - 클래스 정보 출력
    
    * feat: 요구사항 2 - test로 시작하는 메소드 실행
    
    * refactor: 요구사항 2 - run() 메서드 Junit3TestRunner 클래스로 이동
    
    * feat: 요구사항 3 - @Test 애노테이션 메서드 실행
    
    * refactor: 요구사항 3 - isAnnotationPresent 함수 활용
    
    * feat: 요구사항 4 - private field에 값 할당
    
    * feat: 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
    
    * feat: 요구사항 6 - component scan

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,18 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method m : declaredMethods) {
+            if (m.isAnnotationPresent(MyTest.class)) {
+                m.invoke(new Junit4Test());
+            }
+        }
     }
 }
+
--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,19 +19,54 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        logger.debug("fields : {}", Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug("constructors : {}", Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug("methods : {}", Arrays.toString(clazz.getDeclaredMethods()));
     }
 
     @Test
     @SuppressWarnings("rawtypes")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
+        Constructor[] constructors = clazz.getDeclaredConstructors();
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
+            int length = parameterTypes.length;
+            logger.debug("paramer length : {}", length);
             for (Class paramType : parameterTypes) {
                 logger.debug("param type : {}", paramType);
+
+            }
+
+            Question newQuestion;
+            if (length == 3) {
+                newQuestion = (Question) constructor.newInstance("pobi", "test", "test contents");
+            } else {
+                newQuestion = (Question) constructor.newInstance(0L, "pobi", "test", "test contents", new Date(), 2);
+            }
+            logger.debug("newInstance : {}", newQuestion);
+        }
+    }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+        logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getDeclaredFields();
+        Student newStudent = clazz.getDeclaredConstructor().newInstance();
+        for (Field f : fields) {
+            f.setAccessible(true);
+
+            if(f.getName().equals("name")) {
+                f.set(newStudent, "재성");
+            }
+            if(f.getName().equals("age")) {
+                f.set(newStudent, 30);
             }
         }
+        logger.debug("newStudent : {}", newStudent);
+        assertThat(newStudent.getName()).isEqualTo("재성");
+        assertThat(newStudent.getAge()).isEqualTo(30);
     }
 }
