dba8cd40fd49661fdb655cae0f31d4daccc956fe
wjdxor <wjdxortaek@gmail.com>
2022-08-30 17:27:59 +0900

    feat: 요구사항1_1 - controller 클래스 찾아 인스턴스 생성 하여 Map에 추가

---
+++ src/main/java/core/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,39 @@
+package core.mvc.tobe;
+
+import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Set;
+
+public class ControllerScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
+
+    private Reflections reflections;
+
+    public ControllerScanner(Object... basePackage) {
+        reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> preInitiatedControllers = reflections.getTypesAnnotatedWith(Controller.class);
+        return instantiateControllers(preInitiatedControllers);
+    }
+
+    private Map<Class<?>, Object> instantiateControllers(Set<Class<?>> preInitiatedControllers) {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        try {
+            for (Class<?> clazz : preInitiatedControllers) {
+                Object object = clazz.getConstructor().newInstance();
+                controllers.put(clazz, object);
+            }
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+            logger.error(e.getMessage());
+        }
+        return controllers;
+    }
+}
