ebd1dc3eac73c5da2ec881dfdbbadae17bb9e39c
hongbin <fusis1@naver.com>
2022-08-31 00:09:35 +0900

    test: 4단계 미션에 맞는 테스트코드 이동 및 변경

--- src/test/java/core/mvc/tobe/MyController.java
+++ src/test/java/core/mvc/tobe/MyController.java
@@ -1,18 +1,18 @@
 package core.mvc.tobe;
 
-import core.annotation.web.Controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import core.db.DataBase;
 import core.mvc.ModelAndView;
 import next.model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Controller
+// @Controller
 public class MyController {
     private static final Logger logger = LoggerFactory.getLogger(MyController.class);
 
