99104b413fd35e19bae8b543628f332c74f83853
KimYongKwon <dydrnjs518@gmail.com>
2022-08-31 22:12:15 +0900

    feat: 요구사항1, 요구사항2 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,20 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
