f8b3dec4cb0d1b4092b4f9848714516d97bb88bf
Shin seongsu <tlstjdtn321@naver.com>
2022-09-01 04:01:23 +0900

    feat: request, response resolver 구현

---
+++ src/main/java/core/mvc/resolver/HttpResponseArgumentResolver.java
@@ -0,0 +1,18 @@
+package core.mvc.resolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpResponseArgumentResolver implements ArgumentResolver {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter) {
+        return methodParameter.getType().equals(HttpServletResponse.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, HttpServletRequest request,
+        HttpServletResponse response) {
+        return response;
+    }
+}
