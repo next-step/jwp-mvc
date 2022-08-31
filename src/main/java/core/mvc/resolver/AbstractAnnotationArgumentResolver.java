7bb2a69a6b396d9d868a050f538d77b904aff364
Shin seongsu <tlstjdtn321@naver.com>
2022-09-01 04:02:24 +0900

    feat: resolver adapter로 구현하기 및 Request Param, Pathvariable resolver 구현

---
+++ src/main/java/core/mvc/resolver/AbstractAnnotationArgumentResolver.java
@@ -0,0 +1,34 @@
+package core.mvc.resolver;
+
+import core.mvc.exception.InvalidParameterAnnotation;
+import core.mvc.exception.MethodArgumentTypeNotSupportedException;
+import java.util.Arrays;
+
+public abstract class AbstractAnnotationArgumentResolver implements ArgumentResolver {
+
+    public boolean supportAnnotation(MethodParameter methodParameter, Class<?> annotationClass) {
+        return Arrays.stream(methodParameter.getAnnotations()).anyMatch(
+            annotation -> annotation.annotationType() == annotationClass
+        );
+    }
+
+    public <T> T getAnnotation(MethodParameter methodParameter, Class<T> annotationClass) {
+        return Arrays.stream(methodParameter.getAnnotations())
+            .filter(annotation -> annotation.annotationType() == annotationClass)
+            .findAny()
+            .map(annotationClass::cast)
+            .orElseThrow(() -> new InvalidParameterAnnotation(annotationClass));
+    }
+
+    public Object resolve(MethodParameter methodParameter, Object argument) {
+        if (methodParameter.isString()) {
+            return argument;
+        }
+        if (methodParameter.isInteger()) {
+            return Integer.valueOf(argument.toString());
+        }
+
+        throw new MethodArgumentTypeNotSupportedException(methodParameter.getType(), argument);
+    }
+
+}
