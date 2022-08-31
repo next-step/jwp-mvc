e4524b0086e229c92e2f47c8a2b9134008a8dd54
KimYongKwon <dydrnjs518@gmail.com>
2022-08-31 22:26:57 +0900

    feat: 요구사항3, 요구사항4 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 public class ReflectionTest {
@@ -31,5 +32,23 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+
+        Student student = new Student();
+        name.set(student, "yongkwon");
+        age.set(student, 32);
+
+        logger.debug("name => " + student.getName());
+        logger.debug("age => " + student.getAge());
     }
 }
