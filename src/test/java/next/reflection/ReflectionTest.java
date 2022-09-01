c7900a6686e359b09fb7a1496c0f71b72a46d544
YongKwon Kim <dydrnjs518@gmail.com>
2022-09-02 01:02:14 +0900

    Step1 (#254)
    * feat: 요구사항1, 요구사항2 구현
    
    * feat: 요구사항3, 요구사항4 구현
    
    * feat: 요구사항5, 요구사항6 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,10 +1,20 @@
 package next.reflection;
 
+import core.annotation.Component;
+import core.annotation.Repository;
+import core.annotation.Service;
+import core.annotation.web.Controller;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +23,9 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        logger.debug(Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug(Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug(Arrays.toString(clazz.getDeclaredMethods()));
     }
 
     @Test
@@ -27,5 +40,54 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Student> clazz = Student.class;
+
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+
+        Student student = new Student();
+        name.set(student, "yongkwon");
+        age.set(student, 32);
+
+        logger.debug("name => " + student.getName());
+        logger.debug("age => " + student.getAge());
+    }
+
+    @Test
+    @DisplayName("요구사항5")
+    public void instanceInit() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Question> clazz = Question.class;
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+
+        Question question = (Question)declaredConstructors[0].newInstance("kim", "nextstep", "nextstep is good");
+
+        logger.debug("writer => " + question.getWriter());
+        logger.debug("title => " + question.getTitle());
+        logger.debug("contents => " + question.getContents());
+    }
+
+    @Test
+    @DisplayName("요구사항6")
+    public void showAnnotations() {
+        Reflections reflections = new Reflections("core.di.factory.example");
+
+        Set<Class<?>> classByComponentAnnotation = reflections.getTypesAnnotatedWith(Controller.class);
+        classByComponentAnnotation.stream()
+                .forEach(System.out::println);
+
+        Set<Class<?>> classByServiceAnnotation = reflections.getTypesAnnotatedWith(Service.class);
+        classByServiceAnnotation.stream()
+                .forEach(System.out::println);
+
+        Set<Class<?>> classByRepositoryAnnotation = reflections.getTypesAnnotatedWith(Repository.class);
+        classByRepositoryAnnotation.stream()
+                .forEach(System.out::println);
     }
 }
