d5c860446e924f217e71218b5f2c70953de094ae
이정수 <lsecret@naver.com>
2022-09-03 17:19:35 +0900

    feat: 애노테이션 기반 MVC 프레임워크 추가

--- src/main/java/core/annotation/web/RequestMethod.java
+++ src/main/java/core/annotation/web/RequestMethod.java
@@ -1,5 +1,14 @@
 package core.annotation.web;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod requestMethod(String method) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(requestMethod -> requestMethod.name().equalsIgnoreCase(method))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
