e0ceb76caf4293b64046c11709f94815b0f1a107
joel-you <ruew12@gmail.com>
2022-09-06 19:26:17 +0900

    feature: 요구사항 3단계
    * docs 수정 (요구사항 check)
    * Junit4TestRunner에 요구사항 구현

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -10,7 +10,6 @@ public class Junit3TestRunner {
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
-        
         Method[] methods = clazz.getDeclaredMethods();
 
         for (Method method : methods) {
--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,24 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            runWithTestMethod(method, clazz);
+        }
+    }
+
+    private void runWithTestMethod(Method method, Class<Junit4Test> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        if (method.isAnnotationPresent(MyTest.class)) {
+            method.invoke(clazz.newInstance());
+        }
     }
 }
