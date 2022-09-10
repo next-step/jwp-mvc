d6091389e94cff43985c3b7cee907e0c0e43810f
kyun <annakky@naver.com>
2022-09-10 23:07:24 +0900

    feat: controller에서 requestMapping annotation 찾기 구현

--- src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
+++ src/main/java/core/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,22 @@
 package core.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -16,7 +26,60 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Set<Class<?>> controllers = findControllers();
 
+        Set<Method> methods = findMethods(controllers);
+
+        addHandlerExecutions(methods);
+    }
+
+    private Set<Class<?>> findControllers() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private Set<Method> findMethods(Set<Class<?>> controllers) {
+        return controllers.stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .filter(it -> it.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toSet());
+    }
+
+    private void addHandlerExecutions(Set<Method> methods) {
+        for (Method method : methods) {
+            Set<HandlerKey> handlerKeys = createHandlerKey(method);
+            HandlerExecution handlerExecution = createHandlerExecution(method);
+
+            for (HandlerKey handlerKey : handlerKeys) {
+                handlerExecutions.put(handlerKey, handlerExecution);
+            }
+        }
+    }
+
+    private Set<HandlerKey> createHandlerKey(Method method) {
+        RequestMapping annotation = method.getDeclaredAnnotation(RequestMapping.class);
+        String url = annotation.value();
+        RequestMethod requestMethod = annotation.method();
+
+        if (requestMethod != null) {
+            return Set.of(new HandlerKey(url, requestMethod));
+        }
+
+        return Arrays.stream(RequestMethod.values())
+                .map(it -> new HandlerKey(url, it))
+                .collect(Collectors.toSet());
+    }
+
+    private HandlerExecution createHandlerExecution(Method method) {
+        try {
+            Class<?> declaringClass = method.getDeclaringClass();
+            Object classInstance = declaringClass.getDeclaredConstructor().newInstance();
+
+            return new HandlerExecution(classInstance, method);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/main/java/core/mvc/tobe/HandlerExecution.java
+++ src/main/java/core/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,20 @@
 package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
-
+import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HandlerExecution {
+    private final Object object;
+    private final Method method;
+
+    public HandlerExecution(Object object, Method method) {
+        this.object = object;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(object, request, response);
     }
 }
