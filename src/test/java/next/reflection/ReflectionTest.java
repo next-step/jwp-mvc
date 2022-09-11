59bf42d920bf9724a4c6946a6eca849999d300d5
dev-jiwonyang <jiwonyang@devsisters.com>
2022-09-11 22:33:38 +0900

    feat: 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,13 +33,23 @@ public class ReflectionTest {
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
+        Map<Integer, Question> questions = new HashMap();
         for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+            final int parameterNum = constructor.getParameterTypes().length;
+            if (parameterNum == 3) {
+                Question question = (Question) constructor.newInstance("지원", "제목1", "내용1");
+                questions.put(3, question);
+            }
+            if (parameterNum == 6) {
+                Question question = (Question) constructor.newInstance(1L, "지원", "제목1", "내용1", new Date(0), 0);
+                questions.put(6, question);
             }
         }
+
+        final Question expectedQuestionWithThreeParams = new Question("지원", "제목1", "내용1");
+        final Question expectedQuestionWithSixParams = new Question(1L, "지원", "제목1", "내용1", new Date(0), 0);
+        assertEquals(expectedQuestionWithThreeParams, questions.get(3));
+        assertEquals(expectedQuestionWithSixParams, questions.get(6));
     }
 
     @Test
