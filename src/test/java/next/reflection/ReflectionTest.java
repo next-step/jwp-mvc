0064123f52a6a5ccc838bb8dc8c36d32d1cce117
dev-jiwonyang <jiwonyang@devsisters.com>
2022-09-11 22:17:40 +0900

    feat: 요구사항 4 - private field에 값 할당

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -32,5 +35,21 @@ public class ReflectionTest {
                 logger.debug("param type : {}", paramType);
             }
         }
+    }
+
+    @Test
+    public void privateFieldAccess() throws IllegalAccessException, NoSuchFieldException {
+        Class<Student> clazz = Student.class;
+        final Student student = new Student();
+
+        final Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);
+        nameField.set(student, "지원");
+        final Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+        ageField.set(student, 25);
+
+        assertEquals("지원", student.getName());
+        assertEquals(25, student.getAge());
     }
 }
