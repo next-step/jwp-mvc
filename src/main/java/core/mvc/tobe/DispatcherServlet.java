98137e520029f82a47739c6c24f8887456a9301b
joel-you <ruew12@gmail.com>
2022-09-14 03:31:29 +0900

    feature: 요구사항 2 - 레거시 MVC와 애노테이션 기반 MVC 통합
    * 레거시코드 삭제
    * ForwardController 대체용으로 UserController 에 Method 추가

--- src/main/java/core/mvc/tobe/DispatcherServlet.java
+++ src/main/java/core/mvc/tobe/DispatcherServlet.java
@@ -1,20 +1,15 @@
-package core.mvc.asis;
+package core.mvc.tobe;
 
 import core.mvc.ModelAndView;
 import core.mvc.View;
-import core.mvc.tobe.AnnotationHandlerMapping;
-import core.mvc.tobe.HandlerExecution;
-import core.mvc.tobe.HandlerMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Objects;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
@@ -22,14 +17,10 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
-
-    private RequestMapping requestMapping;
     private final HandlerMapping handlerMapping = new AnnotationHandlerMapping();
 
     @Override
     public void init() {
-        requestMapping = new RequestMapping();
-        requestMapping.initMapping();
         handlerMapping.initialize();
     }
 
@@ -43,18 +34,6 @@ public class DispatcherServlet extends HttpServlet {
         if (Objects.nonNull(handlerExecution)) {
             renderingByView(handlerExecution, req, resp);
         }
-
-        Controller controller = requestMapping.findController(requestUri);
-        if (Objects.isNull(controller)) {
-            return;
-        }
-        try {
-            String viewName = controller.execute(req, resp);
-            move(viewName, req, resp);
-        } catch (Throwable e) {
-            logger.error("Exception : {}", e);
-            throw new ServletException(e.getMessage());
-        }
     }
 
     private void renderingByView(HandlerExecution handlerExecution, HttpServletRequest req, HttpServletResponse resp) {
@@ -65,15 +44,5 @@ public class DispatcherServlet extends HttpServlet {
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
