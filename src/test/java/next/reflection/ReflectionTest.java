68b19f14e553fcd487f6ee9db61fb8a8ac8bddd2
rheew <47098896+rheew@users.noreply.github.com>
2022-09-13 14:15:11 +0900

    feat: ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다. 요구사항 1

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,7 +14,17 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+        logger.debug("클래스 이름");
         logger.debug(clazz.getName());
+        logger.debug("필드 이름");
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(i -> logger.debug(i.getName()));
+        logger.debug("생성자 이름");
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .forEach(i -> logger.debug(i.getName()));
+        logger.debug("메소드 이름");
+        Arrays.stream(clazz.getDeclaredMethods())
+                .forEach(i -> logger.debug(i.getName()));
     }
 
     @Test
