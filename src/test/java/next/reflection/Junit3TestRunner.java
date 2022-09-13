45bffc5193303445e16eafc1e592ecdc567605d4
rheew <47098896+rheew@users.noreply.github.com>
2022-09-13 14:21:58 +0900

    feat: Junit3Test 클래스에서 test로 시작하는 메소드만 Java Reflection을 활용해 실행하도록 구현 요구사항 2

--- src/test/java/next/reflection/Junit3TestRunner.java
+++ src/test/java/next/reflection/Junit3TestRunner.java
@@ -2,10 +2,27 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(i -> i.getName().startsWith("test"))
+                .forEach(i -> {
+                    try {
+                        i.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
