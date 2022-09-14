2017d68b6e6cece41cab187e17c683619b95e8e0
hiro <kiryuinh32@gmail.com>
2022-09-14 21:27:00 +0900

    feat: UserController

---
+++ src/main/java/next/controller/UserController.java
@@ -0,0 +1,29 @@
+package next.controller;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.db.DataBase;
+import core.mvc.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller(path = "/users")
+public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ModelAndView createUser(HttpServletRequest request, HttpServletResponse response) {
+		User user = new User(request.getParameter("userId"), request.getParameter("password"), request.getParameter("name"),
+			request.getParameter("email"));
+		log.debug("User : {}", user);
+
+		DataBase.addUser(user);
+		return new ModelAndView("redirect:/");
+	}
+
+}
