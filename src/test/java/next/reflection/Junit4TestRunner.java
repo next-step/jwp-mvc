d4cc68a618b3a6eb84520d3a6a0d68c94a46b78d
limhangyeol <hang19663@gmail.com>
2022-10-22 21:27:48 +0900

    feature: @MyTest 어노테이션이 설정된 메서드 실행 (요구사항3)

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -1,11 +1,33 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
+
     @Test
-    public void run() throws Exception {
+    @DisplayName("@MyTest 어노테이션이 설정되어 있는 메서드를 실행한다.")
+    public void run() {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        Method[] methods = clazz.getDeclaredMethods();
+        Constructor<?> defaultConstructor = clazz.getConstructors()[0];
+
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(it -> executeMethod(defaultConstructor, it));
+    }
+
+    private void executeMethod(Constructor<?> defaultConstructor, Method it) {
+        try {
+            it.invoke(defaultConstructor.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
